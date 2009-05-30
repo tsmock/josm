d5a20359806e596b8ae199dcb6691e58909bc984
Dirk Stöcker <github@dstoecker.de>
2009-05-30 11:58:30 +0000

    cleanup group settings in preferences

--- src/org/openstreetmap/josm/gui/historycombobox/SuggestingJHistoryComboBox.java
+++ src/org/openstreetmap/josm/gui/historycombobox/SuggestingJHistoryComboBox.java
@@ -1,18 +1,18 @@
 /* Copyright (c) 2008, Henrik Niehaus
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of the project nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -43,17 +43,17 @@ import javax.swing.text.JTextComponent;
 public class SuggestingJHistoryComboBox extends JHistoryComboBox implements KeyListener {
 
     private EventConsumingPlainDocument doc = new EventConsumingPlainDocument();
-    
+
     public SuggestingJHistoryComboBox(List<String> history) {
         super(history);
 
         // add keylistener for ctrl + space
         getEditor().getEditorComponent().addKeyListener(this);
-        
+
         // add specialized document, which can consume events, which are
         // produced by the suggestion
         ((JTextComponent)getEditor().getEditorComponent()).setDocument(doc);
-        
+
         // add DocumentFilter to trigger suggestion
         JTextField editor = (JTextField) getEditor().getEditorComponent();
         final AbstractDocument doc = (AbstractDocument) editor.getDocument();
@@ -88,7 +88,7 @@ public class SuggestingJHistoryComboBox extends JHistoryComboBox implements KeyL
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() instanceof JTextField) {
             JTextField textField = (JTextField) e.getSource();
-    
+
             // if the ActionCommand equals SUGGEST, the user confirms a suggestion
             if("SUGGEST".equals(e.getActionCommand())) {
                 textField.setSelectionStart(textField.getText().length());
@@ -109,7 +109,7 @@ public class SuggestingJHistoryComboBox extends JHistoryComboBox implements KeyL
             if (suggestion.startsWith(text)) {
                 textField.setActionCommand("SUGGEST");
                 doc.setConsumeEvents(true);
-                // avoid unbound recursion via setText() -> replace() -> 
+                // avoid unbound recursion via setText() -> replace() ->
                 // suggest() -> setText() ... in some environments
                 if (! text.equals(suggestion)) {
                     textField.setText(suggestion);
@@ -121,11 +121,11 @@ public class SuggestingJHistoryComboBox extends JHistoryComboBox implements KeyL
             }
         }
     }
-    
+
     public void keyReleased(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_SPACE && e.isControlDown()) {
             suggest();
-        } 
+        }
     }
     public void keyPressed(KeyEvent e) {}
     public void keyTyped(KeyEvent e) {}
