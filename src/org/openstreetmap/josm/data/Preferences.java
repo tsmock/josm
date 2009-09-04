tsmock/josm

commit 0302c9d044410e6593149cabdfa3b1a381d280fd

applied #2322: patch by xeen: JOSM destroys its preferences, when disk is full
git-svn-id: https://josm.openstreetmap.de/svn/trunk@2038 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/Preferences.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -43,7 +44,7 @@ import org.openstreetmap.josm.tools.ColorHelper;
 public class Preferences {
 
     /**
-     * Internal storage for the preferenced directory.
+     * Internal storage for the preference directory.
      * Do not access this variable directly!
      * @see #getPreferencesDirFile()
      */
@@ -290,8 +291,13 @@ public class Preferences {
         properties.put("josm.version", AboutAction.getVersionString());
         try {
             setSystemProperties();
+            String prefFile = getPreferencesDir() + "preferences";
+
+            // Backup old preferences
+            copyFile(new File(prefFile), new File(prefFile + "_backup"));
+
             final PrintWriter out = new PrintWriter(new OutputStreamWriter(
-                    new FileOutputStream(getPreferencesDir() + "preferences"), "utf-8"), false);
+                    new FileOutputStream(prefFile + "_tmp"), "utf-8"), false);
             for (final Entry<String, String> e : properties.entrySet()) {
                 String s = defaults.get(e.getKey());
                 /* don't save default values */
@@ -300,12 +306,45 @@ public class Preferences {
                 }
             }
             out.close();
+
+            File tmpFile = new File(prefFile + "_tmp");
+            copyFile(tmpFile, new File(prefFile));
+            tmpFile.delete();
+
         } catch (final IOException e) {
             e.printStackTrace();
             // do not message anything, since this can be called from strange
             // places.
         }
     }
+
+    /**
+     * Simple file copy function that will overwrite the target file
+     * Taken from http://www.rgagnon.com/javadetails/java-0064.html (CC-NC-BY-SA)
+     * @param in
+     * @param out
+     * @throws IOException
+     */
+    public static void copyFile(File in, File out) throws IOException  {
+        FileChannel inChannel = new FileInputStream(in).getChannel();
+        FileChannel outChannel = new FileOutputStream(out).getChannel();
+        try {
+            inChannel.transferTo(0, inChannel.size(),
+                    outChannel);
+        }
+        catch (IOException e) {
+            throw e;
+        }
+        finally {
+            if (inChannel != null) {
+                inChannel.close();
+            }
+            if (outChannel != null) {
+                outChannel.close();
+            }
+        }
+    }
+
 
     public void load() throws IOException {
         properties.clear();

commit 01086ed74eca13e7bf12ac8d2dfd0af7c0196f40

fixed #2322: Regression in r2038 (patches "JOSM destroys its preferences, when disk is full")

src/org/openstreetmap/josm/data/Preferences.java
@@ -118,6 +118,10 @@ public class Preferences {
         return preferencesDirFile;
     }
 
+    public File getPreferenceFile() {
+        return new File(getPreferencesDirFile(), "preferences");
+    }
+
     public File getPluginsDirFile() {
         return new File(getPreferencesDirFile(), "plugins");
     }
@@ -253,7 +257,11 @@ public class Preferences {
             } else {
                 properties.put(key, value);
             }
-            save();
+            try {
+                save();
+            } catch(IOException e){
+                System.out.println(tr("Warning: failed to persist preferences to ''{0}''", getPreferenceFile().getAbsoluteFile()));
+            }
             firePreferenceChanged(key, value);
             return true;
         }
@@ -286,36 +294,32 @@ public class Preferences {
      * Called after every put. In case of a problem, do nothing but output the error
      * in log.
      */
-    public void save() {
+    public void save() throws IOException {
         /* currently unused, but may help to fix configuration issues in future */
         properties.put("josm.version", AboutAction.getVersionString());
-        try {
-            setSystemProperties();
-            String prefFile = getPreferencesDir() + "preferences";
 
-            // Backup old preferences
-            copyFile(new File(prefFile), new File(prefFile + "_backup"));
+        setSystemProperties();
+        File prefFile = new File(getPreferencesDirFile(), "preferences");
 
-            final PrintWriter out = new PrintWriter(new OutputStreamWriter(
-                    new FileOutputStream(prefFile + "_tmp"), "utf-8"), false);
-            for (final Entry<String, String> e : properties.entrySet()) {
-                String s = defaults.get(e.getKey());
-                /* don't save default values */
-                if(s == null || !s.equals(e.getValue())) {
-                    out.println(e.getKey() + "=" + e.getValue());
-                }
-            }
-            out.close();
-
-            File tmpFile = new File(prefFile + "_tmp");
-            copyFile(tmpFile, new File(prefFile));
-            tmpFile.delete();
-
-        } catch (final IOException e) {
-            e.printStackTrace();
-            // do not message anything, since this can be called from strange
-            // places.
+        // Backup old preferences if there are old preferences
+        if(prefFile.exists()) {
+            copyFile(prefFile, new File(prefFile + "_backup"));
         }
+
+        final PrintWriter out = new PrintWriter(new OutputStreamWriter(
+                new FileOutputStream(prefFile + "_tmp"), "utf-8"), false);
+        for (final Entry<String, String> e : properties.entrySet()) {
+            String s = defaults.get(e.getKey());
+            /* don't save default values */
+            if(s == null || !s.equals(e.getValue())) {
+                out.println(e.getKey() + "=" + e.getValue());
+            }
+        }
+        out.close();
+
+        File tmpFile = new File(prefFile + "_tmp");
+        copyFile(tmpFile, prefFile);
+        tmpFile.delete();
     }
 
     /**
@@ -365,49 +369,77 @@ public class Preferences {
         setSystemProperties();
     }
 
-    public void init(Boolean reset)
-    {
+    public void init(boolean reset){
         // get the preferences.
         File prefDir = getPreferencesDirFile();
         if (prefDir.exists()) {
             if(!prefDir.isDirectory()) {
+                System.err.println(tr("Warning: Failed to initialize preferences. Preference directory ''{0}'' isn't a directory.", prefDir.getAbsoluteFile()));
                 JOptionPane.showMessageDialog(
                         Main.parent,
-                        tr("Cannot open preferences directory: {0}",Main.pref.getPreferencesDir()),
+                        tr("<html>Failed to initialize preferences.<br>Preference directory ''{0}'' isn't a directory.</html>", prefDir.getAbsoluteFile()),
                         tr("Error"),
                         JOptionPane.ERROR_MESSAGE
                 );
                 return;
             }
         } else {
-            prefDir.mkdirs();
-        }
-
-        if (!new File(getPreferencesDir()+"preferences").exists()) {
-            resetToDefault();
-        }
-
-        try {
-            if (reset) {
-                resetToDefault();
-            } else {
-                load();
+            if (! prefDir.mkdirs()) {
+                System.err.println(tr("Warning: Failed to initialize preferences. Failed to create missing preference directory: {0}", prefDir.getAbsoluteFile()));
+                JOptionPane.showMessageDialog(
+                        Main.parent,
+                        tr("<html>Failed to initialize preferences.<br>Failed to create missing preference directory: {0}</html>",prefDir.getAbsoluteFile()),
+                        tr("Error"),
+                        JOptionPane.ERROR_MESSAGE
+                );
+                return;
             }
-        } catch (final IOException e1) {
-            e1.printStackTrace();
-            String backup = getPreferencesDir() + "preferences.bak";
+        }
+
+        File preferenceFile = getPreferenceFile();
+        try {
+            if (!preferenceFile.exists()) {
+                System.out.println(tr("Warning: Missing preference file ''{0}''. Creating a default preference file.", preferenceFile.getAbsoluteFile()));
+                resetToDefault();
+                save();
+            } else if (reset) {
+                System.out.println(tr("Warning: Replacing existing preference file ''{0}'' with default preference file.", preferenceFile.getAbsoluteFile()));
+                resetToDefault();
+                save();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
             JOptionPane.showMessageDialog(
                     Main.parent,
-                    tr("Preferences file had errors. Making backup of old one to {0}.", backup),
+                    tr("<html>Failed to initialize preferences.<br>Failed to reset preference file to default: {0}</html>",getPreferenceFile().getAbsoluteFile()),
                     tr("Error"),
                     JOptionPane.ERROR_MESSAGE
             );
-            new File(getPreferencesDir() + "preferences").renameTo(new File(backup));
-            save();
+            return;
+        }
+        try {
+            load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            File backupFile = new File(prefDir,"preferences.bak");
+            JOptionPane.showMessageDialog(
+                    Main.parent,
+                    tr("<html>Preferences file had errors.<br> Making backup of old one to <br>{0}<br> and creating a new default preference file.</html>", backupFile.getAbsoluteFile()),
+                    tr("Error"),
+                    JOptionPane.ERROR_MESSAGE
+            );
+            preferenceFile.renameTo(backupFile);
+            try {
+                resetToDefault();
+                save();
+            } catch(IOException e1) {
+                e1.printStackTrace();
+                System.err.println(tr("Warning: Failed to initialize preferences.Failed to reset preference file to default: {0}", getPreferenceFile()));
+            }
         }
     }
 
-    public final void resetToDefault() {
+    public final void resetToDefault(){
         properties.clear();
         put("layerlist.visible", true);
         put("propertiesdialog.visible", true);
@@ -418,11 +450,14 @@ public class Preferences {
         } else {
             put("laf", "com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         }
-        save();
+    }
+
+    public File getBookmarksFile() {
+        return new File(getPreferencesDir(),"bookmarks");
     }
 
     public Collection<Bookmark> loadBookmarks() throws IOException {
-        File bookmarkFile = new File(getPreferencesDir()+"bookmarks");
+        File bookmarkFile = getBookmarksFile();
         if (!bookmarkFile.exists()) {
             bookmarkFile.createNewFile();
         }

src/org/openstreetmap/josm/gui/BookmarkList.java
@@ -14,6 +14,8 @@ import javax.swing.JOptionPane;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Preferences;
 
+import sun.security.action.GetBooleanAction;
+
 /**
  * List class that read and save its content from the bookmark file.
  * @author imi
@@ -43,7 +45,10 @@ public class BookmarkList extends JList {
             e.printStackTrace();
             JOptionPane.showMessageDialog(
                     Main.parent,
-                    tr("<html>Could not read bookmarks.<br>{0}</html>", e.getMessage()),
+                    tr("<html>Could not read bookmarks from<br>''{0}''<br>Error was: {1}</html>",
+                            Main.pref.getBookmarksFile(),
+                            e.getMessage()
+                    ),
                     tr("Error"),
                     JOptionPane.ERROR_MESSAGE
             );

src/org/openstreetmap/josm/gui/MainApplet.java
@@ -9,6 +9,7 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
@@ -35,7 +36,7 @@ public class MainApplet extends JApplet {
     public static final class UploadPreferencesAction extends JosmAction {
         public UploadPreferencesAction() {
             super(tr("Upload Preferences"), "upload-preferences", tr("Upload the current preferences to the server"),
-            Shortcut.registerShortcut("applet:uploadprefs", tr("Upload Preferences"), KeyEvent.VK_U, Shortcut.GROUP_HOTKEY), true);
+                    Shortcut.registerShortcut("applet:uploadprefs", tr("Upload Preferences"), KeyEvent.VK_U, Shortcut.GROUP_HOTKEY), true);
         }
         public void actionPerformed(ActionEvent e) {
             ((ServerSidePreferences)Main.pref).upload();
@@ -69,8 +70,9 @@ public class MainApplet extends JApplet {
     @Override public void init() {
         for (String[] s : paramInfo) {
             Collection<String> p = readParameter(s[0], args.get(s[0]));
-            if (p != null)
+            if (p != null) {
                 args.put(s[0], p);
+            }
         }
         if (!args.containsKey("geometry") && getParameter("width") != null && getParameter("height") != null) {
             args.put("geometry", Arrays.asList(new String[]{getParameter("width")+"x"+getParameter("height")}));
@@ -97,7 +99,6 @@ public class MainApplet extends JApplet {
         Main.applet = true;
         Main.pref = new ServerSidePreferences(getCodeBase());
         ((ServerSidePreferences)Main.pref).download(username, password);
-
         Main.preConstructorInit(args);
         Main.parent = this;
 
@@ -123,8 +124,9 @@ public class MainApplet extends JApplet {
     private Collection<String> readParameter(String s, Collection<String> v) {
         String param = getParameter(s);
         if (param != null) {
-            if (v == null)
+            if (v == null) {
                 v = new LinkedList<String>();
+            }
             v.addAll(Arrays.asList(param.split(";")));
         }
         return v;