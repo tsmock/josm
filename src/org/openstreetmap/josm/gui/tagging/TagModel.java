a179da1fec1129b785f8b6d260aed20ab706f8c2
Dirk Stöcker <github@dstoecker.de>
2009-11-24 09:45:04 +0000

    i18n updated, fixed files to reduce problems when applying patches, fix #4017

--- src/org/openstreetmap/josm/gui/tagging/TagModel.java
+++ src/org/openstreetmap/josm/gui/tagging/TagModel.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TagModel {
-    
+
     /** the name of the tag */
     private String name = null;
-    
+
     /** the list of values */
     private ArrayList<String> values = null;
-    
+
     /**
      * constructor
      */
@@ -20,100 +20,99 @@ public class TagModel {
         setName("");
         setValue("");
     }
-    
+
     /**
-     * constructor 
-     * @param name the tag name 
+     * constructor
+     * @param name the tag name
      */
     public TagModel(String name) {
         this();
         setName(name);
     }
-    
+
     /**
-     * constructor 
-     * 
-     * @param name the tag name 
-     * @param value the tag value 
+     * constructor
+     *
+     * @param name the tag name
+     * @param value the tag value
      */
     public TagModel(String name, String value) {
         this();
         setName(name);
         setValue(value);
     }
-    
+
     /**
      * sets the name. Converts name to "" if null.
-     * @param name the tag name 
+     * @param name the tag name
      */
     public void setName(String name) {
         name = (name == null) ? "" : name;
-        this.name = name; 
+        this.name = name;
     }
-    
-    /** 
-     * @return the tag name 
+
+    /**
+     * @return the tag name
      */
     public String getName(){
         return name;
     }
-    
+
     /**
-     * removes all values from the list of values 
+     * removes all values from the list of values
      */
     public void clearValues() {
         this.values.clear();
     }
-    
+
     /**
      * sets a unique value for this tag. Converts value to "", if null.
-     * @param value the value. 
+     * @param value the value.
      */
     public void setValue(String value) {
         value = (value == null) ? "" : value;
         clearValues();
         this.values.add(value);
     }
-    
+
     /**
-     * 
-     * @param value the value to be checked; converted to "" if null 
-     * @return true, if the values of this tag include <code>value</code>; false otherwise 
+     *
+     * @param value the value to be checked; converted to "" if null
+     * @return true, if the values of this tag include <code>value</code>; false otherwise
      */
     public boolean hasValue(String value) {
         value = (value == null) ? "" : value;
         return values.contains(value);
     }
-    
+
     public void addValue(String value) {
         value = (value == null) ? "" : value;
         if (hasValue(value)) {
-            return; 
+            return;
         }
         values.add(value);
     }
-    
-    
+
     /**
      * removes a value from the list of values. Converts value to "" if null
-     * @param value the value 
+     * @param value the value
      */
     public void removeValue(String value){
-        value = (value == null) ? "" : value; 
+        value = (value == null) ? "" : value;
         values.remove(value);
-    }	
-    
+    }
+
     public List<String> getValues() {
         return values;
     }
-    
+
     public String getValue() {
         if (getValueCount() == 0) {
             return "";
         } else if (getValueCount() == 1) {
             return values.get(0);
         } else {
-            StringBuilder sb = new StringBuilder();			
+            StringBuilder sb = new StringBuilder();
             for (int i =0; i < values.size(); i++) {
                 sb.append(values.get(i));
                 if (i + 1 < values.size()) {
@@ -123,7 +122,7 @@ public class TagModel {
             return sb.toString();
         }
     }
-    
+
     public int getValueCount() {
         return values.size();
     }
