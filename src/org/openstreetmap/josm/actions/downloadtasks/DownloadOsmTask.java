tsmock/josm

commit 12c907baf07f02bad30c1434d4ab5ca9e3ca9072

in DownloadOsmTask zoom to downloaded primitives when there was not bbox given
git-svn-id: https://josm.openstreetmap.de/svn/trunk@3064 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
@@ -10,7 +10,6 @@ import java.util.logging.Logger;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Bounds;
-import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.DataSource;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
@@ -63,7 +62,8 @@ public class DownloadOsmTask extends AbstractDownloadTask {
         downloadTask = new DownloadTask(new_layer,
                 new OsmServerLocationReader(url),
                 progressMonitor);
-        currentBounds = new Bounds(new LatLon(0,0), new LatLon(0,0));
+        //currentBounds = new Bounds(new LatLon(0,0), new LatLon(0,0));
+        currentBounds = null;
         return Main.worker.submit(downloadTask);
     }
 
@@ -138,8 +138,8 @@ public class DownloadOsmTask extends AbstractDownloadTask {
                 return;
             if (dataSet == null)
                 return; // user canceled download or error occurred
-            if (currentBounds == null)
-                return; // no data retrieved
+            //if (currentBounds == null)
+            //  return; // no data retrieved
             if (dataSet.allPrimitives().isEmpty()) {
                 rememberErrorMessage(tr("No data found in this area."));
                 // need to synthesize a download bounds lest the visual indication of downloaded
@@ -155,14 +155,18 @@ public class DownloadOsmTask extends AbstractDownloadTask {
                 //
                 OsmDataLayer layer = new OsmDataLayer(dataSet, OsmDataLayer.createNewName(), null);
                 final boolean isDisplayingMapView = Main.isDisplayingMapView();
-                
+
                 Main.main.addLayer(layer);
 
                 // If the mapView is not there yet, we cannot calculate the bounds (see constructor of MapView).
                 // Otherwise jump to the current download.
                 if (isDisplayingMapView) {
                     BoundingXYVisitor v = new BoundingXYVisitor();
-                    v.visit(currentBounds);
+                    if (currentBounds != null) {
+                        v.visit(currentBounds);
+                    } else {
+                        v.computeBoundingBox(dataSet.getNodes());
+                    }
                     Main.map.mapView.recalculateCenterScale(v);
                 }
             } else {
@@ -173,7 +177,11 @@ public class DownloadOsmTask extends AbstractDownloadTask {
                 }
                 target.mergeFrom(dataSet);
                 BoundingXYVisitor v = new BoundingXYVisitor();
-                v.visit(currentBounds);
+                if (currentBounds != null) {
+                    v.visit(currentBounds);
+                } else {
+                    v.computeBoundingBox(dataSet.getNodes());
+                }
                 Main.map.mapView.recalculateCenterScale(v);
                 target.onPostDownloadFromServer();
             }

commit 053510d6ef93918f699ddf634f8e79a9888a6d67

fix NPE caused by r3064

src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.DataSource;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
@@ -141,7 +142,7 @@ public class DownloadOsmTask extends AbstractDownloadTask {
                 rememberErrorMessage(tr("No data found in this area."));
                 // need to synthesize a download bounds lest the visual indication of downloaded
                 // area doesn't work
-                dataSet.dataSources.add(new DataSource(currentBounds, "OpenStreetMap server"));
+                dataSet.dataSources.add(new DataSource(currentBounds != null ? currentBounds : new Bounds(new LatLon(0, 0)), "OpenStreetMap server"));
             }
 
             rememberDownloadedData(dataSet);