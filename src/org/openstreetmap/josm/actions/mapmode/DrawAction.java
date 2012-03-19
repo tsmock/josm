tsmock/josm

commit 504269fb0890f85315af540780e4e1de29235ac3

flip default value for wayIsFinished variable when entering drawMode
By default no draw is active, i.e. the helper line needs not to be
drawn. wayIsFinished should reflect this, because otherwise super-
fluous repaints are issued. See #7503


git-svn-id: https://josm.openstreetmap.de/svn/trunk@5099 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/mapmode/DrawAction.java
@@ -186,7 +186,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, SelectionCh
         selectedColor =PaintColors.SELECTED.get();
         drawHelperLine = Main.pref.getBoolean("draw.helper-line", true);
         drawTargetHighlight = Main.pref.getBoolean("draw.target-highlight", true);
-        wayIsFinished = false;
+        wayIsFinished = true;
         snapHelper.init();
         snapCheckboxMenuItem.getAction().setEnabled(true);
 

commit 24273578e2441ea767e40b546ef13f61ea40007a

fix parts of draw mode which broke in r5099 (fixes #7524)

src/org/openstreetmap/josm/actions/mapmode/DrawAction.java
@@ -190,7 +190,12 @@ public class DrawAction extends MapMode implements MapViewPaintable, SelectionCh
         selectedColor =PaintColors.SELECTED.get();
         drawHelperLine = Main.pref.getBoolean("draw.helper-line", true);
         drawTargetHighlight = Main.pref.getBoolean("draw.target-highlight", true);
-        wayIsFinished = true;
+
+        // determine if selection is suitable to continue drawing. If it
+        // isn't, set wayIsFinished to true to avoid superfluous repaints.
+        determineCurrentBaseNodeAndPreviousNode(getCurrentDataSet().getSelected());
+        wayIsFinished = currentBaseNode == null;
+
         snapHelper.init();
         snapCheckboxMenuItem.getAction().setEnabled(true);
 