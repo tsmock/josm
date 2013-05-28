tsmock/josm

commit 28f62d05b820595659690fcc513357395cdd1efe

* Improve PopupMenuLauncher to automatically select JTable/JList/JTree line under cursor * Simplify all popup menus with this new feature * Remove custom popup menus in BoundingBoxSelection and BoundingBoxSelection to use default text field popup menus recently introduced * Fix EDT violations in changeset window * Make Autoscale actions publicly available in MainMenu * Add a new "Zoom to problem" mode in AutoScaleAction * Make "Zoom to problem" menu item in validator dialog use this new feature * Update enabled state of "Zoom to Conflict" and "Zoom to problem" against selection of conflict/validator dialog
git-svn-id: https://josm.openstreetmap.de/svn/trunk@5958 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
@@ -20,7 +20,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -46,6 +45,7 @@ import javax.swing.JPopupMenu;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
@@ -60,6 +60,7 @@ import org.openstreetmap.josm.gui.preferences.PreferenceDialog;
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.SubPreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.TabPreferenceSetting;
+import org.openstreetmap.josm.gui.widgets.PopupMenuLauncher;
 import org.openstreetmap.josm.tools.Destroyable;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
@@ -476,66 +477,7 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
             lblTitle_weak.setMinimumSize(new Dimension(0,20));
             add(lblTitle_weak, GBC.std().fill(GBC.HORIZONTAL));
 
-            addMouseListener(
-                    new MouseAdapter() {
-                        @Override
-                        public void mouseClicked(MouseEvent e) {
-                            if (isCollapsed) {
-                                expand();
-                                dialogsPanel.reconstruct(Action.COLLAPSED_TO_DEFAULT, ToggleDialog.this);
-                            } else {
-                                collapse();
-                                dialogsPanel.reconstruct(Action.ELEMENT_SHRINKS, null);
-                            }
-                        }
-
-                        private void maybeShowPopup(MouseEvent e) {
-                            if (e.isPopupTrigger()) {
-                                JPopupMenu menu = new JPopupMenu();
-                                JMenu buttonHidingMenu = new JMenu(tr("Side buttons"));
-                                JRadioButtonMenuItem alwaysShown = new JRadioButtonMenuItem(new AbstractAction(tr("Always shown")) {
-                                    @Override
-                                    public void actionPerformed(ActionEvent e) {
-                                        setIsButtonHiding(ButtonHiddingType.ALWAYS_SHOWN);
-                                    }
-                                });
-                                JRadioButtonMenuItem dynamic = new JRadioButtonMenuItem(new AbstractAction(tr("Dynamic")) {
-                                    @Override
-                                    public void actionPerformed(ActionEvent e) {
-                                        setIsButtonHiding(ButtonHiddingType.DYNAMIC);
-                                    }
-                                });
-                                JRadioButtonMenuItem alwaysHidden = new JRadioButtonMenuItem(new AbstractAction(tr("Always hidden")) {
-                                    @Override
-                                    public void actionPerformed(ActionEvent e) {
-                                        setIsButtonHiding(ButtonHiddingType.ALWAYS_HIDDEN);
-                                    }
-                                });
-                                alwaysShown.setSelected(buttonHiding == ButtonHiddingType.ALWAYS_SHOWN);
-                                dynamic.setSelected(buttonHiding == ButtonHiddingType.DYNAMIC);
-                                alwaysHidden.setSelected(buttonHiding == ButtonHiddingType.ALWAYS_HIDDEN);
-                                buttonHidingMenu.add(alwaysShown);
-                                buttonHidingMenu.add(dynamic);
-                                buttonHidingMenu.add(alwaysHidden);
-                                menu.add(buttonHidingMenu);
-                                for (javax.swing.Action action: buttonActions) {
-                                    menu.add(action);
-                                }
-                                menu.show(TitleBar.this, e.getX(), e.getY());
-                            }
-                        }
-
-                        @Override
-                        public void mousePressed(MouseEvent e) {
-                            maybeShowPopup(e);
-                        }
-
-                        @Override
-                        public void mouseReleased(MouseEvent e) {
-                            maybeShowPopup(e);
-                        }
-                    }
-                    );
+            addMouseListener(new MouseEventHandler());
 
             if(Main.pref.getBoolean("dialog.dynamic.buttons", true)) {
                 buttonsHide = new JButton(ImageProvider.get("misc", buttonHiding != ButtonHiddingType.ALWAYS_SHOWN ? "buttonhide" : "buttonshow"));
@@ -614,6 +556,54 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
 
         public String getTitle() {
             return lblTitle.getText();
+        }
+        
+        public class DialogPopupMenu extends JPopupMenu {
+            public final JMenu buttonHidingMenu = new JMenu(tr("Side buttons"));
+            public final JRadioButtonMenuItem alwaysShown = new JRadioButtonMenuItem(new AbstractAction(tr("Always shown")) {
+                @Override public void actionPerformed(ActionEvent e) {
+                    setIsButtonHiding(ButtonHiddingType.ALWAYS_SHOWN);
+                }
+            });
+            public final JRadioButtonMenuItem dynamic = new JRadioButtonMenuItem(new AbstractAction(tr("Dynamic")) {
+                @Override public void actionPerformed(ActionEvent e) {
+                    setIsButtonHiding(ButtonHiddingType.DYNAMIC);
+                }
+            });
+            public final JRadioButtonMenuItem alwaysHidden = new JRadioButtonMenuItem(new AbstractAction(tr("Always hidden")) {
+                @Override public void actionPerformed(ActionEvent e) {
+                    setIsButtonHiding(ButtonHiddingType.ALWAYS_HIDDEN);
+                }
+            });
+            public DialogPopupMenu() {
+                alwaysShown.setSelected(buttonHiding == ButtonHiddingType.ALWAYS_SHOWN);
+                dynamic.setSelected(buttonHiding == ButtonHiddingType.DYNAMIC);
+                alwaysHidden.setSelected(buttonHiding == ButtonHiddingType.ALWAYS_HIDDEN);
+                buttonHidingMenu.add(alwaysShown);
+                buttonHidingMenu.add(dynamic);
+                buttonHidingMenu.add(alwaysHidden);
+                add(buttonHidingMenu);
+                for (javax.swing.Action action: buttonActions) {
+                    add(action);
+                }
+            }
+        }
+        
+        class MouseEventHandler extends PopupMenuLauncher {
+            public MouseEventHandler() {
+                super(new DialogPopupMenu());
+            }
+            @Override public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    if (isCollapsed) {
+                        expand();
+                        dialogsPanel.reconstruct(Action.COLLAPSED_TO_DEFAULT, ToggleDialog.this);
+                    } else {
+                        collapse();
+                        dialogsPanel.reconstruct(Action.ELEMENT_SHRINKS, null);
+                    }
+                }
+            }
         }
     }
 

src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -12,14 +12,22 @@ import java.util.HashSet;
 import java.util.List;
 
 import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.conflict.Conflict;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
+import org.openstreetmap.josm.data.validation.TestError;
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.MapFrameListener;
 import org.openstreetmap.josm.gui.MapView;
 import org.openstreetmap.josm.gui.dialogs.LayerListDialog;
+import org.openstreetmap.josm.gui.dialogs.ValidatorDialog.ValidatorBoundingXYVisitor;
 import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.tools.Shortcut;
 
@@ -35,8 +43,14 @@ public class AutoScaleAction extends JosmAction {
         marktr("selection"),
         marktr("conflict"),
         marktr("download"),
+        marktr("problem"),
         marktr("previous"),
         marktr("next")};
+    
+    private final String mode;
+
+    protected ZoomChangeAdapter zoomChangeAdapter;
+    protected MapFrameAdapter mapFrameAdapter;
 
     /**
      * Zooms the current map view to the currently selected primitives.
@@ -76,8 +90,6 @@ public class AutoScaleAction extends JosmAction {
         new AutoScaleAction(mode, false).autoScale();
     }
 
-    private final String mode;
-
     private static int getModeShortcut(String mode) {
         int shortcut = -1;
 
@@ -87,6 +99,7 @@ public class AutoScaleAction extends JosmAction {
         else if (mode.equals("selection")) { shortcut = KeyEvent.VK_3; }
         else if (mode.equals("conflict")) { shortcut = KeyEvent.VK_4; }
         else if (mode.equals("download")) { shortcut = KeyEvent.VK_5; }
+        else if (mode.equals("problem")) { shortcut = KeyEvent.VK_6; }
         else if (mode.equals("previous")) { shortcut = KeyEvent.VK_8; }
         else if (mode.equals("next")) { shortcut = KeyEvent.VK_9; }
 
@@ -94,8 +107,8 @@ public class AutoScaleAction extends JosmAction {
     }
 
     /**
-     *
-     * @param mode
+     * Constructs a new {@code AutoScaleAction}.
+     * @param mode The autoscale mode (one of {@link AutoScaleAction#MODES})
      * @param marker Used only to differentiate from default constructor
      */
     private AutoScaleAction(String mode, boolean marker) {
@@ -103,10 +116,14 @@ public class AutoScaleAction extends JosmAction {
         this.mode = mode;
     }
 
-
-    public AutoScaleAction(String mode) {
+    /**
+     * Constructs a new {@code AutoScaleAction}.
+     * @param mode The autoscale mode (one of {@link AutoScaleAction#MODES})
+     */
+    public AutoScaleAction(final String mode) {
         super(tr("Zoom to {0}", tr(mode)), "dialogs/autoscale/" + mode, tr("Zoom the view to {0}.", tr(mode)),
-                Shortcut.registerShortcut("view:zoom"+mode, tr("View: {0}", tr("Zoom to {0}", tr(mode))), getModeShortcut(mode), Shortcut.DIRECT), true);
+                Shortcut.registerShortcut("view:zoom"+mode, tr("View: {0}", tr("Zoom to {0}", tr(mode))), getModeShortcut(mode), Shortcut.DIRECT), 
+                true, null, false);
         String modeHelp = Character.toUpperCase(mode.charAt(0)) + mode.substring(1);
         putValue("help", "Action/AutoScale/" + modeHelp);
         this.mode = mode;
@@ -118,13 +135,18 @@ public class AutoScaleAction extends JosmAction {
             putValue("help", ht("/Action/ZoomToSelection"));
         } else if (mode.equals("conflict")) {
             putValue("help", ht("/Action/ZoomToConflict"));
+        } else if (mode.equals("problem")) {
+            putValue("help", ht("/Action/ZoomToProblem"));
         } else if (mode.equals("download")) {
             putValue("help", ht("/Action/ZoomToDownload"));
         } else if (mode.equals("previous")) {
             putValue("help", ht("/Action/ZoomToPrevious"));
         } else if (mode.equals("next")) {
             putValue("help", ht("/Action/ZoomToNext"));
+        } else {
+            throw new IllegalArgumentException("Unknown mode: "+mode);
         }
+        installAdapters();
     }
 
     public void autoScale()  {
@@ -170,8 +192,15 @@ public class AutoScaleAction extends JosmAction {
     }
 
     private BoundingXYVisitor getBoundingBox() {
-        BoundingXYVisitor v = new BoundingXYVisitor();
-        if (mode.equals("data")) {
+        BoundingXYVisitor v = mode.equals("problem") ? new ValidatorBoundingXYVisitor() : new BoundingXYVisitor();
+
+        if (mode.equals("problem")) {
+            TestError error = Main.map.validatorDialog.getSelectedError();
+            if (error == null) return null;
+            ((ValidatorBoundingXYVisitor) v).visit(error);
+            if (v.getBounds() == null) return null;
+            v.enlargeBoundingBox(Main.pref.getDouble("validator.zoom-enlarge-bbox", 0.0002));
+        } else if (mode.equals("data")) {
             for (Layer l : Main.map.mapView.getAllLayers()) {
                 l.visitBoundingBox(v);
             }
@@ -235,6 +264,10 @@ public class AutoScaleAction extends JosmAction {
                 // FIXME: should also check for whether a layer is selected in the layer list dialog
                 setEnabled(true);
             }
+        } else if ("conflict".equals(mode)) {
+            setEnabled(Main.map != null && Main.map.conflictDialog.getSelectedConflict() != null);
+        } else if ("problem".equals(mode)) {
+            setEnabled(Main.map != null && Main.map.validatorDialog.getSelectedError() != null);
         } else if ("previous".equals(mode)) {
             setEnabled(Main.isDisplayingMapView() && Main.map.mapView.hasZoomUndoEntries());
         } else if ("next".equals(mode)) {
@@ -257,16 +290,15 @@ public class AutoScaleAction extends JosmAction {
     @Override
     protected void installAdapters() {
         super.installAdapters();
-        // make this action listen to zoom change events
+        // make this action listen to zoom and mapframe change events
         //
-        zoomChangeAdapter = new ZoomChangeAdapter();
-        MapView.addZoomChangeListener(zoomChangeAdapter);
+        MapView.addZoomChangeListener(zoomChangeAdapter = new ZoomChangeAdapter());
+        Main.addMapFrameListener(mapFrameAdapter = new MapFrameAdapter());
         initEnabledState();
     }
 
     /**
-     * Adapter for selection change events
-     *
+     * Adapter for zoom change events
      */
     private class ZoomChangeAdapter implements MapView.ZoomChangeListener {
         public void zoomChanged() {
@@ -274,5 +306,43 @@ public class AutoScaleAction extends JosmAction {
         }
     }
 
-    private ZoomChangeAdapter zoomChangeAdapter;
+    /**
+     * Adapter for MapFrame change events
+     */
+    private class MapFrameAdapter implements MapFrameListener {
+        private ListSelectionListener conflictSelectionListener;
+        private TreeSelectionListener validatorSelectionListener;
+
+        public MapFrameAdapter() {
+            if (mode.equals("conflict")) {
+                conflictSelectionListener = new ListSelectionListener() {
+                    @Override public void valueChanged(ListSelectionEvent e) {
+                        updateEnabledState();
+                    }
+                };
+            } else if (mode.equals("problem")) {
+                validatorSelectionListener = new TreeSelectionListener() {
+                    @Override public void valueChanged(TreeSelectionEvent e) {
+                        updateEnabledState();
+                    }
+                };
+            }
+        }
+
+        @Override public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+            if (conflictSelectionListener != null) {
+                if (newFrame != null) {
+                    newFrame.conflictDialog.addListSelectionListener(conflictSelectionListener);
+                } else if (oldFrame != null) {
+                    oldFrame.conflictDialog.removeListSelectionListener(conflictSelectionListener);
+                }
+            } else if (validatorSelectionListener != null) {
+                if (newFrame != null) {
+                    newFrame.validatorDialog.addTreeSelectionListener(validatorSelectionListener);
+                } else if (oldFrame != null) {
+                    oldFrame.validatorDialog.removeTreeSelectionListener(validatorSelectionListener);
+                }
+            }
+        }
+    }
 }

src/org/openstreetmap/josm/gui/MainMenu.java
@@ -8,6 +8,8 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Component;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
@@ -165,6 +167,7 @@ public class MainMenu extends JMenuBar {
     public final InfoWebAction infoweb = new InfoWebAction();
     public final HistoryInfoAction historyinfo = new HistoryInfoAction();
     public final HistoryInfoWebAction historyinfoweb = new HistoryInfoWebAction();
+    public final Map<String, AutoScaleAction> autoScaleActions = new HashMap<String, AutoScaleAction>();
 
     /* Tools menu */
     public final JosmAction splitWay = new SplitWayAction();
@@ -493,7 +496,8 @@ public class MainMenu extends JMenuBar {
         add(viewMenu, new ZoomOutAction());
         viewMenu.addSeparator();
         for (String mode : AutoScaleAction.MODES) {
-            JosmAction autoScaleAction = new AutoScaleAction(mode);
+            AutoScaleAction autoScaleAction = new AutoScaleAction(mode);
+            autoScaleActions.put(mode, autoScaleAction);
             add(viewMenu, autoScaleAction);
         }
 

src/org/openstreetmap/josm/gui/dialogs/ChangesetDialog.java
@@ -106,10 +106,6 @@ public class ChangesetDialog extends ToggleDialog{
         DblClickHandler dblClickHandler = new DblClickHandler();
         lstInSelection.addMouseListener(dblClickHandler);
         lstInActiveDataLayer.addMouseListener(dblClickHandler);
-
-        ChangesetPopupMenuLauncher popupMenuLauncher = new ChangesetPopupMenuLauncher();
-        lstInSelection.addMouseListener(popupMenuLauncher);
-        lstInActiveDataLayer.addMouseListener(popupMenuLauncher);
     }
 
     protected void registerAsListener() {
@@ -210,6 +206,10 @@ public class ChangesetDialog extends ToggleDialog{
         
         popupMenu = new ChangesetDialogPopup(lstInActiveDataLayer, lstInSelection);
 
+        PopupMenuLauncher popupMenuLauncher = new PopupMenuLauncher(popupMenu);
+        lstInSelection.addMouseListener(popupMenuLauncher);
+        lstInActiveDataLayer.addMouseListener(popupMenuLauncher);
+        
         createLayout(pnl, false, Arrays.asList(new SideButton[] {
             new SideButton(selectObjectsAction, false),
             new SideButton(readChangesetAction, false),
@@ -518,20 +518,6 @@ public class ChangesetDialog extends ToggleDialog{
         }
 
         public void valueChanged(ListSelectionEvent e) {
-        }
-    }
-
-    class ChangesetPopupMenuLauncher extends PopupMenuLauncher {
-        @Override
-        public void launch(MouseEvent evt) {
-            JList lst = getCurrentChangesetList();
-            if (lst.getSelectedIndices().length == 0) {
-                int idx = lst.locationToIndex(evt.getPoint());
-                if (idx >=0) {
-                    lst.getSelectionModel().addSelectionInterval(idx, idx);
-                }
-            }
-            popupMenu.show(lst, evt.getX(), evt.getY());
         }
     }
 

src/org/openstreetmap/josm/gui/dialogs/CommandStackDialog.java
@@ -6,7 +6,6 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -80,7 +79,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
         super(tr("Command Stack"), "commandstack", tr("Open a list of all commands (undo buffer)."),
                 Shortcut.registerShortcut("subwindow:commandstack", tr("Toggle: {0}",
                 tr("Command Stack")), KeyEvent.VK_O, Shortcut.ALT_SHIFT), 100, true);
-        undoTree.addMouseListener(new PopupMenuHandler());
+        undoTree.addMouseListener(new MouseEventHandler());
         undoTree.setRootVisible(false);
         undoTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         undoTree.setShowsRootHandles(true);
@@ -90,7 +89,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
         undoTree.getSelectionModel().addTreeSelectionListener(undoSelectionListener);
         InputMapUtils.unassignCtrlShiftUpDown(undoTree, JComponent.WHEN_FOCUSED);
         
-        redoTree.addMouseListener(new PopupMenuHandler());
+        redoTree.addMouseListener(new MouseEventHandler());
         redoTree.setRootVisible(false);
         redoTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         redoTree.setShowsRootHandles(true);
@@ -381,7 +380,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
     /**
      * undo / redo switch to reduce duplicate code
      */
-    protected enum UndoRedoType {UNDO, REDO};
+    protected enum UndoRedoType {UNDO, REDO}
 
     /**
      * Action to undo or redo all commands up to (and including) the seleced item.
@@ -443,35 +442,19 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
         }
     }
 
-    class PopupMenuHandler extends PopupMenuLauncher {
+    class MouseEventHandler extends PopupMenuLauncher {
+
+        public MouseEventHandler() {
+            super(new CommandStackPopup());
+        }
         
         @Override
         public void mouseClicked(MouseEvent evt) {
-            super.mouseClicked(evt);
-            if (evt.getButton() == MouseEvent.BUTTON1 && evt.getClickCount()>1) {
+            if (isDoubleClick(evt)) {
                 selectAndZoomAction.actionPerformed(null);
             }
         }
-        
-        @Override
-        public void launch(MouseEvent evt) {
-            Point p = evt.getPoint();
-            JTree tree = (JTree) evt.getSource();
-            int row = tree.getRowForLocation(p.x, p.y);
-            if (row != -1) {
-                TreePath path = tree.getPathForLocation(p.x, p.y);
-                // right click on unselected element -> select it first
-                if (!tree.isPathSelected(path)) {
-                    tree.setSelectionPath(path);
-                }
-                TreePath[] selPaths = tree.getSelectionPaths();
-
-                CommandStackPopup menu = new CommandStackPopup();
-                menu.show(tree, p.x, p.y-3);
-            }
-        }
     }
-
     
     private class CommandStackPopup extends JPopupMenu {
         public CommandStackPopup(){

commit 0b4f2a4d4283758f670bfb158a19fd6785172c4a

fix broken popup menus in toggle dialog title bars (broken by r5958)

src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
@@ -477,8 +477,6 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
             lblTitle_weak.setMinimumSize(new Dimension(0,20));
             add(lblTitle_weak, GBC.std().fill(GBC.HORIZONTAL));
 
-            addMouseListener(new MouseEventHandler());
-
             if(Main.pref.getBoolean("dialog.dynamic.buttons", true)) {
                 buttonsHide = new JButton(ImageProvider.get("misc", buttonHiding != ButtonHiddingType.ALWAYS_SHOWN ? "buttonhide" : "buttonshow"));
                 buttonsHide.setToolTipText(tr("Toggle dynamic buttons"));
@@ -587,6 +585,10 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
                     add(action);
                 }
             }
+        }
+        
+        public void registerMouseListener() {
+            addMouseListener(new MouseEventHandler());
         }
         
         class MouseEventHandler extends PopupMenuLauncher {
@@ -859,6 +861,10 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
         } else if (buttonsHide != null) {
             buttonsHide.setVisible(false);
         }
+        
+        // Register title bar mouse listener only after buttonActions has been initialized to have a complete popup menu
+        titleBar.registerMouseListener();
+        
         return data;
     }
 