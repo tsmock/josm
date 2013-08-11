tsmock/josm

commit 42316b841a8b32dd344cfed95cc0b587abc728de

fix #8735 - Presets: add new `checkgroup` item to specify group of checkboxes on multiple columns
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6114 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/tagging/TaggingPresetReader.java
@@ -11,9 +11,10 @@ import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import javax.swing.JOptionPane;
-import org.xml.sax.SAXException;
 
+import javax.swing.JOptionPane;
+
+import org.xml.sax.SAXException;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.preferences.SourceEntry;
 import org.openstreetmap.josm.gui.preferences.map.TaggingPresetPreference;
@@ -57,6 +58,7 @@ public final class TaggingPresetReader {
         parser.mapOnStart("optional", TaggingPresetItems.Optional.class);
         parser.mapOnStart("roles", TaggingPresetItems.Roles.class);
         parser.map("role", TaggingPresetItems.Role.class);
+        parser.map("checkgroup", TaggingPresetItems.CheckGroup.class);
         parser.map("check", TaggingPresetItems.Check.class);
         parser.map("combo", TaggingPresetItems.Combo.class);
         parser.map("multiselect", TaggingPresetItems.MultiSelect.class);
@@ -68,6 +70,7 @@ public final class TaggingPresetReader {
         LinkedList<TaggingPreset> all = new LinkedList<TaggingPreset>();
         TaggingPresetMenu lastmenu = null;
         TaggingPresetItems.Roles lastrole = null;
+        final List<TaggingPresetItems.Check> checks = new LinkedList<TaggingPresetItems.Check>();
         List<TaggingPresetItems.PresetListEntry> listEntries = new LinkedList<TaggingPresetItems.PresetListEntry>();
 
         if (validate) {
@@ -112,9 +115,19 @@ public final class TaggingPresetReader {
                         if (lastrole == null)
                             throw new SAXException(tr("Preset role element without parent"));
                         lastrole.roles.add((TaggingPresetItems.Role) o);
+                    } else if (o instanceof TaggingPresetItems.Check) {
+                        checks.add((TaggingPresetItems.Check) o);
                     } else if (o instanceof TaggingPresetItems.PresetListEntry) {
                         listEntries.add((TaggingPresetItems.PresetListEntry) o);
+                    } else if (o instanceof TaggingPresetItems.CheckGroup) {
+                        all.getLast().data.add((TaggingPresetItem) o);
+                        ((TaggingPresetItems.CheckGroup) o).checks.addAll(checks);
+                        checks.clear();
                     } else {
+                        if (!checks.isEmpty()) {
+                            all.getLast().data.addAll(checks);
+                            checks.clear();
+                        }
                         all.getLast().data.add((TaggingPresetItem) o);
                         if (o instanceof TaggingPresetItems.ComboMultiSelect) {
                             ((TaggingPresetItems.ComboMultiSelect) o).addListEntries(listEntries);

data/defaultpresets.xml
@@ -88,6 +88,9 @@ multiselect: list of values from which zero or more can be selected
   use_last_as_default: true/false/force (default is "false")
   match: none/key/key!/keyvalue (default is "none", see below for more information)
 
+checkgroup: group of checks
+  columns: number of columns
+
 check: checkbox
   key: key to set
   text: fixed label to display
@@ -2426,22 +2429,24 @@ Note that for a match, at least one positive and no negative is required.
                 <combo key="opening_hours" text="Opening Hours" values="24/7,Mo-Fr 08:30-20:00,Tu-Su 08:00-15:00;Sa 08:00-12:00" />
                 <combo key="shop" text="With shop" values="yes,convenience,kiosk,no" />
                 <label text="Fuel types:" />
-                <check key="fuel:diesel" text="Diesel" default="off" />
-                <check key="fuel:biodiesel" text="Bio Diesel" default="off" />
-                <check key="fuel:GTL_diesel" text="Diesel (Gas To Liquid - ultimate diesel)" default="off" />
-                <check key="fuel:HGV_diesel" text="Diesel for Heavy Good Vehicles" default="off" />
-                <check key="fuel:octane_80" text="Octane 80" default="off" />
-                <check key="fuel:octane_91" text="Octane 91" default="off" />
-                <check key="fuel:octane_92" text="Octane 92" default="off" />
-                <check key="fuel:octane_95" text="Octane 95" default="off" />
-                <check key="fuel:octane_98" text="Octane 98" default="off" />
-                <check key="fuel:octane_100" text="Octane 100" default="off" />
-                <check key="fuel:e10" text="E10 (10% Ethanol mix)" default="off" />
-                <check key="fuel:e85" text="E85 (85% Ethanol mix)" default="off" />
-                <check key="fuel:lpg" text="LPG (Liquefied petroleum gas)" default="off" />
-                <check key="fuel:cng" text="CNG (Compressed Natural Gas)" default="off" />
-                <check key="fuel:1_25" text="1/25 mix (mofa/moped)" default="off" />
-                <check key="fuel:1_50" text="1/50 mix (mofa/moped)" default="off" />
+                <checkgroup columns="2">
+                    <check key="fuel:diesel" text="Diesel" default="off" />
+                    <check key="fuel:biodiesel" text="Bio Diesel" default="off" />
+                    <check key="fuel:GTL_diesel" text="Diesel (Gas To Liquid - ultimate diesel)" default="off" />
+                    <check key="fuel:HGV_diesel" text="Diesel for Heavy Good Vehicles" default="off" />
+                    <check key="fuel:octane_80" text="Octane 80" default="off" />
+                    <check key="fuel:octane_91" text="Octane 91" default="off" />
+                    <check key="fuel:octane_92" text="Octane 92" default="off" />
+                    <check key="fuel:octane_95" text="Octane 95" default="off" />
+                    <check key="fuel:octane_98" text="Octane 98" default="off" />
+                    <check key="fuel:octane_100" text="Octane 100" default="off" />
+                    <check key="fuel:e10" text="E10 (10% Ethanol mix)" default="off" />
+                    <check key="fuel:e85" text="E85 (85% Ethanol mix)" default="off" />
+                    <check key="fuel:lpg" text="LPG (Liquefied petroleum gas)" default="off" />
+                    <check key="fuel:cng" text="CNG (Compressed Natural Gas)" default="off" />
+                    <check key="fuel:1_25" text="1/25 mix (mofa/moped)" default="off" />
+                    <check key="fuel:1_50" text="1/50 mix (mofa/moped)" default="off" />
+                </checkgroup>
             </optional>
         </item>
         <item name="Charging Station" icon="styles/standard/accommodation/camping/hookup.png" type="node">

data/tagging-preset.xsd
@@ -80,6 +80,7 @@
 			<element name="text" type="tns:text" />
 			<element name="combo" type="tns:combo" />
 			<element name="multiselect" type="tns:multiselect" />
+            <element name="checkgroup" type="tns:checkgroup" />
 			<element name="check" type="tns:check" />
 		</choice>
 	</group>
@@ -187,6 +188,16 @@
 		<attribute name="display-values" use="prohibited" />
 		<anyAttribute processContents="skip" />
 	</complexType>
+
+    <complexType name="checkgroup">
+        <sequence>
+            <choice minOccurs="1" maxOccurs="unbounded">
+                <element name="check" type="tns:check" />
+            </choice>
+        </sequence>
+        <attribute name="columns" type="positiveInteger" />
+        <anyAttribute processContents="skip" />
+    </complexType>
 
 	<complexType name="check">
 		<attribute name="key" type="string" use="required" />

src/org/openstreetmap/josm/gui/tagging/TaggingPresetItems.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -23,6 +24,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
+
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -34,8 +36,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 import javax.swing.ListCellRenderer;
 import javax.swing.ListModel;
-import org.xml.sax.SAXException;
 
+import org.xml.sax.SAXException;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.search.SearchCompiler;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
@@ -314,6 +316,16 @@ public final class TaggingPresetItems {
         @Override
         public void addCommands(List<Tag> changedTags) {
         }
+
+        @Override
+        public String toString() {
+            return "Label ["
+                    + (text != null ? "text=" + text + ", " : "")
+                    + (text_context != null ? "text_context=" + text_context
+                            + ", " : "")
+                    + (locale_text != null ? "locale_text=" + locale_text : "")
+                    + "]";
+        }
     }
 
     public static class Link extends TaggingPresetItem {
@@ -390,6 +402,11 @@ public final class TaggingPresetItems {
         @Override
         public void addCommands(List<Tag> changedTags) {
         }
+
+        @Override
+        public String toString() {
+            return "Optional";
+        }
     }
 
     public static class Space extends TaggingPresetItem {
@@ -402,6 +419,11 @@ public final class TaggingPresetItems {
 
         @Override
         public void addCommands(List<Tag> changedTags) {
+        }
+
+        @Override
+        public String toString() {
+            return "Space";
         }
     }
 
@@ -645,6 +667,46 @@ public final class TaggingPresetItems {
         }
     }
 
+    /**
+     * A group of {@link Check}s.
+     * @since 6114
+     */
+    public static class CheckGroup extends TaggingPresetItem {
+        
+        /**
+         * Number of columns (positive integer)
+         */
+        public String columns;
+        
+        /**
+         * List of checkboxes
+         */
+        public final List<Check> checks = new LinkedList<Check>();
+
+        @Override
+        boolean addToPanel(JPanel p, Collection<OsmPrimitive> sel) {
+            Integer cols = new Integer(columns);
+            int rows = (int) Math.ceil((double)checks.size()/cols.doubleValue());
+            JPanel panel = new JPanel(new GridLayout(rows, cols));
+            
+            for (Check check : checks) {
+                check.addToPanel(panel, sel);
+            }
+            
+            p.add(panel, GBC.eol());
+            return false;
+        }
+
+        @Override
+        void addCommands(List<Tag> changedTags) {
+        }
+
+        @Override
+        public String toString() {
+            return "CheckGroup [columns=" + columns + "]";
+        }
+    }
+
     public static class Check extends KeyedItem {
 
         public String locale_text;
@@ -733,6 +795,18 @@ public final class TaggingPresetItems {
         @Override
         public Collection<String> getValues() {
             return Arrays.asList(value_on, value_off);
+        }
+
+        @Override
+        public String toString() {
+            return "Check ["
+                    + (locale_text != null ? "locale_text=" + locale_text + ", " : "")
+                    + (value_on != null ? "value_on=" + value_on + ", " : "")
+                    + (value_off != null ? "value_off=" + value_off + ", " : "")
+                    + "default_=" + default_ + ", "
+                    + (check != null ? "check=" + check + ", " : "")
+                    + (initialState != null ? "initialState=" + initialState
+                            + ", " : "") + "def=" + def + "]";
         }
     }
 

commit 0e8df9e98760f2bd2704ad26c097df6f61544b03

fix #8954 - checkboxes not displayed in presets if they are the last file element (regression from r6114, patch by AlfonZ)

src/org/openstreetmap/josm/gui/tagging/TaggingPresetReader.java
@@ -147,6 +147,10 @@ public final class TaggingPresetReader {
                     throw new SAXException(tr("Preset sub element without parent"));
             }
         }
+        if (!all.isEmpty() && !checks.isEmpty()) {
+            all.getLast().data.addAll(checks);
+            checks.clear();
+        }
         return all;
     }
     