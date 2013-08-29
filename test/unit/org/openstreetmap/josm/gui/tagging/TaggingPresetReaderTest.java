tsmock/josm

commit 28bcd5e172903f1df547aac6a032afe62153440f

fix #9010 - presets: add some context for `skating` used in different presets with distinct translations
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6195 0c6e7542-c601-0410-84e7-c038aed88b3b

data/defaultpresets.xml
@@ -4276,7 +4276,7 @@ Note that for a match, at least one positive and no negative is required.
             <space />
             <label text="select sport:" />
             <space />
-            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,swimming,table_tennis,tennis" />
+            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,swimming,table_tennis,tennis" values_context="sport" />
         </item>
         <item name="Sports Centre" icon="presets/sports_centre.png" type="node,closedway">
             <label text="Edit Sports Centre" />
@@ -4285,7 +4285,7 @@ Note that for a match, at least one positive and no negative is required.
             <space />
             <label text="select sport:" />
             <space />
-            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,swimming,table_tennis,tennis" />
+            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,swimming,table_tennis,tennis" values_context="sport" />
         </item>
         <item name="Pitch" icon="presets/pitch.png" type="node,closedway">
             <label text="Edit Pitch" />
@@ -4294,7 +4294,7 @@ Note that for a match, at least one positive and no negative is required.
             <space />
             <label text="select sport:" />
             <space />
-            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,table_tennis,tennis" />
+            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,table_tennis,tennis" values_context="sport" />
         </item>
         <item name="Racetrack" icon="presets/stadium.png" type="node,way,closedway,relation">
             <label text="Edit Racetrack" />
@@ -4303,7 +4303,7 @@ Note that for a match, at least one positive and no negative is required.
             <space />
             <label text="select sport:" />
             <space />
-            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,table_tennis,tennis" />
+            <combo key="sport" text="sport" values="multi,archery,athletics,american_football,australian_football,baseball,basketball,boules,bowls,canadian_football,canoe,climbing,cricket,cricket_nets,croquet,cycling,dog_racing,equestrian,gaelic_games,golf,gymnastics,hockey,horse_racing,motor,pelota,racquet,rugby_league,rugby_union,shooting,skateboard,skating,skiing,soccer,table_tennis,tennis" values_context="sport" />
         </item>
         <item name="Golf Course" icon="presets/golf.png" type="node,closedway">
             <label text="Edit Golf Course" />
@@ -4410,7 +4410,7 @@ Note that for a match, at least one positive and no negative is required.
             <link href="http://wiki.openstreetmap.org/wiki/Tag:sport=skating"
                   uk.href="http://wiki.openstreetmap.org/wiki/Uk:Tag:sport=skating" />
             <label text="Edit Skating" />
-            <key key="sport" value="skating" />
+            <key key="sport" value="skating" values_context="sport" />
             <text key="name" text="Name" />
             <combo key="leisure" text="type" values="ice_rink,pitch,sports_centre,stadium" default="ice_rink" />
         </item>
@@ -4437,7 +4437,7 @@ Note that for a match, at least one positive and no negative is required.
             <text key="name" text="Name" />
             <combo key="piste:type" text="Piste type" values="downhill,nordic,skitour,snow_park" />
             <combo key="piste:difficulty" text="Difficulty" values="novice,easy,intermediate,advanced,expert,freeride" values_context="piste:difficulty" />
-            <combo key="piste:grooming" text="Grooming" values="classic,mogul,skating,classic;skating,scooter,backcountry" />
+            <combo key="piste:grooming" text="Grooming" values="classic,mogul,skating,classic;skating,scooter,backcountry" values_context="piste:grooming" />
         </item>
         <item name="Shooting" icon="presets/range.png" type="node,closedway">
             <link href="http://wiki.openstreetmap.org/wiki/Tag:sport=shooting"

commit 1beb358c01bcae525afbd634af264a2bc74993b4

fix #9027 - fix error in internal preset introduced in r6195 (patch by skyper) + validation check in build.xml and junit test (patch by AlfonZ)

build.xml
@@ -61,7 +61,15 @@ Is-Local-Build: true
 Build-Date: ${build.tstamp}
 </echo>
     </target>
-    <target name="dist" depends="compile,create-revision">
+    <target name="check-schemas">
+        <schemavalidate file="data/defaultpresets.xml" >
+            <schema namespace="http://josm.openstreetmap.de/tagging-preset-1.0" file="data/tagging-preset.xsd" />
+        </schemavalidate>
+        <schemavalidate file="styles/standard/elemstyles.xml" >
+            <schema namespace="http://josm.openstreetmap.de/mappaint-style-1.0" file="data/mappaint-style.xsd" />
+        </schemavalidate>
+    </target>
+    <target name="dist" depends="compile,create-revision,check-schemas">
         <exec append="false" output="REVISION" executable="svn" failifexecutionfails="false">
             <env key="LANG" value="C"/>
             <arg value="info"/>

data/defaultpresets.xml
@@ -4420,8 +4420,8 @@ Note that for a match, at least one positive and no negative is required.
         <item name="Skating" icon="presets/skating.png" type="node,closedway">
             <link href="http://wiki.openstreetmap.org/wiki/Tag:sport=skating"
                   uk.href="http://wiki.openstreetmap.org/wiki/Uk:Tag:sport=skating" />
-            <label text="Edit Skating" />
-            <key key="sport" value="skating" values_context="sport" />
+            <label text="Edit Skating" text_context="sport" />
+            <key key="sport" value="skating" />
             <text key="name" text="Name" />
             <combo key="leisure" text="type" values="ice_rink,pitch,sports_centre,stadium" default="ice_rink" />
         </item>

test/unit/org/openstreetmap/josm/gui/tagging/TaggingPresetReaderTest.java
@@ -63,4 +63,15 @@ public class TaggingPresetReaderTest {
         Assert.assertTrue("Entry is not checkbox", item instanceof TaggingPresetItems.Check);
     }
 
+    /**
+     * Validate internal presets
+     * See #9027
+     */
+    @Test
+    public void readDefaulPresets() throws SAXException, IOException {
+        String presetfile = "resource://data/defaultpresets.xml";
+        final Collection<TaggingPreset> presets = TaggingPresetReader.readAll(presetfile, true);
+        Assert.assertTrue("Default presets are empty", presets.size()>0);
+    }
+    
 }