tsmock/josm

commit 54c86242b43f154bd9caa93f4ea80a9f9a68bfdc

Ask user to change proxy settings when proxy errors occur at startup (useful when a laptop is often used between two locations with different proxy settings)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6523 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/preferences/ServerAccessPreference.java
@@ -7,19 +7,24 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
 import org.openstreetmap.josm.gui.help.HelpUtil;
-import org.openstreetmap.josm.gui.preferences.server.AuthenticationPreferencesPanel;
 import org.openstreetmap.josm.gui.preferences.server.OsmApiUrlInputPanel;
-import org.openstreetmap.josm.gui.preferences.server.ProxyPreferencesPanel;
 import org.openstreetmap.josm.gui.widgets.VerticallyScrollablePanel;
 
+/**
+ * Connection preferences, including authentication and proxy sub-preferences.
+ */
 public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
 
+    /**
+     * Factory used to create a new {@code ServerAccessPreference}.
+     */
     public static class Factory implements PreferenceSettingFactory {
         @Override
         public PreferenceSetting createPreferenceSetting() {
@@ -31,13 +36,8 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
         super("connection", tr("Connection Settings"), tr("Connection Settings for the OSM server."), false, new JTabbedPane());
     }
 
-    private OsmApiUrlInputPanel pnlApiUrlPreferences;
-
     /** indicates whether to use the default OSM URL or not */
-    /** panel for configuring authentication preferences */
-    private AuthenticationPreferencesPanel pnlAuthPreferences;
-    /** panel for configuring proxy preferences */
-    private ProxyPreferencesPanel pnlProxyPreferences;
+    private OsmApiUrlInputPanel pnlApiUrlPreferences;
 
     /**
      * Embeds a vertically scrollable panel in a {@link JScrollPane}
@@ -58,19 +58,7 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
      */
     protected JPanel buildTabbedServerPreferences() {
         JPanel pnl = new JPanel(new BorderLayout());
-
-        JTabbedPane tpServerPreferences = getTabPane();
-        pnlAuthPreferences = new AuthenticationPreferencesPanel();
-        tpServerPreferences.add(wrapVerticallyScrollablePanel(pnlAuthPreferences));
-        pnlProxyPreferences = new ProxyPreferencesPanel();
-        tpServerPreferences.add(wrapVerticallyScrollablePanel(pnlProxyPreferences));
-
-        tpServerPreferences.setTitleAt(0, tr("Authentication"));
-        tpServerPreferences.setTitleAt(1, tr("Proxy settings"));
-        tpServerPreferences.setToolTipTextAt(0, tr("Configure your identity and how to authenticate at the OSM server"));
-        tpServerPreferences.setToolTipTextAt(1, tr("Configure whether to use a proxy server"));
-
-        pnl.add(tpServerPreferences, BorderLayout.CENTER);
+        pnl.add(getTabPane(), BorderLayout.CENTER);
         return pnl;
     }
 
@@ -98,12 +86,17 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
         gc.insets = new Insets(10,0,3,3);
         pnl.add(buildTabbedServerPreferences(), gc);
 
-        // let the AuthPreferencesPanel know when the API URL changes
-        //
-        pnlApiUrlPreferences.addPropertyChangeListener(pnlAuthPreferences);
-
         HelpUtil.setHelpContext(pnl, HelpUtil.ht("/Preferences/Connection"));
         return pnl;
+    }
+
+    /**
+     * Adds a listener that will be notified of API URL change.
+     * @param listener the listener
+     * @since 6523
+     */
+    public final void addApiUrlChangeListener(PropertyChangeListener listener) {
+        pnlApiUrlPreferences.addPropertyChangeListener(listener);
     }
 
     @Override
@@ -123,8 +116,6 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
      */
     public void initFromPreferences() {
         pnlApiUrlPreferences.initFromPreferences();
-        pnlAuthPreferences.initFromPreferences();
-        pnlProxyPreferences.initFromPreferences();
     }
 
     /**
@@ -133,8 +124,6 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
     @Override
     public boolean ok() {
         pnlApiUrlPreferences.saveToPreferences();
-        pnlAuthPreferences.saveToPreferences();
-        pnlProxyPreferences.saveToPreferences();
         return false;
     }
 }

src/org/openstreetmap/josm/gui/preferences/server/AuthenticationPreference.java
@@ -1,0 +1,57 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.preferences.server;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.gui.preferences.PreferenceSettingFactory;
+import org.openstreetmap.josm.gui.preferences.PreferenceTabbedPane;
+import org.openstreetmap.josm.gui.preferences.SubPreferenceSetting;
+import org.openstreetmap.josm.gui.preferences.TabPreferenceSetting;
+
+/**
+ * Authentication sub-preferences in server preferences.
+ * @since 6523
+ */
+public class AuthenticationPreference implements SubPreferenceSetting {
+
+    /**
+     * Factory used to create a new {@code AuthenticationPreference}.
+     */
+    public static class Factory implements PreferenceSettingFactory {
+        @Override
+        public PreferenceSetting createPreferenceSetting() {
+            return new AuthenticationPreference();
+        }
+    }
+
+    AuthenticationPreferencesPanel pnlAuthPreferences;
+
+    private AuthenticationPreference() {
+        super();
+    }
+
+    @Override
+    public void addGui(PreferenceTabbedPane gui) {
+        pnlAuthPreferences = new AuthenticationPreferencesPanel();
+        gui.getServerPreference().addApiUrlChangeListener(pnlAuthPreferences);
+        gui.getServerPreference().addSubTab(this, tr("Authentication"), pnlAuthPreferences,
+                tr("Configure your identity and how to authenticate at the OSM server"));
+    }
+
+    @Override
+    public boolean ok() {
+        pnlAuthPreferences.saveToPreferences();
+        return false;
+    }
+
+    @Override
+    public boolean isExpert() {
+        return false;
+    }
+
+    @Override
+    public TabPreferenceSetting getTabPreferenceSetting(PreferenceTabbedPane gui) {
+        return gui.getServerPreference();
+    }
+}

src/org/openstreetmap/josm/gui/preferences/server/ProxyPreference.java
@@ -1,0 +1,56 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.preferences.server;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.gui.preferences.PreferenceSettingFactory;
+import org.openstreetmap.josm.gui.preferences.PreferenceTabbedPane;
+import org.openstreetmap.josm.gui.preferences.SubPreferenceSetting;
+import org.openstreetmap.josm.gui.preferences.TabPreferenceSetting;
+
+/**
+ * Proxy sub-preferences in server preferences.
+ * @since 6523
+ */
+public class ProxyPreference implements SubPreferenceSetting {
+
+    /**
+     * Factory used to create a new {@code ProxyPreference}.
+     */
+    public static class Factory implements PreferenceSettingFactory {
+        @Override
+        public PreferenceSetting createPreferenceSetting() {
+            return new ProxyPreference();
+        }
+    }
+
+    ProxyPreferencesPanel pnlProxyPreferences;
+
+    private ProxyPreference() {
+        super();
+    }
+
+    @Override
+    public void addGui(PreferenceTabbedPane gui) {
+        pnlProxyPreferences = new ProxyPreferencesPanel();
+        gui.getServerPreference().addSubTab(this, tr("Proxy settings"), pnlProxyPreferences,
+                tr("Configure whether to use a proxy server"));
+    }
+
+    @Override
+    public boolean ok() {
+        pnlProxyPreferences.saveToPreferences();
+        return false;
+    }
+
+    @Override
+    public boolean isExpert() {
+        return false;
+    }
+
+    @Override
+    public TabPreferenceSetting getTabPreferenceSetting(PreferenceTabbedPane gui) {
+        return gui.getServerPreference();
+    }
+}

src/org/openstreetmap/josm/Main.java
@@ -408,6 +408,9 @@ abstract public class Main {
         initListener = listener;
     }
 
+    /**
+     * Constructs new {@code Main} object. A lot of global variables are initialized here.
+     */
     public Main() {
         main = this;
         isOpenjdk = System.getProperty("java.vm.name").toUpperCase().indexOf("OPENJDK") != -1;

src/org/openstreetmap/josm/gui/MainApplication.java
@@ -6,6 +6,7 @@ import static org.openstreetmap.josm.tools.I18n.trn;
 import gnu.getopt.Getopt;
 import gnu.getopt.LongOpt;
 
+import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
@@ -27,17 +28,19 @@ import java.util.List;
 import java.util.Map;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.RepaintManager;
 import javax.swing.SwingUtilities;
 
 import org.jdesktop.swinghelper.debug.CheckThreadViolationRepaintManager;
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.actions.PreferencesAction;
 import org.openstreetmap.josm.data.AutosaveTask;
 import org.openstreetmap.josm.data.CustomConfigurator;
-import org.openstreetmap.josm.data.Preferences;
 import org.openstreetmap.josm.data.Version;
 import org.openstreetmap.josm.gui.download.DownloadDialog;
 import org.openstreetmap.josm.gui.preferences.server.OAuthAccessTokenHolder;
+import org.openstreetmap.josm.gui.preferences.server.ProxyPreference;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.gui.util.GuiHelper;
 import org.openstreetmap.josm.io.DefaultProxySelector;
@@ -339,7 +342,8 @@ public class MainApplication extends Main {
 
         DefaultAuthenticator.createInstance();
         Authenticator.setDefault(DefaultAuthenticator.getInstance());
-        ProxySelector.setDefault(new DefaultProxySelector(ProxySelector.getDefault()));
+        DefaultProxySelector proxySelector = new DefaultProxySelector(ProxySelector.getDefault());
+        ProxySelector.setDefault(proxySelector);
         OAuthAccessTokenHolder.getInstance().init(Main.pref, CredentialsManager.getInstance());
 
         // asking for help? show help and exit
@@ -406,40 +410,7 @@ public class MainApplication extends Main {
             main.menu.fullscreenToggleAction.initial();
         }
 
-        final Map<Option, Collection<String>> args_final = args;
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                if (AutosaveTask.PROP_AUTOSAVE_ENABLED.get()) {
-                    AutosaveTask autosaveTask = new AutosaveTask();
-                    List<File> unsavedLayerFiles = autosaveTask.getUnsavedLayersFiles();
-                    if (!unsavedLayerFiles.isEmpty()) {
-                        ExtendedDialog dialog = new ExtendedDialog(
-                                Main.parent,
-                                tr("Unsaved osm data"),
-                                new String[] {tr("Restore"), tr("Cancel"), tr("Discard")}
-                                );
-                        dialog.setContent(
-                                trn("JOSM found {0} unsaved osm data layer. ",
-                                        "JOSM found {0} unsaved osm data layers. ", unsavedLayerFiles.size(), unsavedLayerFiles.size()) +
-                                        tr("It looks like JOSM crashed last time. Would you like to restore the data?"));
-                        dialog.setButtonIcons(new String[] {"ok", "cancel", "dialogs/delete"});
-                        int selection = dialog.showDialog().getValue();
-                        if (selection == 1) {
-                            autosaveTask.recoverUnsavedLayers();
-                        } else if (selection == 3) {
-                            autosaveTask.dicardUnsavedLayers();
-                        }
-                    }
-                    autosaveTask.schedule();
-                }
-
-                postConstructorProcessCmdLine(args_final);
-
-                DownloadDialog.autostartIfNeeded();
-            }
-        });
+        SwingUtilities.invokeLater(new GuiFinalizationWorker(args, proxySelector));
 
         if (RemoteControl.PROP_REMOTECONTROL_ENABLED.get()) {
             RemoteControl.start();
@@ -453,6 +424,88 @@ public class MainApplication extends Main {
             // Repaint manager is registered so late for a reason - there is lots of violation during startup process but they don't seem to break anything and are difficult to fix
             info("Enabled EDT checker, wrongful access to gui from non EDT thread will be printed to console");
             RepaintManager.setCurrentManager(new CheckThreadViolationRepaintManager());
+        }
+    }
+
+    private static class GuiFinalizationWorker implements Runnable {
+
+        private final Map<Option, Collection<String>> args;
+        private final DefaultProxySelector proxySelector;
+
+        public GuiFinalizationWorker(Map<Option, Collection<String>> args, DefaultProxySelector proxySelector) {
+            this.args = args;
+            this.proxySelector = proxySelector;
+        }
+
+        @Override
+        public void run() {
+
+            // Handle proxy errors early to inform user he should change settings to be able to use JOSM correctly
+            handleProxyErrors();
+
+            // Restore autosave layers after crash and start autosave thread
+            handleAutosave();
+
+            // Handle command line instructions
+            postConstructorProcessCmdLine(args);
+
+            // Show download dialog if autostart is enabled
+            DownloadDialog.autostartIfNeeded();
+        }
+
+        private void handleAutosave() {
+            if (AutosaveTask.PROP_AUTOSAVE_ENABLED.get()) {
+                AutosaveTask autosaveTask = new AutosaveTask();
+                List<File> unsavedLayerFiles = autosaveTask.getUnsavedLayersFiles();
+                if (!unsavedLayerFiles.isEmpty()) {
+                    ExtendedDialog dialog = new ExtendedDialog(
+                            Main.parent,
+                            tr("Unsaved osm data"),
+                            new String[] {tr("Restore"), tr("Cancel"), tr("Discard")}
+                            );
+                    dialog.setContent(
+                            trn("JOSM found {0} unsaved osm data layer. ",
+                                    "JOSM found {0} unsaved osm data layers. ", unsavedLayerFiles.size(), unsavedLayerFiles.size()) +
+                                    tr("It looks like JOSM crashed last time. Would you like to restore the data?"));
+                    dialog.setButtonIcons(new String[] {"ok", "cancel", "dialogs/delete"});
+                    int selection = dialog.showDialog().getValue();
+                    if (selection == 1) {
+                        autosaveTask.recoverUnsavedLayers();
+                    } else if (selection == 3) {
+                        autosaveTask.dicardUnsavedLayers();
+                    }
+                }
+                autosaveTask.schedule();
+            }
+        }
+
+        private static String getHtmlList(Collection<String> set) {
+            StringBuilder sb = new StringBuilder("<ul>");
+            for (String s : set) {
+                sb.append("<li>"+s+"</li>");
+            }
+            return sb.append("</ul>").toString();
+        }
+
+        private void handleProxyErrors() {
+            if (proxySelector.hasErrors()) {
+                ExtendedDialog ed = new ExtendedDialog(
+                        Main.parent, tr("Proxy errors occured"),
+                        new String[]{tr("Change proxy settings"), tr("Cancel")});
+                ed.setButtonIcons(new String[]{"dialogs/settings.png", "cancel.png"}).setCancelButton(2);
+                ed.setMinimumSize(new Dimension(460, 260));
+                ed.setIcon(JOptionPane.WARNING_MESSAGE);
+                ed.setContent(tr("JOSM tried to access the following resources:")+
+                        "<br>"+getHtmlList(proxySelector.getErrorResources())+
+                        tr("but <b>failed</b> to do so, because of the following proxy errors:")+
+                        "<br>"+getHtmlList(proxySelector.getErrorMessages())+
+                        tr("Would you like to change your proxy settings now ?")
+                        );
+
+                if (ed.showDialog().getValue() == 1) {
+                    PreferencesAction.forPreferenceSubTab(null, null, ProxyPreference.class).run();
+                }
+            }
         }
     }
 }

commit 0850c3f82e1c725e44859dae6b5b6dc657a6e478

fix server preferences dialog broken in r6523 + reload MOTD after proxy update if it fails because of a proxy error

src/org/openstreetmap/josm/gui/GettingStarted.java
@@ -24,14 +24,20 @@ import javax.swing.event.HyperlinkListener;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Version;
+import org.openstreetmap.josm.gui.preferences.server.ProxyPreference;
+import org.openstreetmap.josm.gui.preferences.server.ProxyPreferenceListener;
 import org.openstreetmap.josm.gui.widgets.JosmEditorPane;
 import org.openstreetmap.josm.io.CacheCustomContent;
 import org.openstreetmap.josm.tools.LanguageInfo;
 import org.openstreetmap.josm.tools.OpenBrowser;
 import org.openstreetmap.josm.tools.WikiReader;
 
-public final class GettingStarted extends JPanel {
+public final class GettingStarted extends JPanel implements ProxyPreferenceListener {
+
+    private final LinkGeneral lg;
     private String content = "";
+    private boolean contentInitialized = false;
+
     private static final String STYLE = "<style type=\"text/css\">\n"
             + "body {font-family: sans-serif; font-weight: bold; }\n"
             + "h1 {text-align: center; }\n"
@@ -112,7 +118,7 @@ public final class GettingStarted extends JPanel {
      */
     public GettingStarted() {
         super(new BorderLayout());
-        final LinkGeneral lg = new LinkGeneral("<html>" + STYLE + "<h1>" + "JOSM - " + tr("Java OpenStreetMap Editor")
+        lg = new LinkGeneral("<html>" + STYLE + "<h1>" + "JOSM - " + tr("Java OpenStreetMap Editor")
                 + "</h1><h2 align=\"center\">" + tr("Downloading \"Message of the day\"") + "</h2></html>");
         // clear the build-in command ctrl+shift+O, because it is used as shortcut in JOSM
         lg.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.SHIFT_MASK | InputEvent.CTRL_MASK), "none");
@@ -121,17 +127,27 @@ public final class GettingStarted extends JPanel {
         scroller.setViewportBorder(new EmptyBorder(10, 100, 10, 100));
         add(scroller, BorderLayout.CENTER);
 
+        getMOTD();
+
+        new FileDrop(scroller);
+    }
+
+    private void getMOTD() {
         // Asynchronously get MOTD to speed-up JOSM startup
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                if (content.isEmpty() && Main.pref.getBoolean("help.displaymotd", true)) {
+                if (!contentInitialized && Main.pref.getBoolean("help.displaymotd", true)) {
                     try {
                         content = new MotdContent().updateIfRequiredString();
+                        contentInitialized = true;
+                        ProxyPreference.removeProxyPreferenceListener(GettingStarted.this);
                     } catch (IOException ex) {
                         Main.warn(tr("Failed to read MOTD. Exception was: {0}", ex.toString()));
                         content = "<html>" + STYLE + "<h1>" + "JOSM - " + tr("Java OpenStreetMap Editor")
                                 + "</h1>\n<h2 align=\"center\">(" + tr("Message of the day not available") + ")</h2></html>";
+                        // In case of MOTD not loaded because of proxy error, listen to preference changes to retry after update
+                        ProxyPreference.addProxyPreferenceListener(GettingStarted.this);
                     }
                 }
 
@@ -145,8 +161,6 @@ public final class GettingStarted extends JPanel {
         }, "MOTD-Loader");
         t.setDaemon(true);
         t.start();
-
-        new FileDrop(scroller);
     }
 
     private String fixImageLinks(String s) {
@@ -161,5 +175,10 @@ public final class GettingStarted extends JPanel {
         }
         m.appendTail(sb);
         return sb.toString();
+    }
+
+    @Override
+    public void proxyPreferenceChanged() {
+        getMOTD();
     }
 }

src/org/openstreetmap/josm/gui/preferences/ServerAccessPreference.java
@@ -44,7 +44,7 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
      * @param panel the panel
      * @return the scroll pane
      */
-    protected JScrollPane wrapVerticallyScrollablePanel(VerticallyScrollablePanel panel) {
+    public static JScrollPane wrapVerticallyScrollablePanel(VerticallyScrollablePanel panel) {
         JScrollPane sp = new JScrollPane(panel);
         sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -108,13 +108,6 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
         gc.anchor = GridBagConstraints.NORTHWEST;
         gui.createPreferenceTab(this).add(buildContentPanel(), gc);
 
-        initFromPreferences();
-    }
-
-    /**
-     * Initializes the configuration panel with values from the preferences
-     */
-    public void initFromPreferences() {
         pnlApiUrlPreferences.initFromPreferences();
     }
 

src/org/openstreetmap/josm/gui/preferences/server/AuthenticationPreference.java
@@ -6,6 +6,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.PreferenceSettingFactory;
 import org.openstreetmap.josm.gui.preferences.PreferenceTabbedPane;
+import org.openstreetmap.josm.gui.preferences.ServerAccessPreference;
 import org.openstreetmap.josm.gui.preferences.SubPreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.TabPreferenceSetting;
 
@@ -35,7 +36,8 @@ public class AuthenticationPreference implements SubPreferenceSetting {
     public void addGui(PreferenceTabbedPane gui) {
         pnlAuthPreferences = new AuthenticationPreferencesPanel();
         gui.getServerPreference().addApiUrlChangeListener(pnlAuthPreferences);
-        gui.getServerPreference().addSubTab(this, tr("Authentication"), pnlAuthPreferences,
+        gui.getServerPreference().addSubTab(this, tr("Authentication"),
+                ServerAccessPreference.wrapVerticallyScrollablePanel(pnlAuthPreferences),
                 tr("Configure your identity and how to authenticate at the OSM server"));
     }
 

src/org/openstreetmap/josm/gui/preferences/server/AuthenticationPreferencesPanel.java
@@ -105,6 +105,7 @@ public class AuthenticationPreferencesPanel extends VerticallyScrollablePanel im
      */
     public AuthenticationPreferencesPanel() {
         build();
+        initFromPreferences();
         HelpUtil.setHelpContext(this, HelpUtil.ht("/Preferences/Connection#AuthenticationSettings"));
     }
 

src/org/openstreetmap/josm/gui/preferences/server/ProxyPreference.java
@@ -3,9 +3,13 @@ package org.openstreetmap.josm.gui.preferences.server;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.PreferenceSettingFactory;
 import org.openstreetmap.josm.gui.preferences.PreferenceTabbedPane;
+import org.openstreetmap.josm.gui.preferences.ServerAccessPreference;
 import org.openstreetmap.josm.gui.preferences.SubPreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.TabPreferenceSetting;
 
@@ -25,7 +29,9 @@ public class ProxyPreference implements SubPreferenceSetting {
         }
     }
 
-    ProxyPreferencesPanel pnlProxyPreferences;
+    private static Set<ProxyPreferenceListener> listeners = new HashSet<ProxyPreferenceListener>();
+    
+    private ProxyPreferencesPanel pnlProxyPreferences;
 
     private ProxyPreference() {
         super();
@@ -34,13 +40,17 @@ public class ProxyPreference implements SubPreferenceSetting {
     @Override
     public void addGui(PreferenceTabbedPane gui) {
         pnlProxyPreferences = new ProxyPreferencesPanel();
-        gui.getServerPreference().addSubTab(this, tr("Proxy settings"), pnlProxyPreferences,
+        gui.getServerPreference().addSubTab(this, tr("Proxy settings"),
+                ServerAccessPreference.wrapVerticallyScrollablePanel(pnlProxyPreferences),
                 tr("Configure whether to use a proxy server"));
     }
 
     @Override
     public boolean ok() {
         pnlProxyPreferences.saveToPreferences();
+        for (ProxyPreferenceListener listener : listeners) {
+            listener.proxyPreferenceChanged();
+        }
         return false;
     }
 
@@ -52,5 +62,31 @@ public class ProxyPreference implements SubPreferenceSetting {
     @Override
     public TabPreferenceSetting getTabPreferenceSetting(PreferenceTabbedPane gui) {
         return gui.getServerPreference();
+    }
+    
+    /**
+     * Adds a new ProxyPreferenceListener.
+     * @param listener the listener to add
+     * @return {@code true} if the listener has been added, {@code false} otherwise
+     * @since 6525
+     */
+    public static boolean addProxyPreferenceListener(ProxyPreferenceListener listener) {
+        if (listener != null) {
+            return listeners.add(listener);
+        }
+        return false;
+    }
+
+    /**
+     * Removes a ProxyPreferenceListener.
+     * @param listener the listener to remove
+     * @return {@code true} if the listener has been removed, {@code false} otherwise
+     * @since 6525
+     */
+    public static boolean removeProxyPreferenceListener(ProxyPreferenceListener listener) {
+        if (listener != null) {
+            return listeners.remove(listener);
+        }
+        return false;
     }
 }

src/org/openstreetmap/josm/gui/preferences/server/ProxyPreferenceListener.java
@@ -1,0 +1,14 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.preferences.server;
+
+/**
+ * Listener called when proxy settings are updated.
+ * @since 6525
+ */
+public interface ProxyPreferenceListener {
+
+    /**
+     * Method called when proxy settings are updated.
+     */
+    public void proxyPreferenceChanged();
+}

src/org/openstreetmap/josm/gui/preferences/server/ProxyPreferencesPanel.java
@@ -359,6 +359,9 @@ public class ProxyPreferencesPanel extends VerticallyScrollablePanel {
         }
     }
 
+    /**
+     * Constructs a new {@code ProxyPreferencesPanel}.
+     */
     public ProxyPreferencesPanel() {
         setLayout(new GridBagLayout());
         setBorder(BorderFactory.createEmptyBorder(5,5,5,5));