tsmock/josm

commit 4affc24697c8e98c932abde5d9a98fc77c1e31cf

see #8465 - use String switch/case where applicable
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7012 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/imagery/WmsCache.java
@@ -452,13 +452,18 @@ public class WmsCache {
 
         String zoom = SystemOfMeasurement.METRIC.getDistText(ll1.greatCircleDistance(ll2));
         String extension;
-        if ("image/jpeg".equals(mimeType) || "image/jpg".equals(mimeType)) {
+        switch(mimeType) {
+        case "image/jpeg":
+        case "image/jpg":
             extension = "jpg";
-        } else if ("image/png".equals(mimeType)) {
+            break;
+        case "image/png":
             extension = "png";
-        } else if ("image/gif".equals(mimeType)) {
+            break;
+        case "image/gif":
             extension = "gif";
-        } else {
+            break;
+        default:
             extension = "dat";
         }
 

src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -95,6 +95,7 @@ public class AutoScaleAction extends JosmAction {
     private static int getModeShortcut(String mode) {
         int shortcut = -1;
 
+        // TODO: convert this to switch/case and make sure the parsing still works
         /* leave as single line for shortcut overview parsing! */
         if (mode.equals("data")) { shortcut = KeyEvent.VK_1; }
         else if (mode.equals("layer")) { shortcut = KeyEvent.VK_2; }
@@ -129,23 +130,32 @@ public class AutoScaleAction extends JosmAction {
         String modeHelp = Character.toUpperCase(mode.charAt(0)) + mode.substring(1);
         putValue("help", "Action/AutoScale/" + modeHelp);
         this.mode = mode;
-        if (mode.equals("data")) {
+        switch (mode) {
+        case "data":
             putValue("help", ht("/Action/ZoomToData"));
-        } else if (mode.equals("layer")) {
+            break;
+        case "layer":
             putValue("help", ht("/Action/ZoomToLayer"));
-        } else if (mode.equals("selection")) {
+            break;
+        case "selection":
             putValue("help", ht("/Action/ZoomToSelection"));
-        } else if (mode.equals("conflict")) {
+            break;
+        case "conflict":
             putValue("help", ht("/Action/ZoomToConflict"));
-        } else if (mode.equals("problem")) {
+            break;
+        case "problem":
             putValue("help", ht("/Action/ZoomToProblem"));
-        } else if (mode.equals("download")) {
+            break;
+        case "download":
             putValue("help", ht("/Action/ZoomToDownload"));
-        } else if (mode.equals("previous")) {
+            break;
+        case "previous":
             putValue("help", ht("/Action/ZoomToPrevious"));
-        } else if (mode.equals("next")) {
+            break;
+        case "next":
             putValue("help", ht("/Action/ZoomToNext"));
-        } else {
+            break;
+        default:
             throw new IllegalArgumentException("Unknown mode: "+mode);
         }
         installAdapters();
@@ -153,11 +163,14 @@ public class AutoScaleAction extends JosmAction {
 
     public void autoScale()  {
         if (Main.isDisplayingMapView()) {
-            if (mode.equals("previous")) {
+            switch(mode) {
+            case "previous":
                 Main.map.mapView.zoomPrevious();
-            } else if (mode.equals("next")) {
+                break;
+            case "next":
                 Main.map.mapView.zoomNext();
-            } else {
+                break;
+            default:
                 BoundingXYVisitor bbox = getBoundingBox();
                 if (bbox != null && bbox.getBounds() != null) {
                     Main.map.mapView.recalculateCenterScale(bbox);
@@ -186,30 +199,35 @@ public class AutoScaleAction extends JosmAction {
     }
 
     private BoundingXYVisitor getBoundingBox() {
-        BoundingXYVisitor v = mode.equals("problem") ? new ValidatorBoundingXYVisitor() : new BoundingXYVisitor();
+        BoundingXYVisitor v = "problem".equals(mode) ? new ValidatorBoundingXYVisitor() : new BoundingXYVisitor();
 
-        if (mode.equals("problem")) {
+        switch(mode) {
+        case "problem":
             TestError error = Main.map.validatorDialog.getSelectedError();
             if (error == null) return null;
             ((ValidatorBoundingXYVisitor) v).visit(error);
             if (v.getBounds() == null) return null;
             v.enlargeBoundingBox(Main.pref.getDouble("validator.zoom-enlarge-bbox", 0.0002));
-        } else if (mode.equals("data")) {
+            break;
+        case "data":
             for (Layer l : Main.map.mapView.getAllLayers()) {
                 l.visitBoundingBox(v);
             }
-        } else if (mode.equals("layer")) {
+            break;
+        case "layer":
             if (Main.main.getActiveLayer() == null)
                 return null;
             // try to zoom to the first selected layer
             Layer l = getFirstSelectedLayer();
             if (l == null) return null;
             l.visitBoundingBox(v);
-        } else if (mode.equals("selection") || mode.equals("conflict")) {
+            break;
+        case "selection":
+        case "conflict":
             Collection<OsmPrimitive> sel = new HashSet<>();
-            if (mode.equals("selection")) {
+            if ("selection".equals(mode)) {
                 sel = getCurrentDataSet().getSelected();
-            } else if (mode.equals("conflict")) {
+            } else {
                 Conflict<? extends OsmPrimitive> c = Main.map.conflictDialog.getSelectedConflict();
                 if (c != null) {
                     sel.add(c.getMy());
@@ -220,7 +238,7 @@ public class AutoScaleAction extends JosmAction {
             if (sel.isEmpty()) {
                 JOptionPane.showMessageDialog(
                         Main.parent,
-                        (mode.equals("selection") ? tr("Nothing selected to zoom to.") : tr("No conflicts to zoom to")),
+                        ("selection".equals(mode) ? tr("Nothing selected to zoom to.") : tr("No conflicts to zoom to")),
                         tr("Information"),
                         JOptionPane.INFORMATION_MESSAGE
                 );
@@ -235,7 +253,8 @@ public class AutoScaleAction extends JosmAction {
             // Make the bounding box at least 0.0005 degrees (≈ 56 m) wide to
             // ensure reasonable zoom level when zooming onto single nodes.
             v.enlargeToMinDegrees(0.0005);
-        } else if (mode.equals("download")) {
+            break;
+        case "download":
             Bounds bounds = DownloadDialog.getSavedDownloadBounds();
             if (bounds != null) {
                 try {
@@ -244,33 +263,39 @@ public class AutoScaleAction extends JosmAction {
                     Main.warn(e);
                 }
             }
+            break;
         }
         return v;
     }
 
     @Override
     protected void updateEnabledState() {
-        if ("selection".equals(mode)) {
+        switch(mode) {
+        case "selection":
             setEnabled(getCurrentDataSet() != null && ! getCurrentDataSet().getSelected().isEmpty());
-        }  else if ("layer".equals(mode)) {
+            break;
+        case "layer":
             if (!Main.isDisplayingMapView() || Main.map.mapView.getAllLayersAsList().isEmpty()) {
                 setEnabled(false);
             } else {
                 // FIXME: should also check for whether a layer is selected in the layer list dialog
                 setEnabled(true);
             }
-        } else if ("conflict".equals(mode)) {
+            break;
+        case "conflict":
             setEnabled(Main.map != null && Main.map.conflictDialog.getSelectedConflict() != null);
-        } else if ("problem".equals(mode)) {
+            break;
+        case "problem":
             setEnabled(Main.map != null && Main.map.validatorDialog.getSelectedError() != null);
-        } else if ("previous".equals(mode)) {
+            break;
+        case "previous":
             setEnabled(Main.isDisplayingMapView() && Main.map.mapView.hasZoomUndoEntries());
-        } else if ("next".equals(mode)) {
+            break;
+        case "next":
             setEnabled(Main.isDisplayingMapView() && Main.map.mapView.hasZoomRedoEntries());
-        } else {
-            setEnabled(
-                    Main.isDisplayingMapView()
-                    && Main.map.mapView.hasLayers()
+            break;
+        default:
+            setEnabled(Main.isDisplayingMapView() && Main.map.mapView.hasLayers()
             );
         }
     }
@@ -310,13 +335,13 @@ public class AutoScaleAction extends JosmAction {
         private TreeSelectionListener validatorSelectionListener;
 
         public MapFrameAdapter() {
-            if (mode.equals("conflict")) {
+            if ("conflict".equals(mode)) {
                 conflictSelectionListener = new ListSelectionListener() {
                     @Override public void valueChanged(ListSelectionEvent e) {
                         updateEnabledState();
                     }
                 };
-            } else if (mode.equals("problem")) {
+            } else if ("problem".equals(mode)) {
                 validatorSelectionListener = new TreeSelectionListener() {
                     @Override public void valueChanged(TreeSelectionEvent e) {
                         updateEnabledState();

src/org/openstreetmap/josm/actions/CreateMultipolygonAction.java
@@ -354,7 +354,7 @@ public class CreateMultipolygonAction extends JosmAction {
         // filter out empty key conflicts - we need second iteration
         if( !Main.pref.getBoolean("multipoly.alltags", false) )
             for( RelationMember m : relation.getMembers() )
-                if( m.hasRole() && m.getRole().equals("outer") && m.isWay() )
+                if( m.hasRole() && "outer".equals(m.getRole()) && m.isWay() )
                     for( String key : values.keySet() )
                         if( !m.getWay().hasKey(key) && !relation.hasKey(key) )
                             conflictingKeys.add(key);
@@ -406,7 +406,7 @@ public class CreateMultipolygonAction extends JosmAction {
             Relation r2 = new Relation(relation);
             for (Entry<String, String> entry : values.entrySet()) {
                 String key = entry.getKey();
-                if (!r2.hasKey(key) && !key.equals("area") ) {
+                if (!r2.hasKey(key) && !"area".equals(key) ) {
                     if (relation.isNew())
                         relation.put(key, entry.getValue());
                     else

src/org/openstreetmap/josm/actions/OrthogonalizeAction.java
@@ -185,7 +185,7 @@ public final class OrthogonalizeAction extends JosmAction {
             }
         } catch (InvalidUserInputException ex) {
             String msg;
-            if (ex.getMessage().equals("usage")) {
+            if ("usage".equals(ex.getMessage())) {
                 msg = "<h2>" + tr("Usage") + "</h2>" + USAGE;
             } else {
                 msg = ex.getMessage() + "<br><hr><h2>" + tr("Usage") + "</h2>" + USAGE;

src/org/openstreetmap/josm/actions/search/SearchCompiler.java
@@ -104,58 +104,65 @@ public class SearchCompiler {
 
         @Override
         public Match get(String keyword, PushbackTokenizer tokenizer) throws ParseError {
-            if ("modified".equals(keyword))
+            switch(keyword) {
+            case "modified":
                 return new Modified();
-            else if ("selected".equals(keyword))
+            case "selected":
                 return new Selected();
-            else if ("incomplete".equals(keyword))
+            case "incomplete":
                 return new Incomplete();
-            else if ("untagged".equals(keyword))
+            case "untagged":
                 return new Untagged();
-            else if ("closed".equals(keyword))
+            case "closed":
                 return new Closed();
-            else if ("new".equals(keyword))
+            case "new":
                 return new New();
-            else if ("indownloadedarea".equals(keyword))
+            case "indownloadedarea":
                 return new InDataSourceArea(false);
-            else if ("allindownloadedarea".equals(keyword))
+            case "allindownloadedarea":
                 return new InDataSourceArea(true);
-            else if ("inview".equals(keyword))
+            case "inview":
                 return new InView(false);
-            else if ("allinview".equals(keyword))
+            case "allinview":
                 return new InView(true);
-            else if (tokenizer != null) {
-                if ("id".equals(keyword))
-                    return new Id(tokenizer);
-                else if ("version".equals(keyword))
-                    return new Version(tokenizer);
-                else if ("changeset".equals(keyword))
-                    return new ChangesetId(tokenizer);
-                else if ("nodes".equals(keyword))
-                    return new NodeCountRange(tokenizer);
-                else if ("tags".equals(keyword))
-                    return new TagCountRange(tokenizer);
-                else if ("areasize".equals(keyword))
-                    return new AreaSize(tokenizer);
-                else if ("nth".equals(keyword))
-                    return new Nth(tokenizer, false);
-                else if ("nth%".equals(keyword))
-                    return new Nth(tokenizer, true);
-                else if ("timestamp".equals(keyword)) {
-                    String rangeS = " " + tokenizer.readTextOrNumber() + " "; // add leading/trailing space in order to get expected split (e.g. "a--" => {"a", ""})
-                    String[] rangeA = rangeS.split("/");
-                    if (rangeA.length == 1)
-                        return new KeyValue(keyword, rangeS.trim(), regexSearch, caseSensitive);
-                    else if (rangeA.length == 2) {
-                        String rangeA1 = rangeA[0].trim();
-                        String rangeA2 = rangeA[1].trim();
-                        long minDate = DateUtils.fromString(rangeA1.isEmpty() ? "1980" : rangeA1).getTime(); // if min timestap is empty: use lowest possible date
-                        long maxDate = rangeA2.isEmpty() ? System.currentTimeMillis() : DateUtils.fromString(rangeA2).getTime(); // if max timestamp is empty: use "now"
-                        return new TimestampRange(minDate, maxDate);
-                    } else
-                        /*
-                         * I18n: Don't translate timestamp keyword
-                         */ throw new ParseError(tr("Expecting <i>min</i>/<i>max</i> after ''timestamp''"));
+            default:
+                if (tokenizer != null) {
+                    switch (keyword) {
+                    case "id":
+                        return new Id(tokenizer);
+                    case "version":
+                        return new Version(tokenizer);
+                    case "changeset":
+                        return new ChangesetId(tokenizer);
+                    case "nodes":
+                        return new NodeCountRange(tokenizer);
+                    case "tags":
+                        return new TagCountRange(tokenizer);
+                    case "areasize":
+                        return new AreaSize(tokenizer);
+                    case "nth":
+                        return new Nth(tokenizer, false);
+                    case "nth%":
+                        return new Nth(tokenizer, true);
+                    case "timestamp":
+                        // add leading/trailing space in order to get expected split (e.g. "a--" => {"a", ""})
+                        String rangeS = " " + tokenizer.readTextOrNumber() + " ";
+                        String[] rangeA = rangeS.split("/");
+                        if (rangeA.length == 1) {
+                            return new KeyValue(keyword, rangeS.trim(), regexSearch, caseSensitive);
+                        } else if (rangeA.length == 2) {
+                            String rangeA1 = rangeA[0].trim();
+                            String rangeA2 = rangeA[1].trim();
+                            // if min timestap is empty: use lowest possible date
+                            long minDate = DateUtils.fromString(rangeA1.isEmpty() ? "1980" : rangeA1).getTime(); 
+                            // if max timestamp is empty: use "now"
+                            long maxDate = rangeA2.isEmpty() ? System.currentTimeMillis() : DateUtils.fromString(rangeA2).getTime(); 
+                            return new TimestampRange(minDate, maxDate);
+                        } else {
+                            // I18n: Don't translate timestamp keyword
+                            throw new ParseError(tr("Expecting <i>min</i>/<i>max</i> after ''timestamp''"));
+                        }
+                    }
                 }
             }
             return null;
@@ -1313,14 +1320,16 @@ public class SearchCompiler {
         if (value == null) {
             value = "";
         }
-        if ("type".equals(key))
+        switch(key) {
+        case "type":
             return new ExactType(value);
-        else if ("user".equals(key))
+        case "user":
             return new UserMatch(value);
-        else if ("role".equals(key))
+        case "role":
             return new RoleMatch(value);
-        else
+        default:
             return new KeyValue(key, value, regexSearch, caseSensitive);
+        }
     }
 
     private static int regexFlags(boolean caseSensitive) {

src/org/openstreetmap/josm/data/CustomConfigurator.java
@@ -489,45 +489,54 @@ public final class CustomConfigurator {
                 String elementName = item.getNodeName();
                 Element elem = (Element) item;
 
-                if ("var".equals(elementName)) {
+                switch(elementName) {
+                case "var":
                     setVar(elem.getAttribute("name"), evalVars(elem.getAttribute("value")));
-                } else if ("task".equals(elementName)) {
+                    break;
+                case "task":
                     tasksMap.put(elem.getAttribute("name"), elem);
-                } else if ("runtask".equals(elementName)) {
+                    break;
+                case "runtask":
                     if (processRunTaskElement(elem)) return;
-                } else if ("ask".equals(elementName)) {
+                    break;
+                case "ask":
                     processAskElement(elem);
-                } else if ("if".equals(elementName)) {
+                    break;
+                case "if":
                     processIfElement(elem);
-                } else if ("else".equals(elementName)) {
+                    break;
+                case "else":
                     processElseElement(elem);
-                } else if ("break".equals(elementName)) {
+                    break;
+                case "break":
                     return;
-                } else if ("plugin".equals(elementName)) {
+                case "plugin":
                     processPluginInstallElement(elem);
-                } else if ("messagebox".equals(elementName)){
+                    break;
+                case "messagebox":
                     processMsgBoxElement(elem);
-                } else if ("preferences".equals(elementName)) {
+                    break;
+                case "preferences":
                     processPreferencesElement(elem);
-                } else if ("download".equals(elementName)) {
+                    break;
+                case "download":
                     processDownloadElement(elem);
-                } else if ("delete".equals(elementName)) {
+                    break;
+                case "delete":
                     processDeleteElement(elem);
-                } else if ("script".equals(elementName)) {
+                    break;
+                case "script":
                     processScriptElement(elem);
-                } else {
+                    break;
+                default:
                     log("Error: Unknown element " + elementName);
                 }
-
             }
         }
-
-
 
         private void processPreferencesElement(Element item) {
             String oper = evalVars(item.getAttribute("operation"));
             String id = evalVars(item.getAttribute("id"));
-
 
             if ("delete-keys".equals(oper)) {
                 String pattern = evalVars(item.getAttribute("pattern"));
@@ -613,7 +622,6 @@ public final class CustomConfigurator {
             messageBox(type, text);
         }
 
-
         private void processAskElement(Element elem) {
             String text = evalVars(elem.getAttribute("text"));
             String locText = evalVars(elem.getAttribute(LanguageInfo.getJOSMLocaleCode()+".text"));
@@ -672,7 +680,6 @@ public final class CustomConfigurator {
             return false;
         }
 
-
         private void processScriptElement(Element elem) {
             String js = elem.getChildNodes().item(0).getTextContent();
             log("Processing script...");
@@ -729,8 +736,6 @@ public final class CustomConfigurator {
             if (s.endsWith("/")) s=s.substring(0,s.length()-1);
             return s;
         }
-
-
     }
 
     /**

src/org/openstreetmap/josm/data/Preferences.java
@@ -1359,16 +1359,18 @@ public class Preferences {
             int event = parser.next();
             if (event == XMLStreamConstants.START_ELEMENT) {
                 String localName = parser.getLocalName();
-                if ("tag".equals(localName)) {
+                switch(localName) {
+                case "tag":
                     settingsMap.put(parser.getAttributeValue(null, "key"), new StringSetting(parser.getAttributeValue(null, "value")));
                     jumpToEnd();
-                } else if ("list".equals(localName) ||
-                        "collection".equals(localName) ||
-                        "lists".equals(localName) ||
-                        "maps".equals(localName)
-                ) {
+                    break;
+                case "list":
+                case "collection":
+                case "lists":
+                case "maps":
                     parseToplevelList();
-                } else {
+                    break;
+                default:
                     throwException("Unexpected element: "+localName);
                 }
             } else if (event == XMLStreamConstants.END_ELEMENT) {
@@ -1399,23 +1401,27 @@ public class Preferences {
             int event = parser.next();
             if (event == XMLStreamConstants.START_ELEMENT) {
                 String localName = parser.getLocalName();
-                if ("entry".equals(localName)) {
+                switch(localName) {
+                case "entry":
                     if (entries == null) {
                         entries = new ArrayList<>();
                     }
                     entries.add(parser.getAttributeValue(null, "value"));
                     jumpToEnd();
-                } else if ("list".equals(localName)) {
+                    break;
+                case "list":
                     if (lists == null) {
                         lists = new ArrayList<>();
                     }
                     lists.add(parseInnerList());
-                } else if ("map".equals(localName)) {
+                    break;
+                case "map":
                     if (maps == null) {
                         maps = new ArrayList<>();
                     }
                     maps.add(parseMap());
-                } else {
+                    break;
+                default:
                     throwException("Unexpected element: "+localName);
                 }
             } else if (event == XMLStreamConstants.END_ELEMENT) {

src/org/openstreetmap/josm/data/osm/visitor/paint/StyledMapRenderer.java
@@ -915,35 +915,33 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         OsmPrimitive via = null;
 
         /* find the "from", "via" and "to" elements */
-        for (RelationMember m : r.getMembers())
-        {
+        for (RelationMember m : r.getMembers()) {
             if(m.getMember().isIncomplete())
                 return;
-            else
-            {
-                if(m.isWay())
-                {
+            else {
+                if(m.isWay()) {
                     Way w = m.getWay();
                     if(w.getNodesCount() < 2) {
                         continue;
                     }
 
-                    if("from".equals(m.getRole())) {
+                    switch(m.getRole()) {
+                    case "from":
                         if(fromWay == null) {
                             fromWay = w;
                         }
-                    } else if("to".equals(m.getRole())) {
+                        break;
+                    case "to":
                         if(toWay == null) {
                             toWay = w;
                         }
-                    } else if("via".equals(m.getRole())) {
+                        break;
+                    case "via":
                         if(via == null) {
                             via = w;
                         }
                     }
-                }
-                else if(m.isNode())
-                {
+                } else if(m.isNode()) {
                     Node n = m.getNode();
                     if("via".equals(m.getRole()) && via == null) {
                         via = n;

commit 118bee876ea8b5722a9da8e881b647610cf391cd

fix #9962 - NPE in WMS cache (regression from r7012)

src/org/openstreetmap/josm/data/imagery/WmsCache.java
@@ -451,20 +451,22 @@ public class WmsCache {
         int precisionLon = Math.max(0, -(int)Math.ceil(Math.log10(deltaLon)) + 1);
 
         String zoom = SystemOfMeasurement.METRIC.getDistText(ll1.greatCircleDistance(ll2));
-        String extension;
-        switch(mimeType) {
-        case "image/jpeg":
-        case "image/jpg":
-            extension = "jpg";
-            break;
-        case "image/png":
-            extension = "png";
-            break;
-        case "image/gif":
-            extension = "gif";
-            break;
-        default:
-            extension = "dat";
+        String extension = "dat";
+        if (mimeType != null) {
+            switch(mimeType) {
+            case "image/jpeg":
+            case "image/jpg":
+                extension = "jpg";
+                break;
+            case "image/png":
+                extension = "png";
+                break;
+            case "image/gif":
+                extension = "gif";
+                break;
+            default:
+                Main.warn("Unrecognized MIME type: "+mimeType);
+            }
         }
 
         int counter = 0;