tsmock/josm

commit 1290ef4b6dbf2d0046ec8b5413f259325b6a24f2

see #8465 - global use of `try-with-resources`, according to  * http://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html  * https://blogs.oracle.com/darcy/entry/project_coin_null_try_with
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7033 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/remotecontrol/RemoteControlHttpServer.java
@@ -82,11 +82,10 @@ public class RemoteControlHttpServer extends Thread {
         Main.info(marktr("RemoteControl::Accepting connections on port {0}"),
              Integer.toString(server.getLocalPort()));
         while (true) {
-            try {
-                Socket request = server.accept();
+            try (Socket request = server.accept()) {
                 RequestProcessor.processRequest(request);
-            } catch( SocketException se) {
-                if( !server.isClosed() )
+            } catch (SocketException se) {
+                if (!server.isClosed())
                     Main.error(se);
             } catch (IOException ioe) {
                 Main.error(ioe);

src/org/openstreetmap/josm/io/remotecontrol/RemoteControlHttpsServer.java
@@ -55,37 +55,38 @@ public class RemoteControlHttpsServer extends Thread {
                 char[] password = KEYSTORE_PASSWORD.toCharArray();
                 
                 // Load keystore
-                InputStream in = RemoteControlHttpsServer.class.getResourceAsStream(KEYSTORE_PATH);
-                if (in == null) {
-                    Main.error(tr("Unable to find JOSM keystore at {0}. Remote control will not be available on HTTPS.", KEYSTORE_PATH));
-                } else {
-                    try {
-                        ks.load(in, password);
-                    } finally {
-                        Utils.close(in);
-                    }
-                    
-                    if (Main.isDebugEnabled()) {
-                        for (Enumeration<String> aliases = ks.aliases(); aliases.hasMoreElements();) {
-                            Main.debug("Alias in keystore: "+aliases.nextElement());
+                try (InputStream in = RemoteControlHttpsServer.class.getResourceAsStream(KEYSTORE_PATH)) {
+                    if (in == null) {
+                        Main.error(tr("Unable to find JOSM keystore at {0}. Remote control will not be available on HTTPS.", KEYSTORE_PATH));
+                    } else {
+                        try {
+                            ks.load(in, password);
+                        } finally {
+                            Utils.close(in);
                         }
+                        
+                        if (Main.isDebugEnabled()) {
+                            for (Enumeration<String> aliases = ks.aliases(); aliases.hasMoreElements();) {
+                                Main.debug("Alias in keystore: "+aliases.nextElement());
+                            }
+                        }
+    
+                        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+                        kmf.init(ks, password);
+                        
+                        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+                        tmf.init(ks);
+                        
+                        sslContext = SSLContext.getInstance("TLS");
+                        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+                        
+                        if (Main.isDebugEnabled()) {
+                            Main.debug("SSL Context protocol: " + sslContext.getProtocol());
+                            Main.debug("SSL Context provider: " + sslContext.getProvider());
+                        }
+                        
+                        initOK = true;
                     }
-
-                    KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-                    kmf.init(ks, password);
-                    
-                    TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-                    tmf.init(ks);
-                    
-                    sslContext = SSLContext.getInstance("TLS");
-                    sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-                    
-                    if (Main.isDebugEnabled()) {
-                        Main.debug("SSL Context protocol: " + sslContext.getProtocol());
-                        Main.debug("SSL Context provider: " + sslContext.getProvider());
-                    }
-                    
-                    initOK = true;
                 }
             } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | 
                     IOException | UnrecoverableKeyException | KeyManagementException e) {
@@ -174,8 +175,7 @@ public class RemoteControlHttpsServer extends Thread {
         Main.info(marktr("RemoteControl::Accepting secure connections on port {0}"),
              Integer.toString(server.getLocalPort()));
         while (true) {
-            try {
-                Socket request = server.accept();
+            try (Socket request = server.accept()) {
                 if (Main.isDebugEnabled() && request instanceof SSLSocket) {
                     SSLSocket sslSocket = (SSLSocket) request;
                     Main.debug("SSL socket - Enabled Cipher suites: "+Arrays.toString(sslSocket.getEnabledCipherSuites()));

commit 0dccee5e868d4edc0069f57805dd765dde3d060c

fix #9967, see #8465 - RemoteControl broken by changes in r7033

src/org/openstreetmap/josm/io/remotecontrol/RemoteControlHttpServer.java
@@ -82,7 +82,9 @@ public class RemoteControlHttpServer extends Thread {
         Main.info(marktr("RemoteControl::Accepting connections on port {0}"),
              Integer.toString(server.getLocalPort()));
         while (true) {
-            try (Socket request = server.accept()) {
+            try {
+                @SuppressWarnings("resource")
+                Socket request = server.accept();
                 RequestProcessor.processRequest(request);
             } catch (SocketException se) {
                 if (!server.isClosed())

src/org/openstreetmap/josm/io/remotecontrol/RemoteControlHttpsServer.java
@@ -175,7 +175,9 @@ public class RemoteControlHttpsServer extends Thread {
         Main.info(marktr("RemoteControl::Accepting secure connections on port {0}"),
              Integer.toString(server.getLocalPort()));
         while (true) {
-            try (Socket request = server.accept()) {
+            try {
+                @SuppressWarnings("resource")
+                Socket request = server.accept();
                 if (Main.isDebugEnabled() && request instanceof SSLSocket) {
                     SSLSocket sslSocket = (SSLSocket) request;
                     Main.debug("SSL socket - Enabled Cipher suites: "+Arrays.toString(sslSocket.getEnabledCipherSuites()));