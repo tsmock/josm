tsmock/josm

commit 0a3c0b23b8ab5655cd2bfbbff2f8d6a48acb7f72

fix #10191 - Wrong number of errors displayed in validator tree
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7266 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/validator/ValidatorTreePanel.java
@@ -42,6 +42,18 @@ import org.openstreetmap.josm.tools.MultiMap;
  */
 public class ValidatorTreePanel extends JTree implements Destroyable {
 
+    private static final class GroupTreeNode extends DefaultMutableTreeNode {
+
+        public GroupTreeNode(Object userObject) {
+            super(userObject);
+        }
+
+        @Override
+        public String toString() {
+            return tr("{0} ({1})", super.toString(), getLeafCount());
+        }
+    }
+
     /**
      * The validation data.
      */
@@ -208,12 +220,7 @@ public class ValidatorTreePanel extends JTree implements Destroyable {
             }
 
             // Severity node
-            DefaultMutableTreeNode severityNode = new DefaultMutableTreeNode(s) {
-                @Override
-                public String toString() {
-                    return super.toString() + " (" + getLeafCount() + ")";
-                }
-            };
+            DefaultMutableTreeNode severityNode = new GroupTreeNode(s);
             rootNode.add(severityNode);
 
             if (oldSelectedRows.contains(s)) {
@@ -242,8 +249,7 @@ public class ValidatorTreePanel extends JTree implements Destroyable {
                 MultiMap<String, TestError> errorlist = bag.getValue();
                 DefaultMutableTreeNode groupNode = null;
                 if (errorlist.size() > 1) {
-                    String nmsg = tr("{0} ({1})", bag.getKey(), errorlist.size());
-                    groupNode = new DefaultMutableTreeNode(nmsg);
+                    groupNode = new GroupTreeNode(bag.getKey());
                     severityNode.add(groupNode);
                     if (oldSelectedRows.contains(bag.getKey())) {
                         expandedPaths.add(new TreePath(new Object[] { rootNode, severityNode, groupNode }));

commit 09acd6776d16285c06ae228e6f98e2a3c6ca5991

fix #10197 - StringIndexOutOfBoundsException in validator panel (regression from r7266)

src/org/openstreetmap/josm/gui/dialogs/validator/ValidatorTreePanel.java
@@ -163,7 +163,10 @@ public class ValidatorTreePanel extends JTree implements Destroyable {
                     oldSelectedRows.add(userObject);
                 } else if (userObject instanceof String) {
                     String msg = (String) userObject;
-                    msg = msg.substring(0, msg.lastIndexOf(" ("));
+                    int index = msg.lastIndexOf(" (");
+                    if (index > 0) {
+                        msg = msg.substring(0, index);
+                    }
                     oldSelectedRows.add(msg);
                 }
             }