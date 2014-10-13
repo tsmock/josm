tsmock/josm

commit 3316dd20316edcd5812ef34a8f8d1e435be9df3e

fix #5957, fix #10458, see #10455: fix various focus/shortcuts issues: * proper use of "Command" modifier on OS X instead of "Ctrl" * fix related focus issue on OS X in layer list dialog (copy/paste between layers not working) * fix non-working commands after a layer selection: Ctrl-A, Tab, Ctrl-Down/Up/Left/Right, F8 * fix impossibility to use 'A', 'S' or 'X' keys in relation filter or when renaming layer
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7539 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/widgets/DisableShortcutsOnFocusGainedTextField.java
@@ -112,10 +112,12 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
     public void focusGained(FocusEvent e) {
         disableMenuActions();
         unregisterActionShortcuts();
+        Main.map.keyDetector.setEnabled(false);
     }
 
     @Override
     public void focusLost(FocusEvent e) {
+        Main.map.keyDetector.setEnabled(true);
         restoreActionShortcuts();
         restoreMenuActions();
     }

src/org/openstreetmap/josm/gui/dialogs/RelationListDialog.java
@@ -116,6 +116,7 @@ public class RelationListDialog extends ToggleDialog implements DataSetListener 
 
     HighlightHelper highlightHelper = new HighlightHelper();
     private boolean highlightEnabled = Main.pref.getBoolean("draw.target-highlight", true);
+
     /**
      * Constructs <code>RelationListDialog</code>
      */
@@ -196,7 +197,8 @@ public class RelationListDialog extends ToggleDialog implements DataSetListener 
         }
     }
 
-    @Override public void showNotify() {
+    @Override
+    public void showNotify() {
         MapView.addLayerChangeListener(newAction);
         newAction.updateEnabledState();
         DatasetEventManager.getInstance().addDatasetListener(this, FireMode.IN_EDT);
@@ -204,7 +206,8 @@ public class RelationListDialog extends ToggleDialog implements DataSetListener 
         dataChanged(null);
     }
 
-    @Override public void hideNotify() {
+    @Override
+    public void hideNotify() {
         MapView.removeLayerChangeListener(newAction);
         DatasetEventManager.getInstance().removeDatasetListener(this);
         DataSet.removeSelectionListener(addSelectionToRelations);
@@ -322,7 +325,8 @@ public class RelationListDialog extends ToggleDialog implements DataSetListener 
             EditRelationAction.launchEditor(getSelected());
         }
 
-        @Override public void mouseClicked(MouseEvent e) {
+        @Override
+        public void mouseClicked(MouseEvent e) {
             if (!Main.main.hasEditLayer()) return;
             if (isDoubleClick(e)) {
                 if (e.isControlDown()) {

src/org/openstreetmap/josm/gui/dialogs/properties/TagEditHelper.java
@@ -79,7 +79,8 @@ import org.openstreetmap.josm.tools.Shortcut;
 import org.openstreetmap.josm.tools.WindowGeometry;
 
 /**
- * Class that helps PropertiesDialog add and edit tag values
+ * Class that helps PropertiesDialog add and edit tag values.
+ * @since 5633
  */
 class TagEditHelper {
     private final DefaultTableModel tagData;
@@ -138,7 +139,7 @@ class TagEditHelper {
     }
 
     /**
-    * Edit the value in the tags table row
+    * Edit the value in the tags table row.
     * @param row The row of the table from which the value is edited.
     * @param focusOnKey Determines if the initial focus should be set on key instead of value
     * @since 5653
@@ -186,7 +187,7 @@ class TagEditHelper {
     }
 
     /**
-     * Load recently used tags from preferences if needed
+     * Load recently used tags from preferences if needed.
      */
     public void loadTagsIfNeeded() {
         if (PROPERTY_REMEMBER_TAGS.get() && recentTags.isEmpty()) {
@@ -203,7 +204,7 @@ class TagEditHelper {
     }
 
     /**
-     * Store recently used tags in preferences if needed
+     * Store recently used tags in preferences if needed.
      */
     public void saveTagsIfNeeded() {
         if (PROPERTY_REMEMBER_TAGS.get() && !recentTags.isEmpty()) {
@@ -257,24 +258,24 @@ class TagEditHelper {
 
         ListCellRenderer<AutoCompletionListItem> cellRenderer = new ListCellRenderer<AutoCompletionListItem>() {
             final DefaultListCellRenderer def = new DefaultListCellRenderer();
-                @Override
-                public Component getListCellRendererComponent(JList<? extends AutoCompletionListItem> list,
-                        AutoCompletionListItem value, int index, boolean isSelected,  boolean cellHasFocus){
-                    Component c = def.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                    if (c instanceof JLabel) {
-                        String str = value.getValue();
-                        if (valueCount.containsKey(objKey)) {
-                            Map<String, Integer> m = valueCount.get(objKey);
-                            if (m.containsKey(str)) {
-                                str = tr("{0} ({1})", str, m.get(str));
-                                c.setFont(c.getFont().deriveFont(Font.ITALIC + Font.BOLD));
-                            }
+            @Override
+            public Component getListCellRendererComponent(JList<? extends AutoCompletionListItem> list,
+                    AutoCompletionListItem value, int index, boolean isSelected,  boolean cellHasFocus){
+                Component c = def.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                if (c instanceof JLabel) {
+                    String str = value.getValue();
+                    if (valueCount.containsKey(objKey)) {
+                        Map<String, Integer> m = valueCount.get(objKey);
+                        if (m.containsKey(str)) {
+                            str = tr("{0} ({1})", str, m.get(str));
+                            c.setFont(c.getFont().deriveFont(Font.ITALIC + Font.BOLD));
                         }
-                        ((JLabel) c).setText(str);
                     }
-                    return c;
+                    ((JLabel) c).setText(str);
                 }
-            };
+                return c;
+            }
+        };
 
         private EditTagDialog(String key, int row, Map<String, Integer> map, final boolean initialFocusOnKey) {
             super(Main.parent, trn("Change value?", "Change values?", map.size()), new String[] {tr("OK"),tr("Cancel")});
@@ -462,7 +463,7 @@ class TagEditHelper {
         }
 
         private void selectACComboBoxSavingUnixBuffer(AutoCompletingComboBox cb) {
-            // select compbobox with saving unix system selection (middle mouse paste)
+            // select combobox with saving unix system selection (middle mouse paste)
             Clipboard sysSel = Toolkit.getDefaultToolkit().getSystemSelection();
             if(sysSel != null) {
                 Transferable old = sysSel.getContents(null);
@@ -492,11 +493,11 @@ class TagEditHelper {
         */
         protected FocusAdapter addFocusAdapter(final AutoCompletionManager autocomplete, final Comparator<AutoCompletionListItem> comparator) {
            // get the combo box' editor component
-           JTextComponent editor = (JTextComponent)values.getEditor()
-                   .getEditorComponent();
+           JTextComponent editor = (JTextComponent)values.getEditor().getEditorComponent();
            // Refresh the values model when focus is gained
            FocusAdapter focus = new FocusAdapter() {
-               @Override public void focusGained(FocusEvent e) {
+               @Override
+               public void focusGained(FocusEvent e) {
                    String key = keys.getEditor().getItem().toString();
 
                    List<AutoCompletionListItem> valueList = autocomplete.getValues(getAutocompletionKeys(key));
@@ -664,8 +665,7 @@ class TagEditHelper {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         keys.setSelectedItem(t.getKey());
-                        // Update list of values (fix #7951)
-                        // fix #8298 - update list of values before setting value (?)
+                        // fix #7951, #8298 - update list of values before setting value (?)
                         focus.focusGained(null);
                         values.setSelectedItem(t.getValue());
                         selectValuesCombobox();

src/org/openstreetmap/josm/gui/util/AdvancedKeyPressDetector.java
@@ -38,6 +38,8 @@ public class AdvancedKeyPressDetector implements AWTEventListener {
     private final ArrayList<ModifierListener> modifierListeners = new ArrayList<>();
     private int previousModifiers;
 
+    private boolean enabled = true;
+
     /**
      * Adds an object that wants to receive key press and release events.
      * @param l listener to add
@@ -83,7 +85,7 @@ public class AdvancedKeyPressDetector implements AWTEventListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 timer.stop();
-                if (set.remove(releaseEvent.getKeyCode())) {
+                if (set.remove(releaseEvent.getKeyCode()) && enabled) {
                     synchronized (AdvancedKeyPressDetector.this) {
                         for (KeyPressReleaseListener q: keyListeners) {
                             q.doKeyReleased(releaseEvent);
@@ -120,7 +122,7 @@ public class AdvancedKeyPressDetector implements AWTEventListener {
         if (e.getID() == KeyEvent.KEY_PRESSED) {
             if (timer.isRunning()) {
                 timer.stop();
-            } else if (set.add((e.getKeyCode()))) {
+            } else if (set.add((e.getKeyCode())) && enabled) {
                 synchronized (this) {
                     for (KeyPressReleaseListener q: keyListeners) {
                         q.doKeyPressed(e);
@@ -130,7 +132,7 @@ public class AdvancedKeyPressDetector implements AWTEventListener {
         } else if (e.getID() == KeyEvent.KEY_RELEASED) {
             if (timer.isRunning()) {
                 timer.stop();
-                if (set.remove(e.getKeyCode())) {
+                if (set.remove(e.getKeyCode()) && enabled) {
                     synchronized (this) {
                         for (KeyPressReleaseListener q: keyListeners) {
                             q.doKeyReleased(e);
@@ -176,5 +178,15 @@ public class AdvancedKeyPressDetector implements AWTEventListener {
      */
     public boolean isKeyPressed(int keyCode) {
         return set.contains(keyCode);
+    }
+
+    /**
+     * Sets the enabled state of the key detector. We need to disable it when text fields that disable
+     * shortcuts gain focus.
+     * @param enabled if {@code true}, enables this key detector. If {@code false}, disables it
+     * @since 7539
+     */
+    public final void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 }

src/org/openstreetmap/josm/gui/util/GuiHelper.java
@@ -18,6 +18,7 @@ import java.awt.Window;
 import java.awt.event.ActionListener;
 import java.awt.event.HierarchyEvent;
 import java.awt.event.HierarchyListener;
+import java.awt.event.KeyEvent;
 import java.awt.image.FilteredImageSource;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -328,5 +329,20 @@ public final class GuiHelper {
      */
     public static JScrollPane embedInVerticalScrollPane(Component panel) {
         return new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    }
+
+    /**
+     * Returns extended modifier key used as the appropriate accelerator key for menu shortcuts.
+     * It is advised everywhere to use {@link Toolkit#getMenuShortcutKeyMask()} to get the cross-platform modifier, but:
+     * <ul>
+     * <li>it returns KeyEvent.CTRL_MASK instead of KeyEvent.CTRL_DOWN_MASK. We used the extended
+     *    modifier for years, and Oracle recommends to use it instead, so it's best to keep it</li>
+     * <li>the method throws a HeadlessException ! So we would need to handle it for unit tests anyway</li>
+     * </ul>
+     * @return extended modifier key used as the appropriate accelerator key for menu shortcuts
+     * @since 7539
+     */
+    public static int getMenuShortcutKeyMaskEx() {
+        return Main.isPlatformOsx() ? KeyEvent.META_DOWN_MASK : KeyEvent.CTRL_DOWN_MASK;
     }
 }

src/org/openstreetmap/josm/io/remotecontrol/AddTagsDialog.java
@@ -17,9 +17,9 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
 import javax.swing.AbstractAction;
 import javax.swing.JCheckBox;
-
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.KeyStroke;
@@ -68,6 +68,7 @@ public class AddTagsDialog extends ExtendedDialog implements SelectionChangedLis
         public DeleteTagMarker(int num) {
             this.num = num;
         }
+        @Override
         public String toString() {
             return tr("<delete from {0} objects>", num);
         }
@@ -328,9 +329,11 @@ public class AddTagsDialog extends ExtendedDialog implements SelectionChangedLis
     }
 
     /**
-     * Ask user and add the tags he confirm
+     * Ask user and add the tags he confirm.
      * @param keyValue is a table or {{tag1,val1},{tag2,val2},...}
      * @param sender is a string for skipping confirmations. Use epmty string for always confirmed adding.
+     * @param primitives OSM objects that will be modified
+     * @since 7521
      */
     public static void addTags(String[][] keyValue, String sender, Collection<? extends OsmPrimitive> primitives) {
         if (trustedSenders.contains(sender)) {

src/org/openstreetmap/josm/tools/InputMapUtils.java
@@ -12,8 +12,8 @@ import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 /**
- * Tools to work with Swing InputMap
- *
+ * Tools to work with Swing InputMap.
+ * @since 5200
  */
 public final class InputMapUtils {
 
@@ -61,19 +61,30 @@ public final class InputMapUtils {
     }
 
     /**
-     * Enable activating button on Enter (which is replaced with spacebar for certain Look-And-Feels)
+     * Enable activating button on Enter (which is replaced with spacebar for certain Look-And-Feels).
+     * @param b Button
      */
     public static void enableEnter(JButton b) {
          b.setFocusable(true);
          b.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter");
-         b.getActionMap().put("enter",b.getAction());
+         b.getActionMap().put("enter", b.getAction());
     }
 
+    /**
+     * Add an action activated with Enter key on a component.
+     * @param c The Swing component
+     * @param a action activated with Enter key
+     */
     public static void addEnterAction(JComponent c, Action a) {
          c.getActionMap().put("enter", a);
          c.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter");
     }
 
+    /**
+     * Add an action activated with Spacebar key on a component.
+     * @param c The Swing component
+     * @param a action activated with Spacebar key
+     */
     public static void addSpacebarAction(JComponent c, Action a) {
          c.getActionMap().put("spacebar", a);
          c.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0), "spacebar");

commit 3b8d9f4a835a2fe8309baf571b37b82ccaee0346

fix #10633 - fix disabled shortcuts after focusing relation filter, command line field, etc. Regression from r7539 + r7505

src/org/openstreetmap/josm/gui/widgets/DisableShortcutsOnFocusGainedTextField.java
@@ -2,7 +2,6 @@
 package org.openstreetmap.josm.gui.widgets;
 
 import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -26,7 +25,7 @@ import org.openstreetmap.josm.tools.Shortcut;
  * This allows to include text fields in toggle dialogs (needed for relation filter).
  * @since 5696
  */
-public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implements FocusListener {
+public class DisableShortcutsOnFocusGainedTextField extends JosmTextField {
 
     /**
      * Constructs a new <code>TextField</code>.  A default model is created,
@@ -34,7 +33,6 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
      * and the number of columns is set to 0.
      */
     public DisableShortcutsOnFocusGainedTextField() {
-        init();
     }
 
     /**
@@ -46,7 +44,6 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
      */
     public DisableShortcutsOnFocusGainedTextField(String text) {
         super(text);
-        init();
     }
 
     /**
@@ -62,7 +59,6 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
      */
     public DisableShortcutsOnFocusGainedTextField(int columns) {
         super(columns);
-        init();
     }
 
     /**
@@ -77,7 +73,6 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
      */
     public DisableShortcutsOnFocusGainedTextField(String text, int columns) {
         super(text, columns);
-        init();
     }
 
     /**
@@ -98,18 +93,14 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
      */
     public DisableShortcutsOnFocusGainedTextField(Document doc, String text, int columns) {
         super(doc, text, columns);
-        init();
     }
 
     private final List<Pair<Action,Shortcut>> unregisteredActionShortcuts = new ArrayList<>();
     private final Set<JosmAction> disabledMenuActions = new HashSet<>();
 
-    protected final void init() {
-        addFocusListener(this);
-    }
-
     @Override
     public void focusGained(FocusEvent e) {
+        super.focusGained(e);
         disableMenuActions();
         unregisterActionShortcuts();
         Main.map.keyDetector.setEnabled(false);
@@ -117,6 +108,7 @@ public class DisableShortcutsOnFocusGainedTextField extends JosmTextField implem
 
     @Override
     public void focusLost(FocusEvent e) {
+        super.focusLost(e);
         Main.map.keyDetector.setEnabled(true);
         restoreActionShortcuts();
         restoreMenuActions();