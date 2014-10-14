tsmock/josm

commit 4255c82b960ba6b03c15957c31df1c37fbaadfa2

fix #10054 - Multipolygons as members not highlighted when relation is selected
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7555 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/visitor/paint/StyledMapRenderer.java
@@ -309,7 +309,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
     private boolean useStrokes;
     private boolean showNames;
     private boolean showIcons;
-    private boolean  isOutlineOnly;
+    private boolean isOutlineOnly;
 
     private Font orderFont;
 
@@ -1498,7 +1498,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         public void visit(Node n) {
             if (n.isDisabled()) {
                 add(n, FLAG_DISABLED);
-            } else if (data.isSelected(n)) {
+            } else if (n.isSelected()) {
                 add(n, FLAG_SELECTED);
             } else if (n.isMemberOfSelected()) {
                 add(n, FLAG_MEMBER_OF_SELECTED);
@@ -1511,7 +1511,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         public void visit(Way w) {
             if (w.isDisabled()) {
                 add(w, FLAG_DISABLED);
-            } else if (data.isSelected(w)) {
+            } else if (w.isSelected()) {
                 add(w, FLAG_SELECTED);
             } else if (w.isMemberOfSelected()) {
                 add(w, FLAG_MEMBER_OF_SELECTED);
@@ -1524,8 +1524,10 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         public void visit(Relation r) {
             if (r.isDisabled()) {
                 add(r, FLAG_DISABLED);
-            } else if (data.isSelected(r)) {
+            } else if (r.isSelected()) {
                 add(r, FLAG_SELECTED);
+            } else if (r.isMemberOfSelected()) {
+                add(r, FLAG_MEMBER_OF_SELECTED);
             } else {
                 add(r, FLAG_NORMAL);
             }

src/org/openstreetmap/josm/gui/mappaint/AreaElemStyle.java
@@ -86,15 +86,18 @@ public class AreaElemStyle extends ElemStyle {
 
     @Override
     public void paintPrimitive(OsmPrimitive osm, MapPaintSettings paintSettings, StyledMapRenderer painter, boolean selected, boolean member) {
+        Color myColor = color;
         if (osm instanceof Way) {
-            Color myColor = color;
-            if (color != null && osm.isSelected()) {
-                myColor = paintSettings.getSelectedColor(color.getAlpha());
+            if (color != null) {
+                if (selected) {
+                    myColor = paintSettings.getSelectedColor(color.getAlpha());
+                } else if (member) {
+                    myColor = paintSettings.getRelationSelectedColor(color.getAlpha());
+                }
             }
             painter.drawArea((Way) osm, myColor, fillImage, text);
         } else if (osm instanceof Relation) {
-            Color myColor = color;
-            if (color != null && selected) {
+            if (color != null && (selected || (member && ((Relation) osm).isMultipolygon()))) {
                 myColor = paintSettings.getRelationSelectedColor(color.getAlpha());
             }
             painter.drawArea((Relation) osm, myColor, fillImage, text);

src/org/openstreetmap/josm/data/osm/visitor/paint/WireframeMapRenderer.java
@@ -339,8 +339,10 @@ public class WireframeMapRenderer extends AbstractMapRenderer implements Visitor
         Color col;
         if (isInactiveMode || r.isDisabled()) {
             col = inactiveColor;
-        } else if (ds.isSelected(r)) {
+        } else if (r.isSelected()) {
             col = selectedColor;
+        } else if (r.isMultipolygon() && r.isMemberOfSelected()) {
+            col = relationSelectedColor;
         } else {
             col = relationColor;
         }

commit 2e9bb32abf022cef995e54a18a8200425541dfe6

fix #10615 - Inner ways of multipolygon not correctly displayed (regression from r7555)

src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
@@ -589,6 +589,10 @@ public abstract class OsmPrimitive extends AbstractPrimitive implements Comparab
         return dataSet != null && dataSet.isSelected(this);
     }
 
+    /**
+     * Determines if this primitive is a member of a selected relation.
+     * @return {@code true} if this primitive is a member of a selected relation, {@code false} otherwise
+     */
     public boolean isMemberOfSelected() {
         if (referrers == null)
             return false;
@@ -597,6 +601,35 @@ public abstract class OsmPrimitive extends AbstractPrimitive implements Comparab
         for (OsmPrimitive ref : (OsmPrimitive[]) referrers) {
             if (ref instanceof Relation && ref.isSelected())
                 return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determines if this primitive is an outer member of a selected multipolygon relation.
+     * @return {@code true} if this primitive is an outer member of a selected multipolygon relation, {@code false} otherwise
+     * @since 7621
+     */
+    public boolean isOuterMemberOfSelected() {
+        if (referrers == null)
+            return false;
+        if (referrers instanceof OsmPrimitive) {
+            return isOuterMemberOfMultipolygon((OsmPrimitive) referrers);
+        }
+        for (OsmPrimitive ref : (OsmPrimitive[]) referrers) {
+            if (isOuterMemberOfMultipolygon(ref))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isOuterMemberOfMultipolygon(OsmPrimitive ref) {
+        if (ref instanceof Relation && ref.isSelected() && ((Relation)ref).isMultipolygon()) {
+            for (RelationMember rm : ((Relation)ref).getMembersFor(Collections.singleton(this))) {
+                if ("outer".equals(rm.getRole())) {
+                    return true;
+                }
+            }
         }
         return false;
     }

src/org/openstreetmap/josm/data/osm/visitor/paint/StyledMapRenderer.java
@@ -290,6 +290,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
     private static final int FLAG_DISABLED = 1;
     private static final int FLAG_MEMBER_OF_SELECTED = 2;
     private static final int FLAG_SELECTED = 4;
+    private static final int FLAG_OUTERMEMBER_OF_SELECTED = 8;
 
     private static final double PHI = Math.toRadians(20);
     private static final double cosPHI = Math.cos(PHI);
@@ -1513,6 +1514,8 @@ public class StyledMapRenderer extends AbstractMapRenderer {
                 add(w, FLAG_DISABLED);
             } else if (w.isSelected()) {
                 add(w, FLAG_SELECTED);
+            } else if (w.isOuterMemberOfSelected()) {
+                add(w, FLAG_OUTERMEMBER_OF_SELECTED);
             } else if (w.isMemberOfSelected()) {
                 add(w, FLAG_MEMBER_OF_SELECTED);
             } else {
@@ -1526,6 +1529,8 @@ public class StyledMapRenderer extends AbstractMapRenderer {
                 add(r, FLAG_DISABLED);
             } else if (r.isSelected()) {
                 add(r, FLAG_SELECTED);
+            } else if (r.isOuterMemberOfSelected()) {
+                add(r, FLAG_OUTERMEMBER_OF_SELECTED);
             } else if (r.isMemberOfSelected()) {
                 add(r, FLAG_MEMBER_OF_SELECTED);
             } else {
@@ -1653,6 +1658,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
                         paintSettings,
                         StyledMapRenderer.this,
                         (r.flags & FLAG_SELECTED) != 0,
+                        (r.flags & FLAG_OUTERMEMBER_OF_SELECTED) != 0,
                         (r.flags & FLAG_MEMBER_OF_SELECTED) != 0
                 );
             }

src/org/openstreetmap/josm/gui/mappaint/AreaElemStyle.java
@@ -85,19 +85,20 @@ public class AreaElemStyle extends ElemStyle {
     }
 
     @Override
-    public void paintPrimitive(OsmPrimitive osm, MapPaintSettings paintSettings, StyledMapRenderer painter, boolean selected, boolean member) {
+    public void paintPrimitive(OsmPrimitive osm, MapPaintSettings paintSettings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member) {
         Color myColor = color;
         if (osm instanceof Way) {
             if (color != null) {
                 if (selected) {
                     myColor = paintSettings.getSelectedColor(color.getAlpha());
-                } else if (member) {
+                } else if (outermember) {
                     myColor = paintSettings.getRelationSelectedColor(color.getAlpha());
                 }
             }
             painter.drawArea((Way) osm, myColor, fillImage, text);
         } else if (osm instanceof Relation) {
-            if (color != null && (selected || (member && ((Relation) osm).isMultipolygon()))) {
+            if (color != null && (selected || outermember)) {
                 myColor = paintSettings.getRelationSelectedColor(color.getAlpha());
             }
             painter.drawArea((Relation) osm, myColor, fillImage, text);

src/org/openstreetmap/josm/gui/mappaint/BoxTextElemStyle.java
@@ -178,7 +178,8 @@ public class BoxTextElemStyle extends ElemStyle {
     }
 
     @Override
-    public void paintPrimitive(OsmPrimitive osm, MapPaintSettings settings, StyledMapRenderer painter, boolean selected, boolean member) {
+    public void paintPrimitive(OsmPrimitive osm, MapPaintSettings settings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member) {
         if (osm instanceof Node) {
             painter.drawBoxText((Node) osm, this);
         }

src/org/openstreetmap/josm/gui/mappaint/ElemStyle.java
@@ -42,9 +42,11 @@ public abstract class ElemStyle implements StyleKeys {
      * @param paintSettings
      * @param painter
      * @param selected true, if primitive is selected
+     * @param outermember true, if primitive is not selected and outer member of a selected multipolygon relation
      * @param member true, if primitive is not selected and member of a selected relation
      */
-    public abstract void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter, boolean selected, boolean member);
+    public abstract void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member);
 
     public boolean isProperLineStyle() {
         return false;

src/org/openstreetmap/josm/gui/mappaint/LineElemStyle.java
@@ -263,7 +263,8 @@ public class LineElemStyle extends ElemStyle {
     }
 
     @Override
-    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter, boolean selected, boolean member) {
+    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member) {
         Way w = (Way)primitive;
         /* show direction arrows, if draw.segment.relevant_directions_only is not set,
         the way is tagged with a direction key
@@ -296,7 +297,7 @@ public class LineElemStyle extends ElemStyle {
         Color myColor = color;
         if (selected) {
             myColor = paintSettings.getSelectedColor(color.getAlpha());
-        } else if (member) {
+        } else if (member || outermember) {
             myColor = paintSettings.getRelationSelectedColor(color.getAlpha());
         } else if(w.isDisabled()) {
             myColor = paintSettings.getInactiveColor();

src/org/openstreetmap/josm/gui/mappaint/LineTextElemStyle.java
@@ -31,7 +31,8 @@ public class LineTextElemStyle extends ElemStyle {
     }
 
     @Override
-    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter, boolean selected, boolean member) {
+    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member) {
         Way w = (Way)primitive;
         painter.drawTextOnPath(w, text);
     }

src/org/openstreetmap/josm/gui/mappaint/NodeElemStyle.java
@@ -240,7 +240,8 @@ public class NodeElemStyle extends ElemStyle implements StyleKeys {
     }
 
     @Override
-    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings settings, StyledMapRenderer painter, boolean selected, boolean member) {
+    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings settings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member) {
         if (primitive instanceof Node) {
             Node n = (Node) primitive;
             if (mapImage != null && painter.isShowIcons()) {

src/org/openstreetmap/josm/gui/mappaint/RepeatImageElemStyle.java
@@ -49,7 +49,8 @@ public class RepeatImageElemStyle extends ElemStyle implements StyleKeys {
     }
 
     @Override
-    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter, boolean selected, boolean member) {
+    public void paintPrimitive(OsmPrimitive primitive, MapPaintSettings paintSettings, StyledMapRenderer painter,
+            boolean selected, boolean outermember, boolean member) {
         Way w = (Way) primitive;
         painter.drawRepeatImage(w, pattern.getImage(), offset, spacing, phase, align);
     }