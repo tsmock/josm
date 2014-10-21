tsmock/josm

commit 1288bb2cd81043c81c37c16bdddc87f825d0e8e0

fix #9655 - Merge operations loosing tags (regression of r6770)
Drawback: when combining `highway=X` with `highway=X tunnel=yes`, `tunnel=yes` is auto selected for the conflict resolution (as it was before r6770.

git-svn-id: https://josm.openstreetmap.de/svn/trunk@6802 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/conflict/tags/TagConflictResolverModel.java
@@ -257,20 +257,17 @@ public class TagConflictResolverModel extends DefaultTableModel {
      *
      */
     public void prepareDefaultTagDecisions() {
-        // Do not suggest to keep all values in order to reduce the wrong usage of semicolon values, see #9104!
-        // Do not suggest to keep the single value in order to avoid long highways to become tunnels+bridges+...
-
-        /*
         for (MultiValueResolutionDecision decision: decisions.values()) {
             List<String> values = decision.getValues();
             values.remove("");
             if (values.size() == 1) {
+                // TODO: Do not suggest to keep the single value in order to avoid long highways to become tunnels+bridges+... (only if both primitives are tagged)
                 decision.keepOne(values.get(0));
             } else {
-                decision.keepAll();
+                // Do not suggest to keep all values in order to reduce the wrong usage of semicolon values, see #9104!
+                //decision.keepAll();
             }
         }
-        */
         rebuild();
     }
     

src/org/openstreetmap/josm/gui/conflict/tags/CombinePrimitiveResolverDialog.java
@@ -233,6 +233,16 @@ public class CombinePrimitiveResolverDialog extends JDialog {
         return pnlRelationMemberConflictResolver.getModel();
     }
 
+    /**
+     * Replies true if all tag and relation member conflicts have been decided.
+     *
+     * @return true if all tag and relation member conflicts have been decided; false otherwise
+     */
+    public boolean isResolvedCompletely() {
+        return getTagConflictResolverModel().isResolvedCompletely()
+                && getRelationMemberConflictResolverModel().isResolvedCompletely();
+    }
+
     protected List<Command> buildTagChangeCommand(OsmPrimitive primitive, TagCollection tc) {
         LinkedList<Command> cmds = new LinkedList<Command>();
         for (String key : tc.getKeys()) {
@@ -516,7 +526,7 @@ public class CombinePrimitiveResolverDialog extends JDialog {
         }
 
         // Resolve tag conflicts if necessary
-        if (!completeWayTags.isApplicableToPrimitive() || !parentRelations.isEmpty()) {
+        if (!dialog.isResolvedCompletely()) {
             dialog.setVisible(true);
             if (dialog.isCanceled()) {
                 throw new UserCancelException();

src/org/openstreetmap/josm/gui/conflict/tags/RelationMemberConflictResolverModel.java
@@ -38,6 +38,16 @@ public class RelationMemberConflictResolverModel extends DefaultTableModel {
     private PropertyChangeSupport support;
 
     /**
+     * Replies true if each {@link MultiValueResolutionDecision} is decided.
+     *
+     * @return true if each {@link MultiValueResolutionDecision} is decided; false
+     * otherwise
+     */
+    public boolean isResolvedCompletely() {
+        return numConflicts == 0;
+    }
+
+    /**
      * Replies the current number of conflicts
      *
      * @return the current number of conflicts

commit 5c4622a23510ce32b11bfa42912a4bb1a01fe282

fix #10156 - combine ways: conflicts not shown when no conflicting values (regression from r6802)

src/org/openstreetmap/josm/gui/conflict/tags/TagConflictResolverModel.java
@@ -190,11 +190,10 @@ public class TagConflictResolverModel extends DefaultTableModel {
     /**
      * Replies true if each {@link MultiValueResolutionDecision} is decided.
      *
-     * @return true if each {@link MultiValueResolutionDecision} is decided; false
-     * otherwise
+     * @return true if each {@link MultiValueResolutionDecision} is decided; false otherwise
      */
     public boolean isResolvedCompletely() {
-        return numConflicts == 0;
+        return numConflicts == 0 && keysWithConflicts.isEmpty();
     }
 
     public int getNumConflicts() {