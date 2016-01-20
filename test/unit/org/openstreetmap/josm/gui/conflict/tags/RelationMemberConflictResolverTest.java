tsmock/josm

commit 9f9e576ca20f00aaeb139e211f8468a410c6adf7

refactoring - global simplification of use of `setLayout` method - simply pass layout to `JPanel` constructor
git-svn-id: https://josm.openstreetmap.de/svn/trunk@9543 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/conflict/tags/RelationMemberConflictResolver.java
@@ -49,8 +49,7 @@ public class RelationMemberConflictResolver extends JPanel {
 
     protected final void build() {
         setLayout(new GridBagLayout());
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new BorderLayout());
+        JPanel pnl = new JPanel(new BorderLayout());
         pnl.add(lblHeader = new JMultilineLabel(""));
         GridBagConstraints gc = new GridBagConstraints();
         gc.fill = GridBagConstraints.HORIZONTAL;
@@ -64,8 +63,7 @@ public class RelationMemberConflictResolver extends JPanel {
         gc.fill = GridBagConstraints.BOTH;
         gc.insets = new Insets(0, 0, 0, 0);
         add(new JScrollPane(tblResolver = new RelationMemberConflictResolverTable(model)), gc);
-        pnl = new JPanel();
-        pnl.setLayout(new BoxLayout(pnl, BoxLayout.Y_AXIS));
+        pnl = new JPanel(new BoxLayout(pnl, BoxLayout.Y_AXIS));
         pnl.add(buildRoleEditingPanel());
         pnl.add(buildTagRelationsPanel());
         gc.gridy = 2;
@@ -75,8 +73,7 @@ public class RelationMemberConflictResolver extends JPanel {
     }
 
     protected JPanel buildRoleEditingPanel() {
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JPanel pnl = new JPanel(new FlowLayout(FlowLayout.LEFT));
         pnl.add(new JLabel(tr("Role:")));
         pnl.add(tfRole = new AutoCompletingTextField(10));
         tfRole.setToolTipText(tr("Enter a role for all relation memberships"));
@@ -94,8 +91,7 @@ public class RelationMemberConflictResolver extends JPanel {
     }
 
     protected JPanel buildTagRelationsPanel() {
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JPanel pnl = new JPanel(new FlowLayout(FlowLayout.LEFT));
         cbTagRelations = new JCheckBox(tr("Tag modified relations with "));
         cbTagRelations.addChangeListener(new ToggleTagRelationsAction());
         cbTagRelations.setToolTipText(

src/org/openstreetmap/josm/actions/upload/RelationUploadOrderHook.java
@@ -37,8 +37,7 @@ public class RelationUploadOrderHook implements UploadHook {
      * @return the panel
      */
     protected JPanel buildWarningPanel(List<Relation> dep) {
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new BorderLayout());
+        JPanel pnl = new JPanel(new BorderLayout());
         String msg = tr("<html>{0} relations build a cycle because they refer to each other.<br>"
                 + "JOSM cannot upload them. Please edit the relations and remove the "
                 + "cyclic dependency.</html>", dep.size()-1);

src/org/openstreetmap/josm/gui/MapFrame.java
@@ -181,8 +181,7 @@ public class MapFrame extends JPanel implements Destroyable, LayerChangeListener
 
         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true);
 
-        leftPanel = new JPanel();
-        leftPanel.setLayout(new GridBagLayout());
+        leftPanel = new JPanel(new GridBagLayout());
         leftPanel.add(mapView, GBC.std().fill());
         splitPane.setLeftComponent(leftPanel);
 

src/org/openstreetmap/josm/gui/PleaseWaitDialog.java
@@ -47,8 +47,7 @@ public class PleaseWaitDialog extends JDialog implements ProgressMonitorDialog {
         pane.add(currentAction, GBC.eol().fill(GBC.HORIZONTAL));
         pane.add(customText, GBC.eol().fill(GBC.HORIZONTAL));
         pane.add(progressBar, GBC.eop().fill(GBC.HORIZONTAL));
-        JPanel buttons = new JPanel();
-        buttons.setLayout(new GridBagLayout());
+        JPanel buttons = new JPanel(new GridBagLayout());
         btnCancel = new JButton(tr("Cancel"));
         btnCancel.setIcon(ImageProvider.get("cancel"));
         btnCancel.setToolTipText(tr("Click to cancel the current operation"));

src/org/openstreetmap/josm/gui/SplashScreen.java
@@ -64,10 +64,9 @@ public class SplashScreen extends JFrame implements ChangeListener {
         contentPane.setBorder(margin);
 
         // Add a margin from the border to the content
-        JPanel innerContentPane = new JPanel();
+        JPanel innerContentPane = new JPanel(new GridBagLayout());
         innerContentPane.setBorder(new EmptyBorder(10, 10, 2, 10));
         contentPane.add(innerContentPane);
-        innerContentPane.setLayout(new GridBagLayout());
 
         // Add the logo
         JLabel logo = new JLabel(new ImageIcon(ImageProvider.get("logo.svg").getImage().getScaledInstance(128, 129, Image.SCALE_SMOOTH)));

src/org/openstreetmap/josm/gui/conflict/tags/PasteTagsConflictResolverDialog.java
@@ -102,7 +102,7 @@ public class PasteTagsConflictResolverDialog extends JDialog  implements Propert
         gc.fill = GridBagConstraints.BOTH;
         gc.weightx = 1.0;
         gc.weighty = 1.0;
-        getContentPane().add(pnlTagResolver = new JPanel(), gc);
+        getContentPane().add(pnlTagResolver = new JPanel(new BorderLayout()), gc);
         gc.gridx = 0;
         gc.gridy = 2;
         gc.fill = GridBagConstraints.HORIZONTAL;
@@ -112,8 +112,7 @@ public class PasteTagsConflictResolverDialog extends JDialog  implements Propert
     }
 
     protected JPanel buildButtonPanel() {
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JPanel pnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         // -- apply button
         ApplyAction applyAction = new ApplyAction();
@@ -131,8 +130,7 @@ public class PasteTagsConflictResolverDialog extends JDialog  implements Propert
     }
 
     protected JPanel buildSourceAndTargetInfoPanel() {
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new BorderLayout());
+        JPanel pnl = new JPanel(new BorderLayout());
         statisticsModel = new StatisticsTableModel();
         pnl.add(new StatisticsInfoTable(statisticsModel), BorderLayout.CENTER);
         return pnl;
@@ -173,7 +171,6 @@ public class PasteTagsConflictResolverDialog extends JDialog  implements Propert
         allPrimitivesResolver.getModel().prepareDefaultTagDecisions();
 
         // prepare the dialog with one tag resolver
-        pnlTagResolver.setLayout(new BorderLayout());
         pnlTagResolver.removeAll();
         pnlTagResolver.add(allPrimitivesResolver, BorderLayout.CENTER);
 
@@ -217,7 +214,6 @@ public class PasteTagsConflictResolverDialog extends JDialog  implements Propert
         initResolver(OsmPrimitiveType.WAY, tagsForWays, targetStatistics);
         initResolver(OsmPrimitiveType.RELATION, tagsForRelations, targetStatistics);
 
-        pnlTagResolver.setLayout(new BorderLayout());
         pnlTagResolver.removeAll();
         pnlTagResolver.add(tpResolvers, BorderLayout.CENTER);
         mode = Mode.RESOLVING_TYPED_TAGCOLLECTIONS;

src/org/openstreetmap/josm/gui/conflict/tags/TagConflictResolver.java
@@ -32,9 +32,8 @@ public class TagConflictResolver extends JPanel {
     private JCheckBox cbShowTagsWithMultiValuesOnly;
 
     protected JPanel buildInfoPanel() {
-        JPanel pnl = new JPanel();
+        JPanel pnl = new JPanel(new GridBagLayout());
         pnl.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        pnl.setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
         gc.fill = GridBagConstraints.BOTH;
         gc.weighty = 1.0;

src/org/openstreetmap/josm/gui/dialogs/ConflictResolutionDialog.java
@@ -76,8 +76,7 @@ public class ConflictResolutionDialog extends JDialog implements PropertyChangeL
      * @return the panel
      */
     protected JPanel buildButtonRow() {
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JPanel pnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         applyResolutionAction = new ApplyResolutionAction();
         JButton btn = new JButton(applyResolutionAction);

commit 04f92eaf0358698ed456ad67756b664236de132f

fix #12405 - wrong JPanel initialization (regression from r9543)

src/org/openstreetmap/josm/gui/conflict/tags/RelationMemberConflictResolver.java
@@ -49,7 +49,7 @@ public class RelationMemberConflictResolver extends JPanel {
 
     protected final void build() {
         setLayout(new GridBagLayout());
-        JPanel pnl = new JPanel(new BorderLayout());
+        final JPanel pnl = new JPanel(new BorderLayout());
         pnl.add(lblHeader = new JMultilineLabel(""));
         GridBagConstraints gc = new GridBagConstraints();
         gc.fill = GridBagConstraints.HORIZONTAL;
@@ -63,13 +63,15 @@ public class RelationMemberConflictResolver extends JPanel {
         gc.fill = GridBagConstraints.BOTH;
         gc.insets = new Insets(0, 0, 0, 0);
         add(new JScrollPane(tblResolver = new RelationMemberConflictResolverTable(model)), gc);
-        pnl = new JPanel(new BoxLayout(pnl, BoxLayout.Y_AXIS));
-        pnl.add(buildRoleEditingPanel());
-        pnl.add(buildTagRelationsPanel());
+
+        final JPanel pnl2 = new JPanel();
+        pnl2.setLayout(new BoxLayout(pnl2, BoxLayout.Y_AXIS));
+        pnl2.add(buildRoleEditingPanel());
+        pnl2.add(buildTagRelationsPanel());
         gc.gridy = 2;
         gc.weighty = 0.0;
         gc.fill = GridBagConstraints.HORIZONTAL;
-        add(pnl, gc);
+        add(pnl2, gc);
     }
 
     protected JPanel buildRoleEditingPanel() {

test/unit/org/openstreetmap/josm/gui/conflict/tags/RelationMemberConflictResolverTest.java
@@ -1,0 +1,30 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.conflict.tags;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.josm.JOSMFixture;
+
+/**
+ * Unit tests of {@link RelationMemberConflictResolver} class.
+ */
+public class RelationMemberConflictResolverTest {
+
+    /**
+     * Setup test.
+     */
+    @BeforeClass
+    public static void init() {
+        JOSMFixture.createUnitTestFixture().init();
+    }
+
+    /**
+     * Unit test for {@link RelationMemberConflictResolver#RelationMemberConflictResolver}.
+     */
+    @Test
+    public void testRelationMemberConflictResolver() {
+        assertNotNull(new RelationMemberConflictResolver(null));
+    }
+}