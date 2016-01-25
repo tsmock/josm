tsmock/josm

commit 00adb838970208a0b675710af266348a6ec0757f

see #12412 - improve support of multipolygon presets type
git-svn-id: https://josm.openstreetmap.de/svn/trunk@9588 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableModel.java
@@ -53,6 +53,7 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
      **/
     private final transient List<RelationMember> members;
     private transient List<WayConnectionType> connectionType;
+    private final Relation relation;
 
     private DefaultListSelectionModel listSelectionModel;
     private final CopyOnWriteArrayList<IMemberModelListener> listeners;
@@ -64,12 +65,14 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
 
     /**
      * constructor
+     * @param relation relation
      * @param layer data layer
      * @param presetHandler tagging preset handler
      */
-    public MemberTableModel(OsmDataLayer layer, TaggingPresetHandler presetHandler) {
-        members = new ArrayList<>();
-        listeners = new CopyOnWriteArrayList<>();
+    public MemberTableModel(Relation relation, OsmDataLayer layer, TaggingPresetHandler presetHandler) {
+        this.relation = relation;
+        this.members = new ArrayList<>();
+        this.listeners = new CopyOnWriteArrayList<>();
         this.layer = layer;
         this.presetHandler = presetHandler;
         addTableModelListener(this);
@@ -113,6 +116,7 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
         // just trigger a repaint - the display name of the relation members may have changed
         Collection<RelationMember> sel = getSelectedMembers();
         GuiHelper.runInEDT(new Runnable() {
+            @Override
             public void run() {
                 fireTableDataChanged();
             }
@@ -436,7 +440,7 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
     }
 
     RelationMember getRelationMemberForPrimitive(final OsmPrimitive primitive) {
-        final Collection<TaggingPreset> presets = TaggingPresets.getMatchingPresets(EnumSet.of(TaggingPresetType.RELATION),
+        final Collection<TaggingPreset> presets = TaggingPresets.getMatchingPresets(EnumSet.of(TaggingPresetType.forPrimitive(relation)),
                 presetHandler.getSelection().iterator().next().getKeys(), false);
         Collection<String> potentialRoles = new TreeSet<>();
         for (TaggingPreset tp : presets) {

src/org/openstreetmap/josm/gui/dialogs/properties/PresetListPanel.java
@@ -35,7 +35,7 @@ public class PresetListPanel extends JPanel {
      * Updates the preset list based on the {@code tags} and {@code types},
      * and associates an interaction with (matching) presets via {@code presetHandler}.
      * @param types collection of tagging presets types
-     * @param tags colelction of tags
+     * @param tags collection of tags
      * @param presetHandler tagging preset handler
      */
     public void updatePresets(final Collection<TaggingPresetType> types, final Map<String, String> tags,

src/org/openstreetmap/josm/gui/dialogs/relation/GenericRelationEditor.java
@@ -164,13 +164,13 @@ public class GenericRelationEditor extends RelationEditor  {
 
         // init the various models
         //
-        memberTableModel = new MemberTableModel(getLayer(), presetHandler);
+        memberTableModel = new MemberTableModel(relation, getLayer(), presetHandler);
         memberTableModel.register();
         selectionTableModel = new SelectionTableModel(getLayer());
         selectionTableModel.register();
         referrerModel = new ReferringRelationsBrowserModel(relation);
 
-        tagEditorPanel = new TagEditorPanel(presetHandler);
+        tagEditorPanel = new TagEditorPanel(relation, presetHandler);
 
         // populate the models
         //
@@ -804,7 +804,7 @@ public class GenericRelationEditor extends RelationEditor  {
         CheckParameterUtil.ensureParameterNotNull(orig, "orig");
         try {
             final Collection<TaggingPreset> presets = TaggingPresets.getMatchingPresets(
-                    EnumSet.of(TaggingPresetType.RELATION), orig.getKeys(), false);
+                    EnumSet.of(TaggingPresetType.forPrimitive(orig)), orig.getKeys(), false);
             Relation relation = new Relation(orig);
             boolean modified = false;
             for (OsmPrimitive p : primitivesToAdd) {

src/org/openstreetmap/josm/gui/tagging/TagEditorModel.java
@@ -8,6 +8,7 @@ import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -24,11 +25,12 @@ import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Tag;
 import org.openstreetmap.josm.data.osm.TagCollection;
 import org.openstreetmap.josm.data.osm.Tagged;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetType;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 
 /**
- * TagEditorModel is a table model.
- *
+ * TagEditorModel is a table model to use with {@link TagEditorPanel}.
+ * @since 1762
  */
 public class TagEditorModel extends AbstractTableModel {
     public static final String PROP_DIRTY = TagEditorModel.class.getName() + ".dirty";
@@ -42,6 +44,8 @@ public class TagEditorModel extends AbstractTableModel {
 
     private final DefaultListSelectionModel rowSelectionModel;
     private final DefaultListSelectionModel colSelectionModel;
+
+    private OsmPrimitive primitive;
 
     /**
      * Creates a new tag editor model. Internally allocates two selection models
@@ -57,8 +61,7 @@ public class TagEditorModel extends AbstractTableModel {
      * @see #getColumnSelectionModel()
      */
     public TagEditorModel() {
-        this.rowSelectionModel = new DefaultListSelectionModel();
-        this.colSelectionModel  = new DefaultListSelectionModel();
+        this(new DefaultListSelectionModel(), new DefaultListSelectionModel());
     }
 
     /**
@@ -73,7 +76,7 @@ public class TagEditorModel extends AbstractTableModel {
         CheckParameterUtil.ensureParameterNotNull(rowSelectionModel, "rowSelectionModel");
         CheckParameterUtil.ensureParameterNotNull(colSelectionModel, "colSelectionModel");
         this.rowSelectionModel = rowSelectionModel;
-        this.colSelectionModel  = colSelectionModel;
+        this.colSelectionModel = colSelectionModel;
     }
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
@@ -615,6 +618,29 @@ public class TagEditorModel extends AbstractTableModel {
      */
     public boolean isDirty() {
         return dirty;
+    }
+
+    /**
+     * Returns the list of tagging presets types to consider when updating the presets list panel.
+     * By default returns type of associated primitive or empty set.
+     * @return the list of tagging presets types to consider when updating the presets list panel
+     * @see #forPrimitive
+     * @see TaggingPresetType#forPrimitive
+     * @since 9588
+     */
+    public Collection<TaggingPresetType> getTaggingPresetTypes() {
+        return primitive == null ? EnumSet.noneOf(TaggingPresetType.class) : EnumSet.of(TaggingPresetType.forPrimitive(primitive));
+    }
+
+    /**
+     * Makes this TagEditorModel specific to a given OSM primitive.
+     * @param primitive primitive to consider
+     * @return {@code this}
+     * @since 9588
+     */
+    public TagEditorModel forPrimitive(OsmPrimitive primitive) {
+        this.primitive = primitive;
+        return this;
     }
 
     class SelectionStateMemento {

src/org/openstreetmap/josm/gui/tagging/TagEditorPanel.java
@@ -8,7 +8,6 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.util.EnumSet;
 
 import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
@@ -18,12 +17,12 @@ import javax.swing.JScrollPane;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.dialogs.properties.PresetListPanel;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.gui.tagging.ac.AutoCompletionList;
 import org.openstreetmap.josm.gui.tagging.ac.AutoCompletionManager;
 import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetHandler;
-import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetType;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 
 /**
@@ -31,7 +30,7 @@ import org.openstreetmap.josm.tools.CheckParameterUtil;
  * UIs. It provides a spreadsheet like tabular control for editing tag names
  * and tag values. Two action buttons are placed on the left, one for adding
  * a new tag and one for deleting the currently selected tags.
- *
+ * @since 2040
  */
 public class TagEditorPanel extends JPanel {
     /** the tag editor model */
@@ -139,7 +138,8 @@ public class TagEditorPanel extends JPanel {
         }
 
         addFocusListener(new FocusAdapter() {
-            @Override public void focusGained(FocusEvent e) {
+            @Override
+            public void focusGained(FocusEvent e) {
                 tagTable.requestFocusInCell(0, 0);
             }
         });
@@ -148,10 +148,11 @@ public class TagEditorPanel extends JPanel {
     /**
      * Creates a new tag editor panel. The editor model is created
      * internally and can be retrieved with {@link #getModel()}.
+     * @param primitive primitive to consider
      * @param presetHandler tagging preset handler
      */
-    public TagEditorPanel(TaggingPresetHandler presetHandler) {
-        this(null, presetHandler, 0);
+    public TagEditorPanel(OsmPrimitive primitive, TaggingPresetHandler presetHandler) {
+        this(new TagEditorModel().forPrimitive(primitive), presetHandler, 0);
     }
 
     /**
@@ -197,7 +198,7 @@ public class TagEditorPanel extends JPanel {
         TagCellEditor editor = (TagCellEditor) tagTable.getColumnModel().getColumn(0).getCellEditor();
         editor.setAutoCompletionManager(autocomplete);
         editor.setAutoCompletionList(acList);
-        editor = ((TagCellEditor) tagTable.getColumnModel().getColumn(1).getCellEditor());
+        editor = (TagCellEditor) tagTable.getColumnModel().getColumn(1).getCellEditor();
         editor.setAutoCompletionManager(autocomplete);
         editor.setAutoCompletionList(acList);
     }
@@ -210,7 +211,7 @@ public class TagEditorPanel extends JPanel {
 
     private void updatePresets() {
         presetListPanel.updatePresets(
-                EnumSet.of(TaggingPresetType.RELATION),
+                model.getTaggingPresetTypes(),
                 model.getTags(), presetHandler);
         validate();
     }

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPresetType.java
@@ -65,7 +65,7 @@ public enum TaggingPresetType {
         if (type == OsmPrimitiveType.CLOSEDWAY)
             return CLOSEDWAY;
         if (type == OsmPrimitiveType.MULTIPOLYGON)
-            return TaggingPresetType.MULTIPOLYGON;
+            return MULTIPOLYGON;
         if (type == OsmPrimitiveType.RELATION)
             return RELATION;
         throw new IllegalArgumentException("Unexpected primitive type: " + type);

test/unit/org/openstreetmap/josm/gui/dialogs/relation/actions/RelationEditorActionsTest.java
@@ -32,12 +32,12 @@ public class RelationEditorActionsTest {
      */
     @Test
     public void testAllActions() {
-        DataSet ds = new DataSet();
-        OsmDataLayer layer = new OsmDataLayer(new DataSet(), "test", null);
-        MemberTableModel memberTableModel = new MemberTableModel(layer, null);
-        SelectionTableModel selectionTableModel = new SelectionTableModel(layer);
+        final DataSet ds = new DataSet();
         final Relation orig = new Relation(1);
         ds.addPrimitive(orig);
+        OsmDataLayer layer = new OsmDataLayer(new DataSet(), "test", null);
+        MemberTableModel memberTableModel = new MemberTableModel(orig, layer, null);
+        SelectionTableModel selectionTableModel = new SelectionTableModel(layer);
 
         RelationAware editor = new RelationAware() {
             private Relation r = orig;

commit 440ed72ed024cb984fc887fe4f4132c49bd6ca1e

fix #12443 - NPE (regression from r9588, see #12412)

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableModel.java
@@ -440,7 +440,8 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
     }
 
     RelationMember getRelationMemberForPrimitive(final OsmPrimitive primitive) {
-        final Collection<TaggingPreset> presets = TaggingPresets.getMatchingPresets(EnumSet.of(TaggingPresetType.forPrimitive(relation)),
+        final Collection<TaggingPreset> presets = TaggingPresets.getMatchingPresets(
+                EnumSet.of(relation != null ? TaggingPresetType.forPrimitive(relation) : TaggingPresetType.RELATION),
                 presetHandler.getSelection().iterator().next().getKeys(), false);
         Collection<String> potentialRoles = new TreeSet<>();
         for (TaggingPreset tp : presets) {

test/unit/org/openstreetmap/josm/gui/dialogs/relation/MemberTableModelTest.java
@@ -1,0 +1,49 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.dialogs.relation;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.josm.JOSMFixture;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Tag;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetHandler;
+
+/**
+ * Unit tests of {@link MemberTableModel} class.
+ */
+public class MemberTableModelTest {
+
+    /**
+     * Setup test.
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        JOSMFixture.createUnitTestFixture().init();
+    }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/12443">#12443</a>.
+     */
+    @Test
+    public void testTicket12443() {
+        final Node n = new Node(1);
+        assertNotNull(new MemberTableModel(null, null, new TaggingPresetHandler() {
+            @Override
+            public void updateTags(List<Tag> tags) {
+                // Do nothing
+            }
+
+            @Override
+            public Collection<OsmPrimitive> getSelection() {
+                return Collections.<OsmPrimitive>singleton(n);
+            }
+        }).getRelationMemberForPrimitive(n));
+    }
+}