tsmock/josm

commit a4821b47adf0f4fc3d7cf788ecef8b6c75355592

fix #11968 - Show node icons from style in selection list and relation editor (patch by Augustus Kling)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@8903 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableMemberCellRenderer.java
@@ -2,6 +2,7 @@
 package org.openstreetmap.josm.gui.dialogs.relation;
 
 import java.awt.Component;
+import java.awt.Rectangle;
 
 import javax.swing.JTable;
 
@@ -9,14 +10,21 @@ import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.DefaultNameFormatter;
 import org.openstreetmap.josm.tools.ImageProvider;
 
+/**
+ * "Refers to" column in relation editor's member list.
+ */
 public class MemberTableMemberCellRenderer extends MemberTableCellRenderer {
 
+	/**
+	 * Constructs a new {@code MemberTableMemberCellRenderer}.
+	 */
     public MemberTableMemberCellRenderer() {
         super();
     }
 
-    protected void renderPrimitive(OsmPrimitive primitive) {
-        setIcon(ImageProvider.get(primitive.getDisplayType()));
+    protected void renderPrimitive(OsmPrimitive primitive, Rectangle cellSize) {
+        // Make icon the full height of the table cell. Icon background is square.
+        setIcon(ImageProvider.getPadded(primitive, cellSize));
         setText(primitive.getDisplayName(DefaultNameFormatter.getInstance()));
         setToolTipText(DefaultNameFormatter.getInstance().buildDefaultToolTip(primitive));
     }
@@ -29,10 +37,12 @@ public class MemberTableMemberCellRenderer extends MemberTableCellRenderer {
         if (value == null)
             return this;
 
+        Rectangle cellSize = table.getCellRect(row, column, false);
+
         renderForeground(isSelected);
         OsmPrimitive primitive = (OsmPrimitive) value;
         renderBackground(getModel(table), primitive, isSelected);
-        renderPrimitive(primitive);
+        renderPrimitive(primitive, cellSize);
         return this;
     }
 }

src/org/openstreetmap/josm/gui/dialogs/relation/SelectionTableCellRenderer.java
@@ -3,6 +3,7 @@ package org.openstreetmap.josm.gui.dialogs.relation;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Rectangle;
 
 import javax.swing.JLabel;
 import javax.swing.JTable;
@@ -57,8 +58,8 @@ public class SelectionTableCellRenderer extends JLabel implements TableCellRende
         setBackground(bgc);
     }
 
-    protected void renderPrimitive(OsmPrimitive primitive) {
-        setIcon(ImageProvider.get(primitive.getDisplayType()));
+    protected void renderPrimitive(OsmPrimitive primitive, Rectangle cellSize) {
+        setIcon(ImageProvider.getPadded(primitive, cellSize));
         setText(primitive.getDisplayName(DefaultNameFormatter.getInstance()));
         setToolTipText(DefaultNameFormatter.getInstance().buildDefaultToolTip(primitive));
     }
@@ -71,8 +72,10 @@ public class SelectionTableCellRenderer extends JLabel implements TableCellRende
         if (value == null)
             return this;
 
+        Rectangle cellSize = table.getCellRect(row, column, false);
+
         renderBackground((OsmPrimitive) value);
-        renderPrimitive((OsmPrimitive) value);
+        renderPrimitive((OsmPrimitive) value, cellSize);
         return this;
     }
 

src/org/openstreetmap/josm/gui/OsmPrimitivRenderer.java
@@ -2,6 +2,7 @@
 package org.openstreetmap.josm.gui;
 
 import java.awt.Component;
+import java.awt.Rectangle;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.ImageIcon;
@@ -66,13 +67,15 @@ public class OsmPrimitivRenderer implements ListCellRenderer<OsmPrimitive>, Tabl
      * Internal method that stuffs information into the rendering component
      * provided that it's a kind of JLabel.
      * @param def the rendering component
-     * @param value the OsmPrimtive to render
+     * @param value the OsmPrimitive to render
      * @return the modified rendering component
      */
     private Component renderer(Component def, OsmPrimitive value) {
         if (value != null && def instanceof JLabel) {
             ((JLabel) def).setText(getComponentText(value));
-            ImageIcon icon = ImageProvider.get(value.getDisplayType());
+            ImageIcon icon = ImageProvider.getPadded(value,
+                // Height of component no yet known, assume the default 16px.
+                new Rectangle(16, 16));
             if (icon != null) {
                 ((JLabel) def).setIcon(icon);
             } else {
@@ -87,7 +90,7 @@ public class OsmPrimitivRenderer implements ListCellRenderer<OsmPrimitive>, Tabl
      * Internal method that stuffs information into the rendering component
      * provided that it's a kind of JLabel.
      * @param def the rendering component
-     * @param value the HistoryOsmPrimtive to render
+     * @param value the HistoryOsmPrimitive to render
      * @return the modified rendering component
      */
     private Component renderer(Component def, HistoryOsmPrimitive value) {

src/org/openstreetmap/josm/tools/ImageProvider.java
@@ -11,6 +11,7 @@ import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.awt.Transparency;
@@ -55,7 +56,14 @@ import javax.swing.ImageIcon;
 import javax.xml.bind.DatatypeConverter;
 
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.gui.mappaint.ElemStyle;
+import org.openstreetmap.josm.gui.mappaint.MapImage;
+import org.openstreetmap.josm.gui.mappaint.MapPaintStyles;
+import org.openstreetmap.josm.gui.mappaint.NodeElemStyle;
+import org.openstreetmap.josm.gui.mappaint.Range;
+import org.openstreetmap.josm.gui.mappaint.StyleCache.StyleList;
 import org.openstreetmap.josm.io.CachedFile;
 import org.openstreetmap.josm.plugins.PluginHandler;
 import org.w3c.dom.Element;
@@ -1265,6 +1273,57 @@ public class ImageProvider {
     public static ImageIcon get(OsmPrimitiveType type) {
         CheckParameterUtil.ensureParameterNotNull(type, "type");
         return get("data", type.getAPIName());
+    }
+
+    /**
+     * @param primitive Object for which an icon shall be fetched. The icon is chosen based on tags.
+     * @param iconSize Target size of icon. Icon is padded if required.
+     * @return Icon for {@code primitive} that fits in cell.
+     * @since 8903
+     */
+    public static ImageIcon getPadded(OsmPrimitive primitive, Rectangle iconSize) {
+        // Check if the current styles have special icon for tagged nodes.
+        if (primitive instanceof org.openstreetmap.josm.data.osm.Node) {
+            Pair<StyleList, Range> nodeStyles = MapPaintStyles.getStyles().generateStyles(primitive, 100, false);
+            for (ElemStyle style : nodeStyles.a) {
+                if (style instanceof NodeElemStyle) {
+                    NodeElemStyle nodeStyle = (NodeElemStyle) style;
+                    MapImage icon = nodeStyle.mapImage;
+                    if (icon != null) {
+                        int backgroundWidth = iconSize.height;
+                        int backgroundHeight = iconSize.height;
+                        int iconWidth = icon.getWidth();
+                        int iconHeight = icon.getHeight();
+                        BufferedImage image = new BufferedImage(backgroundWidth, backgroundHeight,
+                                BufferedImage.TYPE_INT_ARGB);
+                        double scaleFactor = Math.min(backgroundWidth / (double) iconWidth, backgroundHeight
+                                / (double) iconHeight);
+                        BufferedImage iconImage = icon.getImage(false);
+                        Image scaledIcon;
+                        final int scaledWidth;
+                        final int scaledHeight;
+                        if (scaleFactor < 1) {
+                            // Scale icon such that it fits on background.
+                            scaledWidth = (int) (iconWidth * scaleFactor);
+                            scaledHeight = (int) (iconHeight * scaleFactor);
+                            scaledIcon = iconImage.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+                        } else {
+                            // Use original size, don't upscale.
+                            scaledWidth = iconWidth;
+                            scaledHeight = iconHeight;
+                            scaledIcon = iconImage;
+                        }
+                        image.getGraphics().drawImage(scaledIcon, (backgroundWidth - scaledWidth) / 2,
+                                (backgroundHeight - scaledHeight) / 2, null);
+
+                        return new ImageIcon(image);
+                    }
+                }
+            }
+        }
+
+        // Use generic default icon.
+        return ImageProvider.get(primitive.getDisplayType());
     }
 
     /**

commit 09466140195e2eb9a3969115b2a6d1a5376b78a9

see #11968, fix #12550 - in relation editor, node features are shown without name (only icon, regression from r8903)

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableMemberCellRenderer.java
@@ -24,7 +24,8 @@ public class MemberTableMemberCellRenderer extends MemberTableCellRenderer {
 
     protected void renderPrimitive(OsmPrimitive primitive, Dimension cellSize) {
         // Make icon the full height of the table cell. Icon background is square.
-        setIcon(ImageProvider.getPadded(primitive, cellSize));
+        int size = Math.min(cellSize.width, cellSize.height);
+        setIcon(ImageProvider.getPadded(primitive, new Dimension(size, size)));
         setText(primitive.getDisplayName(DefaultNameFormatter.getInstance()));
         setToolTipText(DefaultNameFormatter.getInstance().buildDefaultToolTip(primitive));
     }

src/org/openstreetmap/josm/gui/dialogs/relation/SelectionTableCellRenderer.java
@@ -60,7 +60,8 @@ public class SelectionTableCellRenderer extends JLabel implements TableCellRende
     }
 
     protected void renderPrimitive(OsmPrimitive primitive, Dimension cellSize) {
-        setIcon(ImageProvider.getPadded(primitive, cellSize));
+        int size = Math.min(cellSize.width, cellSize.height);
+        setIcon(ImageProvider.getPadded(primitive, new Dimension(size, size)));
         setText(primitive.getDisplayName(DefaultNameFormatter.getInstance()));
         setToolTipText(DefaultNameFormatter.getInstance().buildDefaultToolTip(primitive));
     }