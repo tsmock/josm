tsmock/josm

commit 170e5cadcb5fabd854252ab760b5eea5bd26e3dc

Snap scale to mercator zoom levels.
See #12350

Patch submitted by: kolesar



git-svn-id: https://josm.openstreetmap.de/svn/trunk@9818 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/NativeScaleLayer.java
@@ -1,0 +1,235 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.layer;
+
+import java.util.ArrayList;
+
+import org.openstreetmap.josm.gui.NavigatableComponent;
+
+/**
+ * Represents a layer that has native scales.
+ * @author András Kolesár
+ */
+public interface NativeScaleLayer {
+
+    /**
+     * Get native scales of this layer.
+     * @return {@link ScaleList} of native scales
+     */
+    ScaleList getNativeScales();
+
+    /**
+     * Represents a scale with native flag, used in {@link ScaleList}
+     */
+    class Scale {
+        /**
+         * Scale factor, same unit as in {@link NavigatableComponent}
+         */
+        public double scale;
+
+        /**
+         * True if this scale is native resolution for data source.
+         */
+        public boolean isNative;
+
+        private int index;
+
+        /**
+         * Constructs a new Scale with given scale, native defaults to true.
+         * @param scale as defined in WMTS (scaleDenominator)
+         */
+        public Scale(double scale) {
+            this.scale = scale;
+            this.isNative = true;
+        }
+
+        /**
+         * Constructs a new Scale with given scale and native values.
+         * @param scale as defined in WMTS (scaleDenominator)
+         * @param isNative is this scale native to the source or not
+         */
+        public Scale(double scale, boolean isNative) {
+            this.scale = scale;
+            this.isNative = isNative;
+        }
+
+        /**
+         * Constructs a new Scale with given scale, native and index values.
+         * @param scale as defined in WMTS (scaleDenominator)
+         * @param isNative is this scale native to the source or not
+         * @param index zoom index for this scale
+         */
+        public Scale(double scale, boolean isNative, int index) {
+            this.scale = scale;
+            this.isNative = isNative;
+            this.index = index;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%f [%s]", scale, isNative);
+        }
+
+        /**
+         * Get index of this scale in a {@link ScaleList}
+         * @return index
+         */
+        public int getIndex() {
+            return index;
+        }
+    }
+
+    /**
+     * List of scales, may include intermediate steps
+     * between native resolutions
+     */
+    class ScaleList extends ArrayList<Scale> {
+
+        /**
+         * Returns a ScaleList that has intermediate steps between native scales.
+         * Native steps are split to equal steps near given ratio.
+         * @param ratio user defined zoom ratio
+         * @return a {@link ScaleList} with intermediate steps
+         */
+        public ScaleList withIntermediateSteps(double ratio) {
+            ScaleList result = new ScaleList();
+            Scale previous = null;
+            for (Scale current: this) {
+                if (previous != null) {
+                    double step = previous.scale / current.scale;
+                    double factor = Math.log(step) / Math.log(ratio);
+                    int steps = (int) Math.round(factor);
+                    double smallStep = Math.pow(step, 1.0/steps);
+                    for (int j = 1; j < steps; j++) {
+                        double intermediate = previous.scale / Math.pow(smallStep, j);
+                        result.add(new Scale(intermediate, false));
+                    }
+                }
+                result.add(current);
+                previous = current;
+            }
+            return result;
+        }
+
+        /**
+         * Get a scale from this ScaleList or a new scale if zoomed outside.
+         * @param scale previous scale
+         * @param ratio zoom ratio from starting from previous scale
+         * @param floor use floor instead of round, set true when fitting view to objects
+         * @return new {@link Scale}
+         */
+        public Scale getSnapScale(double scale, double ratio, boolean floor) {
+            int size = size();
+            Scale first = get(0);
+            Scale last = get(size-1);
+            if (scale > first.scale) {
+                double step = scale / first.scale;
+                double factor = Math.log(step) / Math.log(ratio);
+                int steps = (int) (floor ? Math.floor(factor) : Math.round(factor));
+                if (steps == 0) {
+                    return new Scale(first.scale, first.isNative, steps);
+                } else {
+                    return new Scale(first.scale * Math.pow(ratio, steps), false, steps);
+                }
+            } else if (scale < last.scale) {
+                double step = last.scale / scale;
+                double factor = Math.log(step) / Math.log(ratio);
+                int steps = (int) (floor ? Math.floor(factor) : Math.round(factor));
+                if (steps == 0) {
+                    return new Scale(last.scale, last.isNative, size-1+steps);
+                } else {
+                    return new Scale(last.scale / Math.pow(ratio, steps), false, size-1+steps);
+                }
+            } else {
+                Scale previous = null;
+                for (int i = 0; i < size; i++) {
+                    Scale current = this.get(i);
+                    if (previous != null) {
+                        if (scale <= previous.scale && scale >= current.scale) {
+                            if (floor || previous.scale / scale < scale / current.scale) {
+                                return new Scale(previous.scale, previous.isNative, i-1);
+                            } else {
+                                return new Scale(current.scale, current.isNative, i);
+                            }
+                        }
+                    }
+                    previous = current;
+                }
+                return null;
+            }
+        }
+
+        /**
+         * Get new scale for zoom in/out with a ratio at a number of times.
+         * Used by mousewheel zoom where wheel can step more than one between events.
+         * @param scale previois scale
+         * @param ratio user defined zoom ratio
+         * @param times number of times to zoom
+         * @return new {@link Scale} object from {@link ScaleList} or outside
+         */
+        public Scale scaleZoomTimes(double scale, double ratio, int times) {
+            Scale next = getSnapScale(scale, ratio, false);
+            int abs = Math.abs(times);
+            for (int i = 0; i < abs; i++) {
+                if (times < 0) {
+                    next = getNextIn(next, ratio);
+                } else {
+                    next = getNextOut(next, ratio);
+                }
+            }
+            return next;
+        }
+
+        /**
+         * Get new scale for zoom in.
+         * @param scale previous scale
+         * @param ratio user defined zoom ratio
+         * @return next scale in list or a new scale when zoomed outside
+         */
+        public Scale scaleZoomIn(double scale, double ratio) {
+            Scale snap = getSnapScale(scale, ratio, false);
+            Scale next = getNextIn(snap, ratio);
+            return next;
+        }
+
+        /**
+         * Get new scale for zoom out.
+         * @param scale previous scale
+         * @param ratio user defined zoom ratio
+         * @return next scale in list or a new scale when zoomed outside
+         */
+        public Scale scaleZoomOut(double scale, double ratio) {
+            Scale snap = getSnapScale(scale, ratio, false);
+            Scale next = getNextOut(snap, ratio);
+            return next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Scale s: this) {
+                stringBuilder.append(s + "\n");
+            }
+            return stringBuilder.toString();
+        }
+
+        private Scale getNextIn(Scale scale, double ratio) {
+            int nextIndex = scale.getIndex() + 1;
+            if (nextIndex <= 0 || nextIndex > size()-1) {
+                return new Scale(scale.scale / ratio, nextIndex == 0, nextIndex);
+            } else {
+                Scale nextScale = get(nextIndex);
+                return new Scale(nextScale.scale, nextScale.isNative, nextIndex);
+            }
+        }
+
+        private Scale getNextOut(Scale scale, double ratio) {
+            int nextIndex = scale.getIndex() - 1;
+            if (nextIndex < 0 || nextIndex >= size()-1) {
+                return new Scale(scale.scale * ratio, nextIndex == size()-1, nextIndex);
+            } else {
+                Scale nextScale = get(nextIndex);
+                return new Scale(nextScale.scale, nextScale.isNative, nextIndex);
+            }
+        }
+    }
+}

images/dialogs/layerlist/scale.svg
@@ -1,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg id="svg2" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" version="1.1" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">
+ <metadata id="metadata10">
+  <rdf:RDF>
+   <cc:Work rdf:about="">
+    <dc:format>image/svg+xml</dc:format>
+    <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
+    <dc:title/>
+   </cc:Work>
+  </rdf:RDF>
+ </metadata>
+ <g>
+  <rect id="rect1" height="4" width="4" y="4" x="4" fill="#000"/>
+  <rect id="rect2" height="4" width="4" y="8" x="8" fill="#000"/>
+  <rect id="rect3" height="4" width="4" y="8" x="4" fill="#fff"/>
+  <rect id="rect4" height="4" width="4" y="4" x="8" fill="#fff"/>
+ </g>
+</svg>

src/org/openstreetmap/josm/actions/ZoomInAction.java
@@ -46,7 +46,7 @@ public final class ZoomInAction extends JosmAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (!Main.isDisplayingMapView()) return;
-        Main.map.mapView.zoomToFactor(1/Math.sqrt(2));
+        Main.map.mapView.zoomIn();
     }
 
     @Override

src/org/openstreetmap/josm/actions/ZoomOutAction.java
@@ -32,7 +32,7 @@ public final class ZoomOutAction extends JosmAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (!Main.isDisplayingMapView()) return;
-        Main.map.mapView.zoomToFactor(Math.sqrt(2));
+        Main.map.mapView.zoomOut();
     }
 
     @Override

src/org/openstreetmap/josm/data/imagery/WMTSTileSource.java
@@ -46,6 +46,8 @@ import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.projection.Projection;
 import org.openstreetmap.josm.data.projection.Projections;
 import org.openstreetmap.josm.gui.ExtendedDialog;
+import org.openstreetmap.josm.gui.layer.NativeScaleLayer.Scale;
+import org.openstreetmap.josm.gui.layer.NativeScaleLayer.ScaleList;
 import org.openstreetmap.josm.io.CachedFile;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.GBC;
@@ -285,7 +287,7 @@ public class WMTSTileSource extends AbstractTMSTileSource implements TemplatedTi
         return output.toString();
     }
 
-    private Collection<Layer> getCapabilities() throws IOException {
+    private Collection<Layer> getCapabilities() {
         XMLInputFactory factory = XMLInputFactory.newFactory();
         // do not try to load external entities, nor validate the XML
         factory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
@@ -708,10 +710,10 @@ public class WMTSTileSource extends AbstractTMSTileSource implements TemplatedTi
         if (zoom > getMaxZoom()) {
             return null;
         }
-        if (zoom < 1) {
+        if (zoom < 0) {
             return null;
         }
-        return this.currentTileMatrixSet.tileMatrix.get(zoom - 1);
+        return this.currentTileMatrixSet.tileMatrix.get(zoom);
     }
 
     @Override
@@ -829,7 +831,7 @@ public class WMTSTileSource extends AbstractTMSTileSource implements TemplatedTi
     @Override
     public int getMaxZoom() {
         if (this.currentTileMatrixSet != null) {
-            return this.currentTileMatrixSet.tileMatrix.size();
+            return this.currentTileMatrixSet.tileMatrix.size()-1;
         }
         return 0;
     }
@@ -910,4 +912,19 @@ public class WMTSTileSource extends AbstractTMSTileSource implements TemplatedTi
         EastNorth max = proj.latlon2eastNorth(proj.getWorldBoundsLatLon().getMax());
         return (int) Math.ceil(Math.abs(max.east() - min.east()) / scale);
     }
+
+    /**
+     * Get native scales of tile source.
+     * @return {@link ScaleList} of native scales
+     */
+    public ScaleList getNativeScales() {
+        ScaleList scales = new ScaleList();
+        if (currentTileMatrixSet != null) {
+            for (TileMatrix tileMatrix : currentTileMatrixSet.tileMatrix) {
+                scales.add(new Scale(tileMatrix.scaleDenominator * 0.28e-03));
+            }
+        }
+        return scales;
+    }
+
 }

src/org/openstreetmap/josm/data/preferences/DoubleProperty.java
@@ -1,0 +1,44 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.data.preferences;
+
+import org.openstreetmap.josm.Main;
+
+/**
+ * A property containing an {@code Double} value.
+ * @since 3246
+ */
+public class DoubleProperty extends AbstractProperty<Double> {
+
+    /**
+     * Constructs a new {@code DoubleProperty}.
+     * @param key The property key
+     * @param defaultValue The default value
+     */
+    public DoubleProperty(String key, double defaultValue) {
+        super(key, defaultValue);
+    }
+
+    @Override
+    public Double get() {
+        return Main.pref.getDouble(getKey(), getDefaultValue());
+    }
+
+    @Override
+    public boolean put(Double value) {
+        return Main.pref.putDouble(getKey(), value);
+    }
+
+    /**
+     * parses and saves a double precision value
+     * @param value the value to be parsed
+     * @return true - preference value has changed
+     *         false - parsing failed or preference value has not changed
+     */
+    public boolean parseAndPut(String value) {
+        try {
+            return put(Double.valueOf(value));
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+}

src/org/openstreetmap/josm/gui/MapMover.java
@@ -213,7 +213,7 @@ public class MapMover extends MouseAdapter implements MouseMotionListener, Mouse
      */
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        nc.zoomToFactor(e.getX(), e.getY(), Math.pow(Math.sqrt(2), e.getWheelRotation()));
+        nc.zoomManyTimes(e.getX(), e.getY(), e.getWheelRotation());
     }
 
     /**

src/org/openstreetmap/josm/gui/MapSlider.java
@@ -10,16 +10,17 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import org.openstreetmap.josm.data.ProjectionBounds;
 import org.openstreetmap.josm.gui.help.Helpful;
 
 class MapSlider extends JSlider implements PropertyChangeListener, ChangeListener, Helpful {
 
+    private static final double zoomStep = 1.1;
     private final MapView mv;
     private boolean preventChange;
+    private int lastValue;
 
     MapSlider(MapView mv) {
-        super(35, 150);
+        super(0, 150);
         setOpaque(false);
         this.mv = mv;
         mv.addPropertyChangeListener("scale", this);
@@ -30,27 +31,11 @@ class MapSlider extends JSlider implements PropertyChangeListener, ChangeListene
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (getModel().getValueIsAdjusting()) return;
-
-        ProjectionBounds world = this.mv.getMaxProjectionBounds();
-        ProjectionBounds current = this.mv.getProjectionBounds();
-
-        double cur_e = current.maxEast-current.minEast;
-        double cur_n = current.maxNorth-current.minNorth;
-        double e = world.maxEast-world.minEast;
-        double n = world.maxNorth-world.minNorth;
-        int zoom = 0;
-
-        while (zoom <= 150) {
-            e /= 1.1;
-            n /= 1.1;
-            if (e < cur_e && n < cur_n) {
-                break;
-            }
-            ++zoom;
-        }
+        double maxScale = this.mv.getMaxScale();
+        int zoom = (int) Math.round(Math.log(maxScale/mv.getScale())/Math.log(zoomStep));
         preventChange = true;
         setValue(zoom);
+        lastValue = zoom;
         preventChange = false;
     }
 
@@ -58,12 +43,19 @@ class MapSlider extends JSlider implements PropertyChangeListener, ChangeListene
     public void stateChanged(ChangeEvent e) {
         if (preventChange) return;
 
-        ProjectionBounds world = this.mv.getMaxProjectionBounds();
-        double fact = Math.pow(1.1, getValue());
-        double es = world.maxEast-world.minEast;
-        double n = world.maxNorth-world.minNorth;
-
-        this.mv.zoomTo(new ProjectionBounds(this.mv.getCenter(), es/fact, n/fact));
+        if (!getModel().getValueIsAdjusting() && mv.getNativeScaleLayer() != null) {
+            if (getValue() < lastValue) {
+                mv.zoomOut();
+            } else if (getValue() > lastValue) {
+                mv.zoomIn();
+            }
+        } else {
+            double maxScale = this.mv.getMaxScale();
+            double scale = maxScale/Math.pow(zoomStep, getValue());
+            double snapped = mv.scaleFloor(scale);
+            mv.zoomTo(this.mv.getCenter(), snapped);
+        }
+        propertyChange(null);
     }
 
     @Override

commit 29f1495c418d2093dc54b6465201be5d87997e93

fix #12568 - IndexOutOfBoundsException in NativeScaleLayer$ScaleList - regression from r9818

src/org/openstreetmap/josm/gui/layer/NativeScaleLayer.java
@@ -149,6 +149,8 @@ public interface NativeScaleLayer {
          * @return new {@link Scale}
          */
         public Scale getSnapScale(double scale, double ratio, boolean floor) {
+            if (scales.isEmpty())
+                return null;
             int size = scales.size();
             Scale first = scales.get(0);
             Scale last = scales.get(size-1);

test/unit/org/openstreetmap/josm/gui/layer/NativeScaleLayerTest.java
@@ -1,0 +1,33 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.layer;
+
+import static org.junit.Assert.assertNull;
+
+import java.util.Collections;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.josm.JOSMFixture;
+
+/**
+ * Unit tests of {@link NativeScaleLayer} class.
+ */
+public class NativeScaleLayerTest {
+
+    /**
+     * Setup tests
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        JOSMFixture.createUnitTestFixture().init();
+    }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/12255">#12255</a>.
+     */
+    @Test
+    public void testTicket12255() {
+        assertNull(new NativeScaleLayer.ScaleList(Collections.<Double>emptyList()).getSnapScale(10, 2, false));
+        assertNull(new NativeScaleLayer.ScaleList(new double[0]).getSnapScale(10, 2, false));
+    }
+}