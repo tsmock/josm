tsmock/josm

commit 327a0e60f1fcdd9c7bdb31e16fb0fb568581aef6

refactor some code from StyleCache into separate classes (see #9891)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@9284 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/mappaint/StyleCache.java
@@ -1,239 +1,42 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.mappaint;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
-
 import org.openstreetmap.josm.data.osm.Storage;
-import org.openstreetmap.josm.gui.mappaint.styleelement.StyleElement;
-import org.openstreetmap.josm.tools.Pair;
 
 /**
  * Caches styles for a single primitive.
- * Splits the range of possible scale values (0 &lt; scale &lt; +Infinity) into multiple
- * subranges, for each scale range it keeps a list of styles.
- * Immutable class, equals &amp; hashCode is required (the same for StyleList, StyleElement
- and its subclasses).
  */
-public final class StyleCache {
-    /* list of boundaries for the scale ranges */
-    private final List<Double> bd;
-    /* styles for each scale range */
-    private final List<StyleList> data;
+public final class StyleCache extends DividedScale<StyleElementList> {
 
     // TODO: clean up the intern pool from time to time (after purge or layer removal)
     private static final Storage<StyleCache> internPool = new Storage<>();
 
     public static final StyleCache EMPTY_STYLECACHE = (new StyleCache()).intern();
 
+    private StyleCache(StyleCache sc) {
+        super(sc);
+    }
+
     private StyleCache() {
-        bd = new ArrayList<>();
-        bd.add(0.0);
-        bd.add(Double.POSITIVE_INFINITY);
-        data = new ArrayList<>();
-        data.add(null);
-    }
-
-    private StyleCache(StyleCache s) {
-        bd = new ArrayList<>(s.bd);
-        data = new ArrayList<>(s.data);
+        super();
     }
 
     /**
-     * List of Styles, immutable
+     * Add data object which is valid for the given range.
+     *
+     * This is only possible, if there is no data for the given range yet.
+     *
+     * @param o data object
+     * @param r the valid range
+     * @return a new, updated, <code>DividedScale</code> object
      */
-    public static class StyleList implements Iterable<StyleElement> {
-        private final List<StyleElement> lst;
-
-        /**
-         * Constructs a new {@code StyleList}.
-         */
-        public StyleList() {
-            lst = new ArrayList<>();
-        }
-
-        public StyleList(StyleElement... init) {
-            lst = new ArrayList<>(Arrays.asList(init));
-        }
-
-        public StyleList(Collection<StyleElement> sl) {
-            lst = new ArrayList<>(sl);
-        }
-
-        public StyleList(StyleList sl, StyleElement s) {
-            lst = new ArrayList<>(sl.lst);
-            lst.add(s);
-        }
-
-        @Override
-        public Iterator<StyleElement> iterator() {
-            return lst.iterator();
-        }
-
-        public boolean isEmpty() {
-            return lst.isEmpty();
-        }
-
-        public int size() {
-            return lst.size();
-        }
-
-        @Override
-        public String toString() {
-            return lst.toString();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            final StyleList other = (StyleList) obj;
-            return Objects.equals(lst, other.lst);
-        }
-
-        @Override
-        public int hashCode() {
-            return lst.hashCode();
-        }
-    }
-
-    /**
-     * looks up styles for a certain scale value
-     * @param scale scale
-     * @return style list
-     */
-    public StyleList get(double scale) {
-        if (scale <= 0)
-            throw new IllegalArgumentException("scale must be <= 0 but is "+scale);
-        for (int i = 0; i < data.size(); ++i) {
-            if (bd.get(i) < scale && scale <= bd.get(i+1)) {
-                return data.get(i);
-            }
-        }
-        throw new AssertionError();
-    }
-
-    /**
-     * looks up styles for a certain scale value and additionally returns
-     * the scale range for the returned styles
-     * @param scale scale
-     * @return pair containing syle list and range
-     */
-    public Pair<StyleList, Range> getWithRange(double scale) {
-        if (scale <= 0)
-            throw new IllegalArgumentException("scale must be <= 0 but is "+scale);
-        for (int i = 0; i < data.size(); ++i) {
-            if (bd.get(i) < scale && scale <= bd.get(i+1)) {
-                return new Pair<>(data.get(i), new Range(bd.get(i), bd.get(i+1)));
-            }
-        }
-        throw new AssertionError();
-    }
-
-    public StyleCache put(StyleList sl, Range r) {
-        return put(sl, r.getLower(), r.getUpper());
-    }
-
-    /**
-     * add a new styles to the cache. this is only possible, if
-     * for this scale range, there is nothing in the cache yet.
-     * @param sl style list
-     * @param lower lower bound
-     * @param upper upper bound
-     * @return interned style cache
-     */
-    public StyleCache put(StyleList sl, double lower, double upper) {
+    @Override
+    public StyleCache put(StyleElementList o, Range r) {
         StyleCache s = new StyleCache(this);
-        s.putImpl(sl, lower, upper);
+        s.putImpl(o, r.getLower(), r.getUpper());
         s.consistencyTest();
-        return s.intern();
-    }
-
-    // this exception type is for debugging #8997 and can later be replaced
-    // by AssertionError
-    public static class RangeViolatedError extends Error {
-        public RangeViolatedError() {
-        }
-
-        public RangeViolatedError(String message) {
-            super(message);
-        }
-    }
-
-    /**
-     * ASCII-art explanation:
-     *
-     *              data[i]
-     *  --|-------|---------|--
-     * bd[i-1]  bd[i]    bd[i+1]
-     *
-     *         (--------]
-     *       lower     upper
-     * @param sl style list
-     * @param lower lower bound
-     * @param upper upper bound
-     */
-    private void putImpl(StyleList sl, double lower, double upper) {
-        int i = 0;
-        while (bd.get(i) < lower) {
-            ++i;
-        }
-        if (bd.get(i) == lower) {
-            if (upper > bd.get(i+1))
-                throw new RangeViolatedError("the new range must be within a single subrange (1)");
-            if (data.get(i) != null)
-                throw new RangeViolatedError("the new range must be within a subrange that has no data");
-
-            if (bd.get(i+1) == upper) {
-                //  --|-------|--------|--
-                //   i-1      i       i+1
-                //            (--------]
-                data.set(i, sl);
-            } else {
-                //  --|-------|--------|--
-                //   i-1      i       i+1
-                //            (-----]
-                bd.add(i+1, upper);
-                data.add(i, sl);
-            }
-            return;
-        } else {
-            if (bd.get(i) < upper)
-                throw new RangeViolatedError("the new range must be within a single subrange (2)");
-            if (data.get(i-1) != null)
-                throw new AssertionError();
-
-            //  --|-------|--------|--
-            //   i-1      i       i+1
-            //       (--]   or
-            //       (----]
-            bd.add(i, lower);
-            data.add(i, sl);
-
-            //  --|--|----|--------|--
-            //   i-1 i   i+1      i+2
-            //       (--]
-            if (bd.get(i+1) > upper) {
-                bd.add(i+1, upper);
-                data.add(i+1, null);
-            }
-            return;
-        }
-    }
-
-    public void consistencyTest() {
-        if (bd.size() < 2) throw new AssertionError(bd);
-        if (data.isEmpty()) throw new AssertionError(data);
-        if (bd.size() != data.size() + 1) throw new AssertionError();
-        if (bd.get(0) != 0) throw new AssertionError();
-        if (bd.get(bd.size() - 1) != Double.POSITIVE_INFINITY) throw new AssertionError();
-        for (int i = 0; i < data.size() - 1; ++i) {
-            if (bd.get(i) >= bd.get(i + 1)) throw new AssertionError();
-        }
+        s.intern();
+        return s;
     }
 
     /**
@@ -241,28 +44,7 @@ public final class StyleCache {
      * StyleCache must not be changed after it has been added to the intern pool.
      * @return style cache
      */
-    public StyleCache intern() {
+    private StyleCache intern() {
         return internPool.putUnique(this);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-        final StyleCache other = (StyleCache) obj;
-        return bd.equals(other.bd) && data.equals(other.data);
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 23 * hash + bd.hashCode();
-        hash = 23 * hash + data.hashCode();
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "SC{" + bd + ' ' + data + '}';
     }
 }

src/org/openstreetmap/josm/data/osm/visitor/paint/StyledMapRenderer.java
@@ -60,7 +60,7 @@ import org.openstreetmap.josm.data.osm.visitor.paint.relations.MultipolygonCache
 import org.openstreetmap.josm.gui.NavigatableComponent;
 import org.openstreetmap.josm.gui.mappaint.ElemStyles;
 import org.openstreetmap.josm.gui.mappaint.MapPaintStyles;
-import org.openstreetmap.josm.gui.mappaint.StyleCache.StyleList;
+import org.openstreetmap.josm.gui.mappaint.StyleElementList;
 import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
 import org.openstreetmap.josm.gui.mappaint.mapcss.Selector;
 import org.openstreetmap.josm.gui.mappaint.styleelement.AreaElement;
@@ -1824,14 +1824,14 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         }
 
         public void add(Node osm, int flags) {
-            StyleList sl = styles.get(osm, circum, nc);
+            StyleElementList sl = styles.get(osm, circum, nc);
             for (StyleElement s : sl) {
                 output.add(new StyleRecord(s, osm, flags));
             }
         }
 
         public void add(Relation osm, int flags) {
-            StyleList sl = styles.get(osm, circum, nc);
+            StyleElementList sl = styles.get(osm, circum, nc);
             for (StyleElement s : sl) {
                 if (drawMultipolygon && drawArea && s instanceof AreaElement && (flags & FLAG_DISABLED) == 0) {
                     output.add(new StyleRecord(s, osm, flags));
@@ -1842,7 +1842,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         }
 
         public void add(Way osm, int flags) {
-            StyleList sl = styles.get(osm, circum, nc);
+            StyleElementList sl = styles.get(osm, circum, nc);
             for (StyleElement s : sl) {
                 if (!(drawArea && (flags & FLAG_DISABLED) == 0) && s instanceof AreaElement) {
                     continue;

src/org/openstreetmap/josm/gui/dialogs/InspectPrimitiveDialog.java
@@ -43,7 +43,7 @@ import org.openstreetmap.josm.gui.mappaint.ElemStyles;
 import org.openstreetmap.josm.gui.mappaint.MapPaintStyles;
 import org.openstreetmap.josm.gui.mappaint.MultiCascade;
 import org.openstreetmap.josm.gui.mappaint.StyleCache;
-import org.openstreetmap.josm.gui.mappaint.StyleCache.StyleList;
+import org.openstreetmap.josm.gui.mappaint.StyleElementList;
 import org.openstreetmap.josm.gui.mappaint.StyleSource;
 import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
 import org.openstreetmap.josm.gui.mappaint.xml.XmlStyleSource;
@@ -361,7 +361,7 @@ public class InspectPrimitiveDialog extends ExtendedDialog {
                     }
                 }
                 txtMappaint.append(tr("\n\nList of generated Styles:\n"));
-                StyleList sl = elemstyles.get(osm, scale, nc);
+                StyleElementList sl = elemstyles.get(osm, scale, nc);
                 for (StyleElement s : sl) {
                     txtMappaint.append(" * ").append(s).append('\n');
                 }

src/org/openstreetmap/josm/gui/mappaint/DividedScale.java
@@ -1,0 +1,196 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.mappaint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openstreetmap.josm.gui.mappaint.styleelement.StyleElement;
+import org.openstreetmap.josm.tools.Pair;
+
+/**
+ * Splits the range of possible scale values (0 &lt; scale &lt; +Infinity) into
+ * multiple subranges, for each scale range it keeps a data object of a certain
+ * type T (can be null).
+ *
+ * Used for caching style information for different zoom levels.
+ *
+ * Immutable class, equals &amp; hashCode is required (the same for
+ * {@link StyleElementList}, {@link StyleElement} and its subclasses).
+ *
+ * @param <T> the type of the data objects
+ */
+public class DividedScale<T> {
+
+    // this exception type is for debugging #8997 and can later be replaced
+    // by AssertionError
+    public static class RangeViolatedError extends Error {
+        public RangeViolatedError() {
+        }
+
+        public RangeViolatedError(String message) {
+            super(message);
+        }
+    }
+
+    /* list of boundaries for the scale ranges */
+    private final List<Double> bd;
+    /* data objects for each scale range */
+    private final List<T> data;
+
+    protected DividedScale() {
+        bd = new ArrayList<>();
+        bd.add(0.0);
+        bd.add(Double.POSITIVE_INFINITY);
+        data = new ArrayList<>();
+        data.add(null);
+    }
+
+    protected DividedScale(DividedScale<T> s) {
+        bd = new ArrayList<>(s.bd);
+        data = new ArrayList<>(s.data);
+    }
+
+    /**
+     * Looks up the data object for a certain scale value.
+     *
+     * @param scale scale
+     * @return the data object at the given scale, can be null
+     */
+    public T get(double scale) {
+        if (scale <= 0)
+            throw new IllegalArgumentException("scale must be <= 0 but is "+scale);
+        for (int i = 0; i < data.size(); ++i) {
+            if (bd.get(i) < scale && scale <= bd.get(i+1)) {
+                return data.get(i);
+            }
+        }
+        throw new AssertionError();
+    }
+
+    /**
+     * Looks up the data object for a certain scale value and additionally returns
+     * the scale range where the object is valid.
+     *
+     * @param scale scale
+     * @return pair containing data object and range
+     */
+    public Pair<T, Range> getWithRange(double scale) {
+        if (scale <= 0)
+            throw new IllegalArgumentException("scale must be <= 0 but is "+scale);
+        for (int i = 0; i < data.size(); ++i) {
+            if (bd.get(i) < scale && scale <= bd.get(i+1)) {
+                return new Pair<>(data.get(i), new Range(bd.get(i), bd.get(i+1)));
+            }
+        }
+        throw new AssertionError();
+    }
+
+    /**
+     * Add data object which is valid for the given range.
+     *
+     * This is only possible, if there is no data for the given range yet.
+     *
+     * @param o data object
+     * @param r the valid range
+     * @return a new, updated, <code>DividedScale</code> object
+     */
+    public DividedScale<T> put(T o, Range r) {
+        DividedScale<T> s = new DividedScale<>(this);
+        s.putImpl(o, r.getLower(), r.getUpper());
+        s.consistencyTest();
+        return s;
+    }
+
+    /**
+     * Implementation of the <code>put</code> operation.
+     *
+     * ASCII-art explanation:
+     *
+     *              data[i]
+     *  --|-------|---------|--
+     * bd[i-1]  bd[i]    bd[i+1]
+     *
+     *         (--------]
+     *       lower     upper
+     * @param o data object
+     * @param lower lower bound
+     * @param upper upper bound
+     */
+    protected void putImpl(T o, double lower, double upper) {
+        int i = 0;
+        while (bd.get(i) < lower) {
+            ++i;
+        }
+        if (bd.get(i) == lower) {
+            if (upper > bd.get(i+1))
+                throw new StyleCache.RangeViolatedError("the new range must be within a single subrange (1)");
+            if (data.get(i) != null)
+                throw new StyleCache.RangeViolatedError("the new range must be within a subrange that has no data");
+
+            if (bd.get(i+1) == upper) {
+                //  --|-------|--------|--
+                //   i-1      i       i+1
+                //            (--------]
+                data.set(i, o);
+            } else {
+                //  --|-------|--------|--
+                //   i-1      i       i+1
+                //            (-----]
+                bd.add(i+1, upper);
+                data.add(i, o);
+            }
+        } else {
+            if (bd.get(i) < upper)
+                throw new StyleCache.RangeViolatedError("the new range must be within a single subrange (2)");
+            if (data.get(i-1) != null)
+                throw new AssertionError();
+
+            //  --|-------|--------|--
+            //   i-1      i       i+1
+            //       (--]   or
+            //       (----]
+            bd.add(i, lower);
+            data.add(i, o);
+
+            //  --|--|----|--------|--
+            //   i-1 i   i+1      i+2
+            //       (--]
+            if (bd.get(i+1) > upper) {
+                bd.add(i+1, upper);
+                data.add(i+1, null);
+            }
+        }
+    }
+
+    public void consistencyTest() {
+        if (bd.size() < 2) throw new AssertionError(bd);
+        if (data.isEmpty()) throw new AssertionError(data);
+        if (bd.size() != data.size() + 1) throw new AssertionError();
+        if (bd.get(0) != 0) throw new AssertionError();
+        if (bd.get(bd.size() - 1) != Double.POSITIVE_INFINITY) throw new AssertionError();
+        for (int i = 0; i < data.size() - 1; ++i) {
+            if (bd.get(i) >= bd.get(i + 1)) throw new AssertionError();
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        final DividedScale other = (DividedScale) obj;
+        return bd.equals(other.bd) && data.equals(other.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + bd.hashCode();
+        hash = 23 * hash + data.hashCode();
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "DS{" + bd + ' ' + data + '}';
+    }
+}

src/org/openstreetmap/josm/gui/mappaint/ElemStyles.java
@@ -20,7 +20,6 @@ import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.osm.visitor.paint.relations.Multipolygon;
 import org.openstreetmap.josm.data.osm.visitor.paint.relations.MultipolygonCache;
 import org.openstreetmap.josm.gui.NavigatableComponent;
-import org.openstreetmap.josm.gui.mappaint.StyleCache.StyleList;
 import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
 import org.openstreetmap.josm.gui.mappaint.styleelement.AreaElement;
 import org.openstreetmap.josm.gui.mappaint.styleelement.BoxTextElement;
@@ -79,7 +78,7 @@ public class ElemStyles implements PreferenceChangedListener {
      * @param nc display component
      * @return list of styles
      */
-    public StyleList get(OsmPrimitive osm, double scale, NavigatableComponent nc) {
+    public StyleElementList get(OsmPrimitive osm, double scale, NavigatableComponent nc) {
         return getStyleCacheWithRange(osm, scale, nc).a;
     }
 
@@ -93,15 +92,15 @@ public class ElemStyles implements PreferenceChangedListener {
      * @param nc navigatable component
      * @return pair containing style list and range
      */
-    public Pair<StyleList, Range> getStyleCacheWithRange(OsmPrimitive osm, double scale, NavigatableComponent nc) {
+    public Pair<StyleElementList, Range> getStyleCacheWithRange(OsmPrimitive osm, double scale, NavigatableComponent nc) {
         if (osm.mappaintStyle == null || osm.mappaintCacheIdx != cacheIdx || scale <= 0) {
             osm.mappaintStyle = StyleCache.EMPTY_STYLECACHE;
         } else {
-            Pair<StyleList, Range> lst = osm.mappaintStyle.getWithRange(scale);
+            Pair<StyleElementList, Range> lst = osm.mappaintStyle.getWithRange(scale);
             if (lst.a != null)
                 return lst;
         }
-        Pair<StyleList, Range> p = getImpl(osm, scale, nc);
+        Pair<StyleElementList, Range> p = getImpl(osm, scale, nc);
         if (osm instanceof Node && isDefaultNodes()) {
             if (p.a.isEmpty()) {
                 if (TextLabel.AUTO_LABEL_COMPOSITION_STRATEGY.compose(osm) != null) {
@@ -122,10 +121,10 @@ public class ElemStyles implements PreferenceChangedListener {
                     }
                 }
                 if (!hasNonModifier) {
-                    p.a = new StyleList(p.a, NodeElement.SIMPLE_NODE_ELEMSTYLE);
+                    p.a = new StyleElementList(p.a, NodeElement.SIMPLE_NODE_ELEMSTYLE);
                     if (!hasText) {
                         if (TextLabel.AUTO_LABEL_COMPOSITION_STRATEGY.compose(osm) != null) {
-                            p.a = new StyleList(p.a, BoxTextElement.SIMPLE_NODE_TEXT_ELEMSTYLE);
+                            p.a = new StyleElementList(p.a, BoxTextElement.SIMPLE_NODE_TEXT_ELEMSTYLE);
                         }
                     }
                 }
@@ -141,7 +140,7 @@ public class ElemStyles implements PreferenceChangedListener {
             if (!hasProperLineStyle) {
                 AreaElement area = Utils.find(p.a, AreaElement.class);
                 LineElement line = area == null ? LineElement.UNTAGGED_WAY : LineElement.createSimpleLineStyle(area.color, true);
-                p.a = new StyleList(p.a, line);
+                p.a = new StyleElementList(p.a, line);
             }
         }
         StyleCache style = osm.mappaintStyle != null ? osm.mappaintStyle : StyleCache.EMPTY_STYLECACHE;
@@ -185,11 +184,11 @@ public class ElemStyles implements PreferenceChangedListener {
      * @param nc navigatable component
      * @return pair containing style list and range
      */
-    private Pair<StyleList, Range> getImpl(OsmPrimitive osm, double scale, NavigatableComponent nc) {
+    private Pair<StyleElementList, Range> getImpl(OsmPrimitive osm, double scale, NavigatableComponent nc) {
         if (osm instanceof Node)
             return generateStyles(osm, scale, false);
         else if (osm instanceof Way) {
-            Pair<StyleList, Range> p = generateStyles(osm, scale, false);
+            Pair<StyleElementList, Range> p = generateStyles(osm, scale, false);
 
             boolean isOuterWayOfSomeMP = false;
             Color wayColor = null;
@@ -215,12 +214,12 @@ public class ElemStyles implements PreferenceChangedListener {
                                 }
                             }
                         }
-                        p.a = new StyleList(tmp);
+                        p.a = new StyleElementList(tmp);
                         isOuterWayOfSomeMP = true;
                     }
 
                     if (!hasIndependentLineStyle) {
-                        Pair<StyleList, Range> mpElemStyles;
+                        Pair<StyleElementList, Range> mpElemStyles;
                         synchronized (r) {
                             mpElemStyles = getStyleCacheWithRange(r, scale, nc);
                         }
@@ -233,7 +232,7 @@ public class ElemStyles implements PreferenceChangedListener {
                         }
                         p.b = Range.cut(p.b, mpElemStyles.b);
                         if (mpLine != null) {
-                            p.a = new StyleList(p.a, mpLine);
+                            p.a = new StyleElementList(p.a, mpLine);
                             break;
                         } else if (wayColor == null && isDefaultLines()) {
                             AreaElement mpArea = Utils.find(mpElemStyles.a, AreaElement.class);
@@ -254,7 +253,7 @@ public class ElemStyles implements PreferenceChangedListener {
                         }
                     }
                     if (!hasLineStyle) {
-                        p.a = new StyleList(p.a, LineElement.createSimpleLineStyle(wayColor, true));
+                        p.a = new StyleElementList(p.a, LineElement.createSimpleLineStyle(wayColor, true));
                     }
                 }
                 return p;
@@ -280,7 +279,7 @@ public class ElemStyles implements PreferenceChangedListener {
                     }
                     if (!hasIndependentElemStyle && !multipolygon.getOuterWays().isEmpty()) {
                         Color mpColor = null;
-                        StyleList mpElemStyles = null;
+                        StyleElementList mpElemStyles = null;
                         synchronized (ref) {
                             mpElemStyles = get(ref, scale, nc);
                         }
@@ -290,24 +289,24 @@ public class ElemStyles implements PreferenceChangedListener {
                                 break;
                             }
                         }
-                        p.a = new StyleList(p.a, LineElement.createSimpleLineStyle(mpColor, true));
+                        p.a = new StyleElementList(p.a, LineElement.createSimpleLineStyle(mpColor, true));
                     }
                     return p;
                 }
             }
             return p;
         } else if (osm instanceof Relation) {
-            Pair<StyleList, Range> p = generateStyles(osm, scale, true);
+            Pair<StyleElementList, Range> p = generateStyles(osm, scale, true);
             if (drawMultipolygon && ((Relation) osm).isMultipolygon()) {
                 if (!Utils.exists(p.a, AreaElement.class) && Main.pref.getBoolean("multipolygon.deprecated.outerstyle", true)) {
                     // look at outer ways to find area style
                     Multipolygon multipolygon = MultipolygonCache.getInstance().get(nc, (Relation) osm);
                     for (Way w : multipolygon.getOuterWays()) {
-                        Pair<StyleList, Range> wayStyles = generateStyles(w, scale, false);
+                        Pair<StyleElementList, Range> wayStyles = generateStyles(w, scale, false);
                         p.b = Range.cut(p.b, wayStyles.b);
                         StyleElement area = Utils.find(wayStyles.a, AreaElement.class);
                         if (area != null) {
-                            p.a = new StyleList(p.a, area);
+                            p.a = new StyleElementList(p.a, area);
                             break;
                         }
                     }
@@ -331,7 +330,7 @@ public class ElemStyles implements PreferenceChangedListener {
      * outer ways of a multipolygon.
      * @return the generated styles and the valid range as a pair
      */
-    public Pair<StyleList, Range> generateStyles(OsmPrimitive osm, double scale, boolean pretendWayIsClosed) {
+    public Pair<StyleElementList, Range> generateStyles(OsmPrimitive osm, double scale, boolean pretendWayIsClosed) {
 
         List<StyleElement> sl = new ArrayList<>();
         MultiCascade mc = new MultiCascade();
@@ -376,7 +375,7 @@ public class ElemStyles implements PreferenceChangedListener {
                 }
             }
         }
-        return new Pair<>(new StyleList(sl), mc.range);
+        return new Pair<>(new StyleElementList(sl), mc.range);
     }
 
     private static <T> void addIfNotNull(List<T> list, T obj) {
@@ -500,7 +499,7 @@ public class ElemStyles implements PreferenceChangedListener {
         try {
             if (MapPaintStyles.getStyles() == null)
                 return false;
-            StyleList styles = MapPaintStyles.getStyles().generateStyles(p, 1.0, false).a;
+            StyleElementList styles = MapPaintStyles.getStyles().generateStyles(p, 1.0, false).a;
             if (styles.isEmpty()) {
                 return false;
             }

src/org/openstreetmap/josm/gui/mappaint/MapPaintStyles.java
@@ -26,7 +26,7 @@ import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.Tag;
 import org.openstreetmap.josm.gui.PleaseWaitRunnable;
-import org.openstreetmap.josm.gui.mappaint.StyleCache.StyleList;
+import org.openstreetmap.josm.gui.mappaint.StyleElementList;
 import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
 import org.openstreetmap.josm.gui.mappaint.styleelement.MapImage;
 import org.openstreetmap.josm.gui.mappaint.styleelement.NodeElement;
@@ -183,7 +183,7 @@ public final class MapPaintStyles {
             DataSet ds = new DataSet();
             Node virtualNode = new Node(LatLon.ZERO);
             virtualNode.put(tag.getKey(), tag.getValue());
-            StyleList styleList;
+            StyleElementList styleList;
             MapCSSStyleSource.STYLE_SOURCE_LOCK.readLock().lock();
             try {
                 // Add primitive to dataset to avoid DataIntegrityProblemException when evaluating selectors

commit 37e34072dc6aacf0bb4809e3d69872dbd4b90cc6

fix #13907 - StyleCache broken (regression from r9284, patch by Gerd Petermann)

src/org/openstreetmap/josm/gui/mappaint/StyleCache.java
@@ -48,8 +48,7 @@ public final class StyleCache {
             ds = new DividedScale<>();
         }
         s.states[idx] = ds.put(o, r);
-        s.intern();
-        return s;
+        return s.intern();
     }
 
     public Pair<StyleElementList, Range> getWithRange(double scale, boolean selected) {