tsmock/josm

commit 53fd03e40e51f176960b5fb8e28eb940a0f0d0d5

refactor duplicated code
git-svn-id: https://josm.openstreetmap.de/svn/trunk@11395 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/Storage.java
@@ -503,19 +503,28 @@ public class Storage<T> extends AbstractSet<T> {
         }
     }
 
-    private final class SafeReadonlyIter implements Iterator<T> {
-        private final T[] data;
-        private int slot;
-
-        SafeReadonlyIter(T[] data) {
-            this.data = data;
-        }
+    private abstract class AbstractIter implements Iterator<T> {
+        protected int slot;
 
         @Override
         public boolean hasNext() {
             if (data == null) return false;
             align();
             return slot < data.length;
+        }
+
+        protected void align() {
+            while (slot < data.length && data[slot] == null) {
+                slot++;
+            }
+        }
+    }
+
+    private final class SafeReadonlyIter extends AbstractIter {
+        private final T[] data;
+
+        SafeReadonlyIter(T[] data) {
+            this.data = data;
         }
 
         @Override
@@ -528,28 +537,14 @@ public class Storage<T> extends AbstractSet<T> {
         public void remove() {
             throw new UnsupportedOperationException();
         }
-
-        private void align() {
-            while (slot < data.length && data[slot] == null) {
-                slot++;
-            }
-        }
     }
 
-    private final class Iter implements Iterator<T> {
+    private final class Iter extends AbstractIter {
         private final int mods;
-        private int slot;
         private int removeSlot = -1;
 
         Iter() {
             mods = modCount;
-        }
-
-        @Override
-        public boolean hasNext() {
-            if (data == null) return false;
-            align();
-            return slot < data.length;
         }
 
         @Override
@@ -568,13 +563,11 @@ public class Storage<T> extends AbstractSet<T> {
             removeSlot = -1;
         }
 
-        private void align() {
+        @Override
+        protected void align() {
             if (mods != modCount)
                 throw new ConcurrentModificationException();
-            while (slot < data.length && data[slot] == null) {
-                slot++;
-            }
+            super.align();
         }
     }
-
 }

commit 4fc3adb3e774557c4f587599e99fbb4f93b84d48

fix #14135 - regression from r11395

src/org/openstreetmap/josm/data/osm/Storage.java
@@ -506,14 +506,13 @@ public class Storage<T> extends AbstractSet<T> {
     private abstract class AbstractIter implements Iterator<T> {
         protected int slot;
 
-        @Override
-        public boolean hasNext() {
+        protected final boolean doHasNext(T[] data) {
             if (data == null) return false;
-            align();
+            align(data);
             return slot < data.length;
         }
 
-        protected void align() {
+        protected void align(T[] data) {
             while (slot < data.length && data[slot] == null) {
                 slot++;
             }
@@ -525,6 +524,11 @@ public class Storage<T> extends AbstractSet<T> {
 
         SafeReadonlyIter(T[] data) {
             this.data = data;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return doHasNext(data);
         }
 
         @Override
@@ -548,6 +552,11 @@ public class Storage<T> extends AbstractSet<T> {
         }
 
         @Override
+        public boolean hasNext() {
+            return doHasNext(data);
+        }
+
+        @Override
         public T next() {
             if (!hasNext()) throw new NoSuchElementException();
             removeSlot = slot;
@@ -564,10 +573,10 @@ public class Storage<T> extends AbstractSet<T> {
         }
 
         @Override
-        protected void align() {
+        protected void align(T[] data) {
             if (mods != modCount)
                 throw new ConcurrentModificationException();
-            super.align();
+            super.align(data);
         }
     }
 }