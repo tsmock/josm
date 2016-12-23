tsmock/josm

commit 6223dda44f17ca1d7207e41b7b369f69ece28bb6

fix #14042 - NPE
git-svn-id: https://josm.openstreetmap.de/svn/trunk@11306 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/history/VersionInfoPanel.java
@@ -201,7 +201,9 @@ public class VersionInfoPanel extends JPanel implements ChangeListener {
         HistoryOsmPrimitive primitive = getPrimitive();
         if (primitive != null) {
             Changeset cs = primitive.getChangeset();
-            update(cs, model.isLatest(primitive), primitive.getTimestamp(), primitive.getVersion());
+            if (cs != null) {
+                update(cs, model.isLatest(primitive), primitive.getTimestamp(), primitive.getVersion());
+            }
         }
     }
 
@@ -216,7 +218,7 @@ public class VersionInfoPanel extends JPanel implements ChangeListener {
 
     /**
      * Updates the content of this panel based on the changeset information given by {@code cs}.
-     * @param cs the changeset information
+     * @param cs the changeset information - must not be null
      * @param isLatest whether this relates to a not yet commited changeset
      * @param timestamp the timestamp
      * @param version the version of the primitive

commit 16cdb34289d00e0de14d940837b2f7672388c6f6

fix #14151 - see #14042 - fix NPE properly (regression from r11306) + add robustness

src/org/openstreetmap/josm/gui/history/VersionInfoPanel.java
@@ -201,9 +201,7 @@ public class VersionInfoPanel extends JPanel implements ChangeListener {
         HistoryOsmPrimitive primitive = getPrimitive();
         if (primitive != null) {
             Changeset cs = primitive.getChangeset();
-            if (cs != null) {
-                update(cs, model.isLatest(primitive), primitive.getTimestamp(), primitive.getVersion());
-            }
+            update(cs, model.isLatest(primitive), primitive.getTimestamp(), primitive.getVersion());
         }
     }
 
@@ -218,7 +216,7 @@ public class VersionInfoPanel extends JPanel implements ChangeListener {
 
     /**
      * Updates the content of this panel based on the changeset information given by {@code cs}.
-     * @param cs the changeset information - must not be null
+     * @param cs the changeset information
      * @param isLatest whether this relates to a not yet commited changeset
      * @param timestamp the timestamp
      * @param version the version of the primitive
@@ -226,7 +224,7 @@ public class VersionInfoPanel extends JPanel implements ChangeListener {
     public void update(final Changeset cs, final boolean isLatest, final Date timestamp, final long version) {
         lblInfo.setText(getInfoText(timestamp, version, isLatest));
 
-        if (!isLatest) {
+        if (!isLatest && cs != null) {
             User user = cs.getUser();
             String url = Main.getBaseBrowseUrl() + "/changeset/" + cs.getId();
             lblChangeset.setUrl(url);
@@ -298,7 +296,9 @@ public class VersionInfoPanel extends JPanel implements ChangeListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            ChangesetDialog.LaunchChangesetManager.displayChangesets(Collections.singleton(id));
+            if (id != null) {
+                ChangesetDialog.LaunchChangesetManager.displayChangesets(Collections.singleton(id));
+            }
             if (componentToSelect != null) {
                 ChangesetCacheManager.getInstance().setSelectedComponentInDetailPanel(componentToSelect);
             }

src/org/openstreetmap/josm/gui/widgets/UrlLabel.java
@@ -73,7 +73,7 @@ public class UrlLabel extends JLabel implements MouseListener {
     }
 
     protected final void refresh() {
-        if (url != null) {
+        if (url != null && !url.isEmpty()) {
             setText("<html><a href=\""+url+"\">"+description+"</a></html>");
             setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
             setToolTipText(String.format("<html>%s<br/>%s</html>", url, tr("Right click = copy to clipboard")));
@@ -85,8 +85,8 @@ public class UrlLabel extends JLabel implements MouseListener {
     }
 
     /**
-     * Sets the URL to be visited if the user clicks on this URL label. If null, the
-     * label turns into a normal label without hyperlink.
+     * Sets the URL to be visited if the user clicks on this URL label.
+     * If null or empty, the label turns into a normal label without hyperlink.
      *
      * @param url the url. Can be null.
      */
@@ -108,10 +108,12 @@ public class UrlLabel extends JLabel implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (SwingUtilities.isLeftMouseButton(e)) {
-            OpenBrowser.displayUrl(url);
-        } else if (SwingUtilities.isRightMouseButton(e)) {
-            ClipboardUtils.copyString(url);
+        if (url != null && !url.isEmpty()) {
+            if (SwingUtilities.isLeftMouseButton(e)) {
+                OpenBrowser.displayUrl(url);
+            } else if (SwingUtilities.isRightMouseButton(e)) {
+                ClipboardUtils.copyString(url);
+            }
         }
     }
 