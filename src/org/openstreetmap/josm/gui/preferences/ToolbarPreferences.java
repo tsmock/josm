tsmock/josm

commit 0325ce668f8a9fbaa9b1d56bcd1fe8f41fa9a901

fix #14450 - Cannot add local imagery to toolbar
git-svn-id: https://josm.openstreetmap.de/svn/trunk@11654 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/preferences/ToolbarPreferences.java
@@ -938,6 +938,7 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
             for (ActionDefinition actionDefinition: getDefinedActions()) {
                 selected.addElement(actionDefinition);
             }
+            actionsTreeModel.reload();
         }
 
         @Override
@@ -1076,6 +1077,7 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
     }
 
     /**
+     * Registers an action to the toolbar preferences.
      * @param action Action to register
      * @return The parameter (for better chaining)
      */
@@ -1095,6 +1097,20 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
             regactions.put(toolbar, action);
         }
         return action;
+    }
+
+    /**
+     * Unregisters an action from the toolbar preferences.
+     * @param action Action to unregister
+     * @return The removed action, or null
+     * @since 11654
+     */
+    public Action unregister(Action action) {
+        String toolbar = (String) action.getValue("toolbar");
+        if (toolbar != null) {
+            return regactions.remove(toolbar);
+        }
+        return null;
     }
 
     /**

src/org/openstreetmap/josm/actions/AddImageryLayerAction.java
@@ -63,8 +63,8 @@ public class AddImageryLayerAction extends JosmAction implements AdaptableAction
      * @param info The imagery info
      */
     public AddImageryLayerAction(ImageryInfo info) {
-        super(info.getMenuName(), /* ICON */"imagery_menu", tr("Add imagery layer {0}", info.getName()), null, false, false);
-        putValue("toolbar", "imagery_" + info.getToolbarName());
+        super(info.getMenuName(), /* ICON */"imagery_menu", tr("Add imagery layer {0}", info.getName()), null,
+                true, "imagery_" + info.getToolbarName(), false);
         putValue("help", ht("/Preferences/Imagery"));
         setTooltip(info.getToolTipText().replaceAll("</?html>", ""));
         this.info = info;
@@ -201,5 +201,10 @@ public class AddImageryLayerAction extends JosmAction implements AdaptableAction
         } else {
             setEnabled(true);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "AddImageryLayerAction [info=" + info + ']';
     }
 }

src/org/openstreetmap/josm/gui/ImageryMenu.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 import javax.swing.Action;
 import javax.swing.JComponent;
@@ -263,18 +264,17 @@ public class ImageryMenu extends JMenu implements LayerChangeListener {
     private final List<Object> dynamicItems = new ArrayList<>(20);
 
     /**
-     * Remove all the items in @field dynamicItems collection
+     * Remove all the items in dynamic items collection
      * @since 5803
      */
     private void removeDynamicItems() {
         for (Object item : dynamicItems) {
             if (item instanceof JMenuItem) {
+                Optional.ofNullable(((JMenuItem) item).getAction()).ifPresent(Main.toolbar::unregister);
                 remove((JMenuItem) item);
-            }
-            if (item instanceof MenuComponent) {
+            } else if (item instanceof MenuComponent) {
                 remove((MenuComponent) item);
-            }
-            if (item instanceof Component) {
+            } else if (item instanceof Component) {
                 remove((Component) item);
             }
         }

commit 5d0cefd28e5a162fb7a6034161e85ba27717df6e

fix #14457 - CCE in imagery menu (regression from r11654)

src/org/openstreetmap/josm/gui/preferences/ToolbarPreferences.java
@@ -1130,8 +1130,8 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
      * @since 11654
      */
     public Action unregister(Action action) {
-        String toolbar = (String) action.getValue("toolbar");
-        if (toolbar != null) {
+        Object toolbar = action.getValue("toolbar");
+        if (toolbar instanceof String) {
             return regactions.remove(toolbar);
         }
         return null;