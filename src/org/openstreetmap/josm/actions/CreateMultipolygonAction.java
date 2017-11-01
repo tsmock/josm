tsmock/josm

commit 476823a79159e91a284dad0c826db266b051d09b

see #13036 - deprecate `Command()` default constructor, fix unit tests and java warnings
git-svn-id: https://josm.openstreetmap.de/svn/trunk@12726 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/CreateMultipolygonAction.java
@@ -247,7 +247,7 @@ public class CreateMultipolygonAction extends JosmAction {
         final List<Command> list = removeTagsFromWaysIfNeeded(relation);
         final String commandName;
         if (existingRelation == null) {
-            list.add(new AddCommand(relation));
+            list.add(new AddCommand(selectedWays.iterator().next().getDataSet(), relation));
             commandName = getName(false);
         } else {
             list.add(new ChangeCommand(existingRelation, relation));

commit 028bf7f508390d9b0151777b831c80c3d7f4936d

fix #15507 - Update multipolygon action no longer works (regression from r12726)

src/org/openstreetmap/josm/actions/CreateMultipolygonAction.java
@@ -449,8 +449,13 @@ public class CreateMultipolygonAction extends JosmAction {
                     fixed = true;
                 }
             }
-            if (fixed && !relation.isNew())
-                commands.add(new ChangeCommand(relation, r2));
+            if (fixed && !relation.isNew()) {
+                DataSet ds = relation.getDataSet();
+                if (ds == null) {
+                    ds = MainApplication.getLayerManager().getEditDataSet();
+                }
+                commands.add(new ChangeCommand(ds, relation, r2));
+            }
         }
 
         return commands;