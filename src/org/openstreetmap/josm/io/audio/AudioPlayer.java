tsmock/josm

commit 33038fc10152570f8a2fcbf0a43117627791ea78

fix #15226 - see #2089 - support for compiling without JavaFX (modified patch by ris)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@12715 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/audio/AudioPlayer.java
@@ -236,10 +236,15 @@ public final class AudioPlayer extends Thread implements AudioListener {
         double leadIn = Main.pref.getDouble("audio.leadin", 1.0 /* default, seconds */);
         double calibration = Main.pref.getDouble("audio.calibration", 1.0 /* default, ratio */);
         try {
-            soundPlayer = new JavaFxMediaPlayer();
-        } catch (NoClassDefFoundError | InterruptedException e) {
+            soundPlayer = (SoundPlayer) Class.forName("org.openstreetmap.josm.io.audio.JavaFxMediaPlayer").getConstructor().newInstance();
+        } catch (ReflectiveOperationException | IllegalArgumentException | SecurityException e) {
+            Logging.debug(e);
+            Logging.warn("JOSM compiled without Java FX support. Falling back to Java Sound API");
+        } catch (NoClassDefFoundError | JosmRuntimeException e) {
             Logging.debug(e);
             Logging.warn("Java FX is unavailable. Falling back to Java Sound API");
+        }
+        if (soundPlayer == null) {
             soundPlayer = new JavaSoundPlayer(leadIn, calibration);
         }
         soundPlayer.addAudioListener(this);

build.xml
@@ -305,6 +305,7 @@ Build-Date: ${build.tstamp}
             <compilerarg value="-XDignore.symbol.file"/>
             <compilerarg value="-Xep:ReferenceEquality:OFF" compiler="com.google.errorprone.ErrorProneAntCompilerAdapter"/>
             <compilerarg line="-Xmaxwarns 1000"/>
+            <exclude name="org/openstreetmap/josm/io/audio/JavaFxMediaPlayer.java" if:set="noJavaFX"/>
         </javac>
     </target>
     <target name="compile" depends="init,javacc,compile-cots,compile-jmapviewer">

src/org/openstreetmap/josm/io/audio/JavaFxMediaPlayer.java
@@ -10,6 +10,7 @@ import java.util.concurrent.CountDownLatch;
 
 import org.openstreetmap.josm.io.audio.AudioPlayer.Execute;
 import org.openstreetmap.josm.io.audio.AudioPlayer.State;
+import org.openstreetmap.josm.tools.JosmRuntimeException;
 import org.openstreetmap.josm.tools.ListenerList;
 
 import com.sun.javafx.application.PlatformImpl;
@@ -36,8 +37,12 @@ class JavaFxMediaPlayer implements SoundPlayer {
 
     private MediaPlayer mediaPlayer;
 
-    JavaFxMediaPlayer() throws InterruptedException {
-        initFxPlatform();
+    JavaFxMediaPlayer() throws JosmRuntimeException {
+        try {
+            initFxPlatform();
+        } catch (InterruptedException e) {
+            throw new JosmRuntimeException(e);
+        }
     }
 
     /**

commit 1d3d5980f1d088a668867f03f8f997267311f8f5

see #15226, see #2089 - fix MP3 support broken in r12715

src/org/openstreetmap/josm/io/audio/AudioPlayer.java
@@ -236,7 +236,8 @@ public final class AudioPlayer extends Thread implements AudioListener {
         double leadIn = Config.getPref().getDouble("audio.leadin", 1.0 /* default, seconds */);
         double calibration = Config.getPref().getDouble("audio.calibration", 1.0 /* default, ratio */);
         try {
-            soundPlayer = (SoundPlayer) Class.forName("org.openstreetmap.josm.io.audio.JavaFxMediaPlayer").getConstructor().newInstance();
+            soundPlayer = (SoundPlayer) Class.forName("org.openstreetmap.josm.io.audio.JavaFxMediaPlayer")
+                    .getDeclaredConstructor().newInstance();
         } catch (ReflectiveOperationException | IllegalArgumentException | SecurityException e) {
             Logging.debug(e);
             Logging.warn("JOSM compiled without Java FX support. Falling back to Java Sound API");