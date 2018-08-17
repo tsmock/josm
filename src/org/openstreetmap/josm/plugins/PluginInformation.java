tsmock/josm

commit 2c59a3e78eb6fc273dcdba076f5bd6a90feedb9f

see #16400 - update integration test to detect invalid manifest entries
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13993 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/plugins/PluginInformation.java
@@ -83,6 +83,8 @@ public class PluginInformation {
     public List<URL> libraries = new LinkedList<>();
     /** All manifest attributes. */
     public final Map<String, String> attr = new TreeMap<>();
+    /** Invalid manifest entries */
+    final List<String> invalidManifestEntries = new ArrayList<>();
     /** Empty icon for these plugins which have none */
     private static final ImageIcon emptyIcon = ImageProvider.getEmpty(ImageProvider.ImageSizes.LARGEICON);
 
@@ -174,6 +176,8 @@ public class PluginInformation {
         this.libraries = other.libraries;
         this.attr.clear();
         this.attr.putAll(other.attr);
+        this.invalidManifestEntries.clear();
+        this.invalidManifestEntries.addAll(other.invalidManifestEntries);
     }
 
     /**
@@ -247,25 +251,25 @@ public class PluginInformation {
             }
         }
         canloadatruntime = Boolean.parseBoolean(attr.getValue("Plugin-Canloadatruntime"));
-        if (oldcheck && mainversion > Version.getInstance().getVersion()) {
-            int myv = Version.getInstance().getVersion();
-            for (Map.Entry<Object, Object> entry : attr.entrySet()) {
+        int myv = Version.getInstance().getVersion();
+        for (Map.Entry<Object, Object> entry : attr.entrySet()) {
+            String key = ((Attributes.Name) entry.getKey()).toString();
+            if (key.endsWith("_Plugin-Url")) {
                 try {
-                    String key = ((Attributes.Name) entry.getKey()).toString();
-                    if (key.endsWith("_Plugin-Url")) {
-                        int mv = Integer.parseInt(key.substring(0, key.length()-11));
-                        if (mv <= myv && (mv > mainversion || mainversion > myv)) {
-                            String v = (String) entry.getValue();
-                            int i = v.indexOf(';');
-                            if (i > 0) {
-                                downloadlink = v.substring(i+1);
-                                mainversion = mv;
-                                version = v.substring(0, i);
-                                oldmode = true;
-                            }
-                        }
+                    int mv = Integer.parseInt(key.substring(0, key.length()-11));
+                    String v = (String) entry.getValue();
+                    int i = v.indexOf(';');
+                    if (i <= 0) {
+                        invalidManifestEntries.add(key);
+                    } else if (oldcheck && mainversion > Version.getInstance().getVersion() &&
+                        mv <= myv && (mv > mainversion || mainversion > myv)) {
+                        downloadlink = v.substring(i+1);
+                        mainversion = mv;
+                        version = v.substring(0, i);
+                        oldmode = true;
                     }
-                } catch (NumberFormatException e) {
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    invalidManifestEntries.add(key);
                     Logging.error(e);
                 }
             }

test/unit/org/openstreetmap/josm/plugins/PluginHandlerTestIT.java
@@ -54,9 +54,12 @@ public class PluginHandlerTestIT {
         Map<String, Throwable> loadingExceptions = PluginHandler.pluginLoadingExceptions.entrySet().stream()
                 .collect(Collectors.toMap(e -> e.getKey(), e -> ExceptionUtils.getRootCause(e.getValue())));
 
+        List<PluginInformation> loadedPlugins = PluginHandler.getPlugins();
+        Map<String, List<String>> invalidManifestEntries = loadedPlugins.stream().filter(pi -> !pi.invalidManifestEntries.isEmpty())
+                .collect(Collectors.toMap(pi -> pi.name, pi -> pi.invalidManifestEntries));
+
         // Add/remove layers twice to test basic plugin good behaviour
         Map<String, Throwable> layerExceptions = new HashMap<>();
-        List<PluginInformation> loadedPlugins = PluginHandler.getPlugins();
         for (int i = 0; i < 2; i++) {
             OsmDataLayer layer = new OsmDataLayer(new DataSet(), "Layer "+i, null);
             testPlugin(MainApplication.getLayerManager()::addLayer, layer, layerExceptions, loadedPlugins);
@@ -68,11 +71,13 @@ public class PluginHandlerTestIT {
             testPlugin(MainApplication.getLayerManager()::removeLayer, layer, layerExceptions, loadedPlugins);
         }
 
+        MapUtils.debugPrint(System.out, null, invalidManifestEntries);
         MapUtils.debugPrint(System.out, null, loadingExceptions);
         MapUtils.debugPrint(System.out, null, layerExceptions);
-        String msg = Arrays.toString(loadingExceptions.entrySet().toArray()) + '\n' +
+        String msg = Arrays.toString(invalidManifestEntries.entrySet().toArray()) + '\n' +
+                     Arrays.toString(loadingExceptions.entrySet().toArray()) + '\n' +
                      Arrays.toString(layerExceptions.entrySet().toArray());
-        assertTrue(msg, loadingExceptions.isEmpty() && layerExceptions.isEmpty());
+        assertTrue(msg, invalidManifestEntries.isEmpty() && loadingExceptions.isEmpty() && layerExceptions.isEmpty());
     }
 
     /**

commit 134ea1cb7343d9ff4f4ead18d616db028a4fe28d

fix #16622 - choose proper version for old instances, broken for releases since r13993

src/org/openstreetmap/josm/plugins/PluginInformation.java
@@ -261,7 +261,7 @@ public class PluginInformation {
                     int i = v.indexOf(';');
                     if (i <= 0) {
                         invalidManifestEntries.add(key);
-                    } else if (oldcheck && mainversion > Version.getInstance().getVersion() &&
+                    } else if (oldcheck &&
                         mv <= myv && (mv > mainversion || mainversion > myv)) {
                         downloadlink = v.substring(i+1);
                         mainversion = mv;