tsmock/josm

commit 3782c5044be8d079f8fd9e992b71ff6ffc0b086d

see #17201 improve progressMonitor for internal osm (xml) importer
git-svn-id: https://josm.openstreetmap.de/svn/trunk@14688 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/AbstractReader.java
@@ -290,7 +290,7 @@ public abstract class AbstractReader {
         progressMonitor.addCancelListener(cancelListener);
         CheckParameterUtil.ensureParameterNotNull(source, "source");
         try {
-            progressMonitor.beginTask(tr("Prepare OSM data...", 2));
+            progressMonitor.beginTask(tr("Prepare OSM data..."), 4); // read, prepare, post-process, render
             progressMonitor.indeterminateSubTask(tr("Parsing OSM data..."));
 
             try (InputStreamReader ir = UTFInputStreamReader.create(source)) {
@@ -309,10 +309,12 @@ public abstract class AbstractReader {
                 getDataSet().lock();
             }
             progressMonitor.worked(1);
-
+            progressMonitor.indeterminateSubTask(tr("Post-processing data set..."));
             // iterate over registered postprocessors and give them each a chance
             // to modify the dataset we have just loaded.
             callPostProcessors(progressMonitor);
+            progressMonitor.worked(1);
+            progressMonitor.indeterminateSubTask(tr("Rendering data set..."));
             // Make sure postprocessors did not change the read-only state
             if (readOnly && !getDataSet().isLocked()) {
                 getDataSet().lock();
@@ -327,7 +329,7 @@ public abstract class AbstractReader {
             if (minId.isPresent() && minId.getAsLong() < AbstractPrimitive.currentUniqueId()) {
                 AbstractPrimitive.advanceUniqueId(minId.getAsLong());
             }
-            progressMonitor.finishTask();
+            // don't call progressMonitor.finishTask() here, let caller do it
             progressMonitor.removeCancelListener(cancelListener);
         }
     }

commit 04e020a49121323203d89d8b85cfdf59c7374885

fix #10784 no progress in DownloadAlongAction (regression from r14688)

src/org/openstreetmap/josm/io/AbstractReader.java
@@ -329,7 +329,7 @@ public abstract class AbstractReader {
             if (minId.isPresent() && minId.getAsLong() < AbstractPrimitive.currentUniqueId()) {
                 AbstractPrimitive.advanceUniqueId(minId.getAsLong());
             }
-            // don't call progressMonitor.finishTask() here, let caller do it
+            progressMonitor.finishTask();
             progressMonitor.removeCancelListener(cancelListener);
         }
     }