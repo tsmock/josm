tsmock/josm

commit 6be84f25b20a948c89f37fccbf3113159d738cf0

ensures consistency of upload comment:
* fix #11168 - ctrl-z/undo could reset unwanted old changeset comment
* fix #13474 - selecting "new changeset" after having entered a changeset comment did reset it to the previous value
* fix #17452 - ctrl-enter while typing a changeset comment did upload with the previous value
* fix behaviour of `upload.comment.max-age`: values were reset after 5 months instead of intended 4 hours because seconds were compared to milliseconds
* avoid creation of unneeded undo/redo internal classes for non-editable text fields
* ensures consistency of upload dialog if `upload.comment` properties are modified manually from advanced preferences
* add a source attribute to preference events to know which class modified the preference entry
* refactor reflection utils

git-svn-id: https://josm.openstreetmap.de/svn/trunk@14977 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/Preferences.java
@@ -15,6 +15,7 @@ import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.InvalidPathException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -43,13 +44,16 @@ import org.openstreetmap.josm.io.OfflineAccessException;
 import org.openstreetmap.josm.io.OnlineResource;
 import org.openstreetmap.josm.spi.preferences.AbstractPreferences;
 import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.spi.preferences.DefaultPreferenceChangeEvent;
 import org.openstreetmap.josm.spi.preferences.IBaseDirectories;
 import org.openstreetmap.josm.spi.preferences.ListSetting;
+import org.openstreetmap.josm.spi.preferences.PreferenceChangeEvent;
 import org.openstreetmap.josm.spi.preferences.Setting;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.ListenerList;
 import org.openstreetmap.josm.tools.Logging;
 import org.openstreetmap.josm.tools.PlatformManager;
+import org.openstreetmap.josm.tools.ReflectionUtils;
 import org.openstreetmap.josm.tools.Utils;
 import org.xml.sax.SAXException;
 
@@ -117,6 +121,14 @@ public class Preferences extends AbstractPreferences {
     private final HashMap<String, ListenerList<org.openstreetmap.josm.spi.preferences.PreferenceChangedListener>> keyListeners = new HashMap<>();
 
     private static final Preferences defaultInstance = new Preferences(JosmBaseDirectories.getInstance());
+
+    /**
+     * Preferences classes calling directly the method {@link #putSetting(String, Setting)}.
+     * This collection allows us to exclude them when searching the business class who set a preference.
+     * The found class is used as event source when notifying event listeners.
+     */
+    private static final Collection<Class<?>> preferencesClasses = Arrays.asList(
+            Preferences.class, PreferencesUtils.class, AbstractPreferences.class);
 
     /**
      * Constructs a new {@code Preferences}.
@@ -215,8 +227,8 @@ public class Preferences extends AbstractPreferences {
     }
 
     protected void firePreferenceChanged(String key, Setting<?> oldValue, Setting<?> newValue) {
-        final org.openstreetmap.josm.spi.preferences.PreferenceChangeEvent evt =
-                new org.openstreetmap.josm.spi.preferences.DefaultPreferenceChangeEvent(key, oldValue, newValue);
+        final PreferenceChangeEvent evt =
+                new DefaultPreferenceChangeEvent(ReflectionUtils.findCallerClass(preferencesClasses), key, oldValue, newValue);
         listeners.fireEvent(listener -> listener.preferenceChanged(evt));
 
         ListenerList<org.openstreetmap.josm.spi.preferences.PreferenceChangedListener> forKey = keyListeners.get(key);

src/org/openstreetmap/josm/tools/ReflectionUtils.java
@@ -1,0 +1,64 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.tools;
+
+import java.lang.reflect.AccessibleObject;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.Collection;
+import java.util.function.Function;
+
+/**
+ * Reflection utilities.
+ * @since 14977
+ */
+public final class ReflectionUtils {
+
+    private ReflectionUtils() {
+        // Hide default constructor for utils classes
+    }
+
+    /**
+     * Sets {@code AccessibleObject}(s) accessible.
+     * @param objects objects
+     * @see AccessibleObject#setAccessible
+     */
+    public static void setObjectsAccessible(final AccessibleObject... objects) {
+        if (objects != null && objects.length > 0) {
+            AccessController.doPrivileged((PrivilegedAction<Object>) () -> {
+                for (AccessibleObject o : objects) {
+                    if (o != null) {
+                        o.setAccessible(true);
+                    }
+                }
+                return null;
+            });
+        }
+    }
+
+    /**
+     * To use from a method to know which class called it.
+     * @param exclusions classes to exclude from the search. Can be null
+     * @return the first calling class not present in {@code exclusions}
+     */
+    public static Class<?> findCallerClass(Collection<Class<?>> exclusions) {
+        return findCaller(x -> {
+            try {
+                return Class.forName(x.getClassName());
+            } catch (ClassNotFoundException e) {
+                Logging.error(e);
+                return null;
+            }
+        }, exclusions);
+    }
+
+    private static <T extends Object> T findCaller(Function<StackTraceElement, T> getter, Collection<T> exclusions) {
+        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+        for (int i = 3; i < stack.length; i++) {
+            T t = getter.apply(stack[i]);
+            if (exclusions == null || !exclusions.contains(t)) {
+                return t;
+            }
+        }
+        return null;
+    }
+}

src/org/openstreetmap/josm/data/StructUtils.java
@@ -32,7 +32,7 @@ import org.openstreetmap.josm.spi.preferences.IPreferences;
 import org.openstreetmap.josm.tools.JosmRuntimeException;
 import org.openstreetmap.josm.tools.Logging;
 import org.openstreetmap.josm.tools.MultiMap;
-import org.openstreetmap.josm.tools.Utils;
+import org.openstreetmap.josm.tools.ReflectionUtils;
 
 /**
  * Utility methods to convert struct-like classes to a string map and back.
@@ -160,7 +160,7 @@ public final class StructUtils {
                 continue;
             }
             try {
-                Utils.setObjectsAccessible(f);
+                ReflectionUtils.setObjectsAccessible(f);
                 Object fieldValue = f.get(struct);
                 Object defaultFieldValue = f.get(structPrototype);
                 if (fieldValue != null && (
@@ -214,7 +214,7 @@ public final class StructUtils {
             if (f.getAnnotation(StructEntry.class) == null) {
                 continue;
             }
-            Utils.setObjectsAccessible(f);
+            ReflectionUtils.setObjectsAccessible(f);
             if (f.getType() == Boolean.class || f.getType() == boolean.class) {
                 value = Boolean.valueOf(keyValue.getValue());
             } else if (f.getType() == Integer.class || f.getType() == int.class) {

src/org/openstreetmap/josm/gui/MainApplication.java
@@ -157,6 +157,7 @@ import org.openstreetmap.josm.tools.OsmUrlToBounds;
 import org.openstreetmap.josm.tools.PlatformHook.NativeOsCallback;
 import org.openstreetmap.josm.tools.PlatformHookWindows;
 import org.openstreetmap.josm.tools.PlatformManager;
+import org.openstreetmap.josm.tools.ReflectionUtils;
 import org.openstreetmap.josm.tools.Shortcut;
 import org.openstreetmap.josm.tools.Utils;
 import org.openstreetmap.josm.tools.bugreport.BugReportExceptionHandler;
@@ -990,7 +991,7 @@ public class MainApplication {
         if (Utils.getJavaVersion() < 9 && !GraphicsEnvironment.isHeadless()) {
             try {
                 Field field = Toolkit.class.getDeclaredField("resources");
-                Utils.setObjectsAccessible(field);
+                ReflectionUtils.setObjectsAccessible(field);
                 field.set(null, ResourceBundle.getBundle("sun.awt.resources.awt"));
             } catch (ReflectiveOperationException | RuntimeException e) { // NOPMD
                 // Catch RuntimeException in order to catch InaccessibleObjectException, new in Java 9

src/org/openstreetmap/josm/gui/MapStatus.java
@@ -252,7 +252,7 @@ public final class MapStatus extends JPanel implements
     private final ImageLabel nameText = new ImageLabel("name",
             tr("The name of the object at the mouse pointer."),
             getNameLabelCharacterCount(MainApplication.getMainFrame()), PROP_BACKGROUND_COLOR.get());
-    private final JosmTextField helpText = new JosmTextField();
+    private final JosmTextField helpText = new JosmTextField(null, null, 0, false);
     private final JProgressBar progressBar = new JProgressBar();
     private final transient ComponentAdapter mvComponentAdapter;
     /**

src/org/openstreetmap/josm/gui/dialogs/changeset/ChangesetDetailPanel.java
@@ -56,12 +56,12 @@ import org.openstreetmap.josm.tools.date.DateUtils;
 public class ChangesetDetailPanel extends JPanel implements PropertyChangeListener, ChangesetAware, Destroyable {
 
     // CHECKSTYLE.OFF: SingleSpaceSeparator
-    private final JosmTextField tfID        = new JosmTextField(10);
+    private final JosmTextField tfID        = new JosmTextField(null, null, 10, false);
     private final JosmTextArea  taComment   = new JosmTextArea(5, 40);
-    private final JosmTextField tfOpen      = new JosmTextField(10);
-    private final JosmTextField tfUser      = new JosmTextField("");
-    private final JosmTextField tfCreatedOn = new JosmTextField(20);
-    private final JosmTextField tfClosedOn  = new JosmTextField(20);
+    private final JosmTextField tfOpen      = new JosmTextField(null, null, 10, false);
+    private final JosmTextField tfUser      = new JosmTextField(null, "", 0);
+    private final JosmTextField tfCreatedOn = new JosmTextField(null, null, 20, false);
+    private final JosmTextField tfClosedOn  = new JosmTextField(null, null, 20, false);
 
     private final OpenChangesetPopupMenuAction   actOpenChangesetPopupMenu   = new OpenChangesetPopupMenuAction();
     private final DownloadChangesetContentAction actDownloadChangesetContent = new DownloadChangesetContentAction(this);

src/org/openstreetmap/josm/gui/io/BasicUploadSettingsPanel.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.Action;
@@ -32,7 +33,6 @@ import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.widgets.HistoryComboBox;
 import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.spi.preferences.Config;
-import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.Utils;
 
@@ -77,9 +77,7 @@ public class BasicUploadSettingsPanel extends JPanel {
         pnl.add(commentLabel, GBC.eol().insets(0, 5, 10, 3).fill(GBC.HORIZONTAL));
         hcbUploadComment.setToolTipText(tr("Enter an upload comment"));
         hcbUploadComment.setMaxTextLength(Changeset.MAX_CHANGESET_TAG_LENGTH);
-        List<String> cmtHistory = new LinkedList<>(Config.getPref().getList(HISTORY_KEY, new LinkedList<String>()));
-        Collections.reverse(cmtHistory); // we have to reverse the history, because ComboBoxHistory will reverse it again in addElement()
-        hcbUploadComment.setPossibleItems(cmtHistory);
+        populateHistoryComboBox(hcbUploadComment, HISTORY_KEY, new LinkedList<String>());
         CommentModelListener commentModelListener = new CommentModelListener(hcbUploadComment, changesetCommentModel);
         hcbUploadComment.getEditor().addActionListener(commentModelListener);
         hcbUploadComment.getEditorComponent().addFocusListener(commentModelListener);
@@ -91,22 +89,42 @@ public class BasicUploadSettingsPanel extends JPanel {
             if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType())) {
                 final String source = MainApplication.getMap().mapView.getLayerInformationForSourceTag();
                 hcbUploadSource.setText(Utils.shortenString(source, Changeset.MAX_CHANGESET_TAG_LENGTH));
-                // Fix #9965
-                changesetSourceModel.setComment(hcbUploadSource.getText());
+                changesetSourceModel.setComment(hcbUploadSource.getText()); // Fix #9965
             }
         });
         pnl.add(sourceLabel, GBC.eol().insets(0, 8, 10, 3).fill(GBC.HORIZONTAL));
 
         hcbUploadSource.setToolTipText(tr("Enter a source"));
         hcbUploadSource.setMaxTextLength(Changeset.MAX_CHANGESET_TAG_LENGTH);
-        List<String> sourceHistory = new LinkedList<>(Config.getPref().getList(SOURCE_HISTORY_KEY, getDefaultSources()));
-        Collections.reverse(sourceHistory); // we have to reverse the history, because ComboBoxHistory will reverse it again in addElement()
-        hcbUploadSource.setPossibleItems(sourceHistory);
+        populateHistoryComboBox(hcbUploadSource, SOURCE_HISTORY_KEY, getDefaultSources());
         CommentModelListener sourceModelListener = new CommentModelListener(hcbUploadSource, changesetSourceModel);
         hcbUploadSource.getEditor().addActionListener(sourceModelListener);
         hcbUploadSource.getEditorComponent().addFocusListener(sourceModelListener);
         pnl.add(hcbUploadSource, GBC.eol().fill(GBC.HORIZONTAL));
         return pnl;
+    }
+
+    /**
+     * Refreshes contents of upload history combo boxes from preferences.
+     */
+    protected void refreshHistoryComboBoxes() {
+        populateHistoryComboBox(hcbUploadComment, HISTORY_KEY, new LinkedList<String>());
+        populateHistoryComboBox(hcbUploadSource, SOURCE_HISTORY_KEY, getDefaultSources());
+    }
+
+    private static void populateHistoryComboBox(HistoryComboBox hcb, String historyKey, List<String> defaultValues) {
+        List<String> cmtHistory = new LinkedList<>(Config.getPref().getList(historyKey, defaultValues));
+        Collections.reverse(cmtHistory); // we have to reverse the history, because ComboBoxHistory will reverse it again in addElement()
+        hcb.setPossibleItems(cmtHistory);
+        hcb.discardAllUndoableEdits();
+    }
+
+    /**
+     * Discards undoable edits of upload history combo boxes.
+     */
+    protected void discardAllUndoableEdits() {
+        hcbUploadComment.discardAllUndoableEdits();
+        hcbUploadSource.discardAllUndoableEdits();
     }
 
     /**
@@ -132,17 +150,14 @@ public class BasicUploadSettingsPanel extends JPanel {
      * @param changesetCommentModel the model for the changeset comment. Must not be null
      * @param changesetSourceModel the model for the changeset source. Must not be null.
      * @param changesetReviewModel the model for the changeset review. Must not be null.
-     * @throws IllegalArgumentException if {@code changesetCommentModel} is null
+     * @throws NullPointerException if a model is null
      * @since 12719 (signature)
      */
     public BasicUploadSettingsPanel(ChangesetCommentModel changesetCommentModel, ChangesetCommentModel changesetSourceModel,
             ChangesetReviewModel changesetReviewModel) {
-        CheckParameterUtil.ensureParameterNotNull(changesetCommentModel, "changesetCommentModel");
-        CheckParameterUtil.ensureParameterNotNull(changesetSourceModel, "changesetSourceModel");
-        CheckParameterUtil.ensureParameterNotNull(changesetReviewModel, "changesetReviewModel");
-        this.changesetCommentModel = changesetCommentModel;
-        this.changesetSourceModel = changesetSourceModel;
-        this.changesetReviewModel = changesetReviewModel;
+        this.changesetCommentModel = Objects.requireNonNull(changesetCommentModel, "changesetCommentModel");
+        this.changesetSourceModel = Objects.requireNonNull(changesetSourceModel, "changesetSourceModel");
+        this.changesetReviewModel = Objects.requireNonNull(changesetReviewModel, "changesetReviewModel");
         changesetCommentModel.addChangeListener(new ChangesetCommentChangeListener(hcbUploadComment));
         changesetSourceModel.addChangeListener(new ChangesetCommentChangeListener(hcbUploadSource));
         changesetReviewModel.addChangeListener(new ChangesetReviewChangeListener());
@@ -166,7 +181,7 @@ public class BasicUploadSettingsPanel extends JPanel {
         // store the history of comments
         hcbUploadComment.addCurrentItemToHistory();
         Config.getPref().putList(HISTORY_KEY, hcbUploadComment.getHistory());
-        Config.getPref().putInt(HISTORY_LAST_USED_KEY, (int) (TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())));
+        Config.getPref().putLong(HISTORY_LAST_USED_KEY, TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
         // store the history of sources
         hcbUploadSource.addCurrentItemToHistory();
         Config.getPref().putList(SOURCE_HISTORY_KEY, hcbUploadSource.getHistory());
@@ -196,8 +211,27 @@ public class BasicUploadSettingsPanel extends JPanel {
         hcbUploadSource.requestFocusInWindow();
     }
 
+    /**
+     * Returns the panel that displays a summary of data the user is about to upload.
+     * @return the upload parameter summary panel
+     */
     public UploadParameterSummaryPanel getUploadParameterSummaryPanel() {
         return pnlUploadParameterSummary;
+    }
+
+    /**
+     * Forces update of comment/source model if matching text field is focused.
+     * @since 14977
+     */
+    public void forceUpdateActiveField() {
+        updateModelIfFocused(hcbUploadComment, changesetCommentModel);
+        updateModelIfFocused(hcbUploadSource, changesetSourceModel);
+    }
+
+    private static void updateModelIfFocused(HistoryComboBox hcb, ChangesetCommentModel changesetModel) {
+        if (hcb.getEditorComponent().hasFocus()) {
+            changesetModel.setComment(hcb.getText());
+        }
     }
 
     static final class HistoryComboBoxKeyAdapter extends KeyAdapter {

src/org/openstreetmap/josm/gui/io/ChangesetManagementPanel.java
@@ -32,17 +32,15 @@ import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
- * ChangesetManagementPanel allows to configure changeset to be used in the next
- * upload.
+ * ChangesetManagementPanel allows to configure changeset to be used in the next upload.
  *
  * It is displayed as one of the configuration panels in the {@link UploadDialog}.
  *
- * ChangesetManagementPanel is a source for {@link java.beans.PropertyChangeEvent}s. Clients can listen
- * to
+ * ChangesetManagementPanel is a source for {@link java.beans.PropertyChangeEvent}s. Clients can listen to
  * <ul>
  *   <li>{@link #SELECTED_CHANGESET_PROP}  - the new value in the property change event is
  *   the changeset selected by the user. The value is null if the user didn't select a
- *   a changeset or if he chosed to use a new changeset.</li>
+ *   a changeset or if he chose to use a new changeset.</li>
  *   <li> {@link #CLOSE_CHANGESET_AFTER_UPLOAD} - the new value is a boolean value indicating
  *   whether the changeset should be closed after the next upload</li>
  * </ul>

commit 005373f6021f455cc96a2e93e47ea30b88894a06

fix #17574 - fix IAE when plugins modify preferences (regression from r14977)

src/org/openstreetmap/josm/data/Preferences.java
@@ -227,8 +227,9 @@ public class Preferences extends AbstractPreferences {
     }
 
     protected void firePreferenceChanged(String key, Setting<?> oldValue, Setting<?> newValue) {
+        final Class<?> source = ReflectionUtils.findCallerClass(preferencesClasses);
         final PreferenceChangeEvent evt =
-                new DefaultPreferenceChangeEvent(ReflectionUtils.findCallerClass(preferencesClasses), key, oldValue, newValue);
+                new DefaultPreferenceChangeEvent(source != null ? source : getClass(), key, oldValue, newValue);
         listeners.fireEvent(listener -> listener.preferenceChanged(evt));
 
         ListenerList<org.openstreetmap.josm.spi.preferences.PreferenceChangedListener> forKey = keyListeners.get(key);

src/org/openstreetmap/josm/plugins/PluginHandler.java
@@ -299,6 +299,11 @@ public final class PluginHandler {
         }
     }
 
+    /**
+     * Plugin class loaders.
+     */
+    private static final Map<PluginInformation, PluginClassLoader> classLoaders = new HashMap<>();
+
     private static PluginDownloadTask pluginDownloadTask;
 
     /**
@@ -316,6 +321,15 @@ public final class PluginHandler {
      */
     public static Collection<ClassLoader> getResourceClassLoaders() {
         return Collections.unmodifiableCollection(sources);
+    }
+
+    /**
+     * Returns all plugin classloaders.
+     * @return all plugin classloaders
+     * @since 14978
+     */
+    public static Collection<PluginClassLoader> getPluginClassLoaders() {
+        return Collections.unmodifiableCollection(classLoaders.values());
     }
 
     /**
@@ -836,7 +850,7 @@ public final class PluginHandler {
             if (toLoad.isEmpty())
                 return;
 
-            Map<PluginInformation, PluginClassLoader> classLoaders = new HashMap<>();
+            classLoaders.clear();
             for (PluginInformation info : toLoad) {
                 PluginClassLoader cl = AccessController.doPrivileged((PrivilegedAction<PluginClassLoader>)
                     () -> new PluginClassLoader(

src/org/openstreetmap/josm/tools/ReflectionUtils.java
@@ -7,6 +7,8 @@ import java.security.PrivilegedAction;
 import java.util.Collection;
 import java.util.function.Function;
 
+import org.openstreetmap.josm.plugins.PluginHandler;
+
 /**
  * Reflection utilities.
  * @since 14977
@@ -45,6 +47,13 @@ public final class ReflectionUtils {
             try {
                 return Class.forName(x.getClassName());
             } catch (ClassNotFoundException e) {
+                for (ClassLoader cl : PluginHandler.getPluginClassLoaders()) {
+                    try {
+                        return Class.forName(x.getClassName(), true, cl);
+                    } catch (ClassNotFoundException ex) {
+                        Logging.trace(ex);
+                    }
+                }
                 Logging.error(e);
                 return null;
             }