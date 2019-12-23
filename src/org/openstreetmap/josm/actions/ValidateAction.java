tsmock/josm

commit 833281b0917e0bf23be7befe7cb74510e942d478

see #17343: prepare solution
updateEnabledState() should be called in ValidatorDialog

git-svn-id: https://josm.openstreetmap.de/svn/trunk@14835 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/ValidateAction.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.preferences.sources.ValidatorPrefHelper;
 import org.openstreetmap.josm.data.validation.OsmValidator;
@@ -94,7 +95,8 @@ public class ValidateAction extends JosmAction {
 
     @Override
     public void updateEnabledState() {
-        setEnabled(getLayerManager().getActiveDataSet() != null);
+        DataSet ds = getLayerManager().getActiveDataSet() ;
+        setEnabled(ds != null && !ds.isEmpty());
     }
 
     @Override

src/org/openstreetmap/josm/data/osm/DataSet.java
@@ -1200,4 +1200,12 @@ public final class DataSet implements OsmData<OsmPrimitive, Node, Way, Relation>
     public void setRemark(String remark) {
         this.remark = remark;
     }
+
+    /**
+     * @return true if this Dataset contains no primitives
+     * @since 14835
+     */
+    public boolean isEmpty() {
+        return allPrimitives.isEmpty();
+    }
 }

commit 17d25fdf6de3cc3c93a1d0e04e2df0ad563a79b0

fix #18456: validation not possible right away (regression from r14835) Enable validator when data layer exists, no matter if it is empty or not.

src/org/openstreetmap/josm/actions/ValidateAction.java
@@ -11,7 +11,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.preferences.sources.ValidatorPrefHelper;
 import org.openstreetmap.josm.data.validation.OsmValidator;
@@ -95,8 +94,7 @@ public class ValidateAction extends JosmAction {
 
     @Override
     public void updateEnabledState() {
-        DataSet ds = getLayerManager().getActiveDataSet();
-        setEnabled(ds != null && !ds.isEmpty());
+        setEnabled(getLayerManager().getActiveDataSet() != null);
     }
 
     @Override