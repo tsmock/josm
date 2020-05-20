tsmock/josm

commit 1588ba6eabfe5ccc03fb320e3edf80be1867a5e1

see #19251 - Java 8: use Stream
git-svn-id: https://josm.openstreetmap.de/svn/trunk@16436 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/projection/datum/NTV2SubGrid.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import org.openstreetmap.josm.tools.Logging;
 import org.openstreetmap.josm.tools.Utils;
@@ -170,18 +171,14 @@ public class NTV2SubGrid implements Serializable {
      * @return the Sub Grid containing the Coordinate or null
      */
     public NTV2SubGrid getSubGridForCoord(double lon, double lat) {
-        if (isCoordWithin(lon, lat)) {
-            if (subGrid == null)
-                return this;
-            else {
-                for (NTV2SubGrid aSubGrid : subGrid) {
-                    if (aSubGrid.isCoordWithin(lon, lat))
-                        return aSubGrid.getSubGridForCoord(lon, lat);
-                }
-                return this;
-            }
-        } else
-            return null;
+        return !isCoordWithin(lon, lat)
+                ? null
+                : subGrid == null
+                ? this
+                : Arrays.stream(subGrid)
+                .filter(aSubGrid -> aSubGrid.isCoordWithin(lon, lat))
+                .map(aSubGrid -> aSubGrid.getSubGridForCoord(lon, lat))
+                .findFirst().orElse(this);
     }
 
     /**

commit 0743179190757e5f47e2572aa3fd7ee0b44acf4b

see #19259 - NPE robustness
Possible regression from r16436

src/org/openstreetmap/josm/data/projection/datum/NTV2SubGrid.java
@@ -24,6 +24,7 @@ import java.io.InputStream;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Objects;
 
 import org.openstreetmap.josm.tools.Logging;
 import org.openstreetmap.josm.tools.Utils;
@@ -178,6 +179,7 @@ public class NTV2SubGrid implements Serializable {
                 : Arrays.stream(subGrid)
                 .filter(aSubGrid -> aSubGrid.isCoordWithin(lon, lat))
                 .map(aSubGrid -> aSubGrid.getSubGridForCoord(lon, lat))
+                .filter(Objects::nonNull)
                 .findFirst().orElse(this);
     }
 