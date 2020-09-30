tsmock/josm

commit 71dd9a214f0538609d30532fb4450a592800a002

fix #16031 - Presets: make `use_last_as_default="true"` work (patch by mikko.lukas)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@16687 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/tagging/presets/items/Combo.java
@@ -94,7 +94,7 @@ public class Combo extends ComboMultiSelect {
         } else if (usage.unused()) {
             // all items were unset (and so is default)
             originalValue = getListEntry("");
-            if (!presetInitiallyMatches && isForceUseLastAsDefault() && LAST_VALUES.containsKey(key)) {
+            if (!presetInitiallyMatches && isUseLastAsDefault() && LAST_VALUES.containsKey(key)) {
                 combobox.setSelectedItem(getListEntry(LAST_VALUES.get(key)));
             } else {
                 combobox.setSelectedItem(originalValue);

src/org/openstreetmap/josm/gui/tagging/presets/items/MultiSelect.java
@@ -38,7 +38,11 @@ public class MultiSelect extends ComboMultiSelect {
             list.setSelectedItem(def);
         } else if (usage.unused()) {
             originalValue = null;
-            list.setSelectedItem(originalValue);
+            if (!presetInitiallyMatches && isUseLastAsDefault() && LAST_VALUES.containsKey(key)) {
+                list.setSelectedItem(getListEntry(LAST_VALUES.get(key)));
+            } else {
+                list.setSelectedItem(originalValue);
+            }
         } else {
             originalValue = DIFFERENT;
             list.setSelectedItem(originalValue);

commit 0cc6cfa99df51fc2914cf73c171a61087addc6d3

fix #19813 - see #16031 - fix selection of Multiselect presets (regression from r16687:16688)

src/org/openstreetmap/josm/gui/tagging/presets/items/Combo.java
@@ -76,7 +76,7 @@ public class Combo extends ComboMultiSelect {
             acList.add(getDisplayValues(), AutoCompletionPriority.IS_IN_STANDARD);
         }
         combobox.setEditor(tf);
-        combobox.setSelectedItem(getItemToSelect(def, presetInitiallyMatches));
+        combobox.setSelectedItem(getItemToSelect(def, presetInitiallyMatches, false));
 
         if (key != null && ("colour".equals(key) || key.startsWith("colour:") || key.endsWith(":colour"))) {
             p.add(combobox, GBC.std().fill(GBC.HORIZONTAL));

src/org/openstreetmap/josm/gui/tagging/presets/items/ComboMultiSelect.java
@@ -341,33 +341,37 @@ public abstract class ComboMultiSelect extends KeyedItem {
         return null;
     }
 
-    protected Object getItemToSelect(String def, boolean presetInitiallyMatches) {
+    protected Object getItemToSelect(String def, boolean presetInitiallyMatches, boolean multi) {
         final Object itemToSelect;
         if (usage.hasUniqueValue()) {
             // all items have the same value (and there were no unset items)
-            originalValue = getListEntry(usage.getFirst());
+            originalValue = multi ? usage.getFirst() : getListEntry(usage.getFirst());
             itemToSelect = originalValue;
         } else if (def != null && usage.unused()) {
             // default is set and all items were unset
             if (!usage.hadKeys() || PROP_FILL_DEFAULT.get() || isForceUseLastAsDefault()) {
                 // selected osm primitives are untagged or filling default feature is enabled
-                PresetListEntry entry = getListEntry(def);
-                itemToSelect = entry == null ? "" : entry.getDisplayValue();
+                if (multi) {
+                    itemToSelect = def;
+                } else {
+                    PresetListEntry entry = getListEntry(def);
+                    itemToSelect = entry == null ? "" : entry.getDisplayValue();
+                }
             } else {
                 // selected osm primitives are tagged and filling default feature is disabled
                 itemToSelect = "";
             }
-            originalValue = getListEntry(DIFFERENT);
+            originalValue = multi ? DIFFERENT : getListEntry(DIFFERENT);
         } else if (usage.unused()) {
             // all items were unset (and so is default)
-            originalValue = getListEntry("");
+            originalValue = multi ? null : getListEntry("");
             if (LAST_VALUES.containsKey(key) && isUseLastAsDefault() && (!presetInitiallyMatches || isForceUseLastAsDefault())) {
                 itemToSelect = getListEntry(LAST_VALUES.get(key));
             } else {
                 itemToSelect = originalValue;
             }
         } else {
-            originalValue = getListEntry(DIFFERENT);
+            originalValue = multi ? DIFFERENT : getListEntry(DIFFERENT);
             itemToSelect = originalValue;
         }
         return itemToSelect;

src/org/openstreetmap/josm/gui/tagging/presets/items/MultiSelect.java
@@ -34,8 +34,7 @@ public class MultiSelect extends ComboMultiSelect {
         component = list;
         ListCellRenderer<PresetListEntry> renderer = getListCellRenderer();
         list.setCellRenderer(renderer);
-        list.setSelectedItem(getItemToSelect(def, presetInitiallyMatches));
-
+        list.setSelectedItem(getItemToSelect(def, presetInitiallyMatches, true));
         JScrollPane sp = new JScrollPane(list);
         // if a number of rows has been specified in the preset,
         // modify preferred height of scroll pane to match that row count.