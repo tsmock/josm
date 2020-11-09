tsmock/josm

commit 4507ff70e88afa15f28b89060c7403834cc661e3

see #7548: Re-organize the preference dialog Apply 7548-no-empty.patch to avoid empty top panel - make sure that a panel is selected when `selectPreviouslySelectedPreferences()` is called and nothing was previously selected - reverts r17097
git-svn-id: https://josm.openstreetmap.de/svn/trunk@17265 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/preferences/PreferenceTabbedPane.java
@@ -558,6 +558,7 @@ public final class PreferenceTabbedPane extends JTabbedPane implements ExpertMod
                 setSelectedIndex(index);
             }
         }
+        setSelectedIndex(-1);
     }
 
     @Override

src/org/openstreetmap/josm/actions/PreferencesAction.java
@@ -7,8 +7,6 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
-import javax.swing.SwingUtilities;
-
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.preferences.PreferenceDialog;
 import org.openstreetmap.josm.gui.preferences.SubPreferenceSetting;
@@ -107,15 +105,13 @@ public class PreferencesAction extends JosmAction implements Runnable {
     @Override
     public void run() {
         final PreferenceDialog p = new PreferenceDialog(MainApplication.getMainFrame());
-        SwingUtilities.invokeLater(() -> {
-            if (tab != null) {
-                p.selectPreferencesTabByClass(tab);
-            } else if (subTab != null) {
-                p.selectSubPreferencesTabByClass(subTab);
-            } else {
-                p.selectPreviouslySelectedPreferences();
-            }
-        });
+        if (tab != null) {
+            p.selectPreferencesTabByClass(tab);
+        } else if (subTab != null) {
+            p.selectSubPreferencesTabByClass(subTab);
+        } else {
+            p.selectPreviouslySelectedPreferences();
+        }
         p.setVisible(true);
     }
 }

src/org/openstreetmap/josm/gui/preferences/PreferenceDialog.java
@@ -153,6 +153,8 @@ public class PreferenceDialog extends JDialog {
             tpPreferences.selectSubTabByPref(previouslySelected.b);
         } else if (previouslySelected != null && previouslySelected.a != null) {
             tpPreferences.selectTabByPref(previouslySelected.a);
+        } else {
+            tpPreferences.setSelectedIndex(0);
         }
     }
 

commit 077dfb16812b1dd4e5db7f5a61b4a3074c98c153

see #7548: Re-organize the preference dialog - Correct handling of expert mode toggle (when enable/disable expert mode ... no content displayed) (regression introduced by r17265)

src/org/openstreetmap/josm/gui/preferences/PreferenceTabbedPane.java
@@ -560,13 +560,11 @@ public final class PreferenceTabbedPane extends JTabbedPane implements ExpertMod
                 Logging.debug("{0}: hiding empty {1}", getClass().getSimpleName(), tps);
             });
         }
+        int index = -1;
         if (sel != null) {
-            int index = indexOfComponent(sel);
-            if (index > -1) {
-                setSelectedIndex(index);
-            }
+            index = indexOfComponent(sel);
         }
-        setSelectedIndex(-1);
+        setSelectedIndex(index);
     }
 
     @Override