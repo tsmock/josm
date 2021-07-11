tsmock/josm

commit c8cf1ccf5de3d18f60106457b828ab276323a043

see #20745 - Introduce TableHelper.selectedIndices
git-svn-id: https://josm.openstreetmap.de/svn/trunk@17773 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/RestorePropertyAction.java
@@ -5,9 +5,10 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import javax.swing.AbstractAction;
 import javax.swing.ListSelectionModel;
@@ -53,10 +54,8 @@ public class RestorePropertyAction extends AbstractAction {
         OsmPrimitive primitive = objectSp.get();
         if (primitive == null) return;
 
-        HashMap<String, String> changes = new HashMap<>();
-        for (int index : TableHelper.getSelectedIndices(selectionModel)) {
-            changes.put(keyFn.apply(index), valueFn.apply(index));
-        }
+        Map<String, String> changes = TableHelper.selectedIndices(selectionModel).boxed()
+                .collect(Collectors.toMap(keyFn::apply, valueFn::apply, (a, b) -> b));
         ChangePropertyCommand command = new ChangePropertyCommand(Collections.singleton(primitive), changes);
         UndoRedoHandler.getInstance().add(command);
     }

src/org/openstreetmap/josm/gui/dialogs/LayerListDialog.java
@@ -89,7 +89,6 @@ import org.openstreetmap.josm.gui.widgets.JosmTextField;
 import org.openstreetmap.josm.gui.widgets.PopupMenuLauncher;
 import org.openstreetmap.josm.gui.widgets.ScrollableTable;
 import org.openstreetmap.josm.spi.preferences.Config;
-import org.openstreetmap.josm.tools.ArrayUtils;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.ImageProvider.ImageSizes;
 import org.openstreetmap.josm.tools.InputMapUtils;
@@ -891,7 +890,7 @@ public class LayerListDialog extends ToggleDialog implements DisplaySettingsChan
          * @return  the list of indices of the selected rows. Never null, but may be empty.
          */
         public List<Integer> getSelectedRows() {
-            return ArrayUtils.toList(TableHelper.getSelectedIndices(selectionModel));
+            return TableHelper.selectedIndices(selectionModel).boxed().collect(Collectors.toList());
         }
 
         /**
@@ -904,9 +903,8 @@ public class LayerListDialog extends ToggleDialog implements DisplaySettingsChan
                 return;
             layer.removePropertyChangeListener(this);
             final int size = getRowCount();
-            final int[] rows = TableHelper.getSelectedIndices(selectionModel);
 
-            if (rows.length == 0 && size > 0) {
+            if (selectionModel.isSelectionEmpty() && size > 0) {
                 selectionModel.setSelectionInterval(size-1, size-1);
             }
             fireTableDataChanged();

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableModel.java
@@ -2,7 +2,6 @@
 package org.openstreetmap.josm.gui.dialogs.relation;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Collection;
 import java.util.Collections;
@@ -50,7 +49,6 @@ import org.openstreetmap.josm.gui.tagging.presets.TaggingPresets;
 import org.openstreetmap.josm.gui.util.GuiHelper;
 import org.openstreetmap.josm.gui.util.SortableTableModel;
 import org.openstreetmap.josm.gui.widgets.OsmPrimitivesTableModel;
-import org.openstreetmap.josm.tools.ArrayUtils;
 import org.openstreetmap.josm.tools.JosmRuntimeException;
 import org.openstreetmap.josm.tools.bugreport.BugReport;
 
@@ -512,7 +510,7 @@ implements TableModelListener, DataSelectionListener, DataSetListener, OsmPrimit
      * @return a collection with the currently selected relation members
      */
     public Collection<RelationMember> getSelectedMembers() {
-        return Arrays.stream(getSelectedIndices())
+        return selectedIndices()
                 .mapToObj(members::get)
                 .collect(Collectors.toList());
     }
@@ -691,10 +689,7 @@ implements TableModelListener, DataSelectionListener, DataSetListener, OsmPrimit
         }
         addToSelectedMembers(selected);
         getSelectionModel().setValueIsAdjusting(false);
-        int[] selectedIndices = getSelectedIndices();
-        if (selectedIndices.length > 0) {
-            fireMakeMemberVisible(selectedIndices[0]);
-        }
+        selectedIndices().findFirst().ifPresent(this::fireMakeMemberVisible);
     }
 
     /**
@@ -722,7 +717,7 @@ implements TableModelListener, DataSelectionListener, DataSetListener, OsmPrimit
             sortedMembers = newMembers;
         } else {
             sortedMembers = relationSorter.sortMembers(selectedMembers);
-            List<Integer> selectedIndices = ArrayUtils.toList(getSelectedIndices());
+            List<Integer> selectedIndices = selectedIndices().boxed().collect(Collectors.toList());
             newMembers = new ArrayList<>();
             boolean inserted = false;
             for (int i = 0; i < members.size(); i++) {
@@ -783,8 +778,8 @@ implements TableModelListener, DataSelectionListener, DataSetListener, OsmPrimit
      */
     @Override
     public void reverse() {
-        List<Integer> selectedIndices = ArrayUtils.toList(getSelectedIndices());
-        List<Integer> selectedIndicesReversed = ArrayUtils.toList(getSelectedIndices());
+        List<Integer> selectedIndices = selectedIndices().boxed().collect(Collectors.toList());
+        List<Integer> selectedIndicesReversed = selectedIndices().boxed().collect(Collectors.toList());
 
         if (selectedIndices.size() <= 1) {
             Collections.reverse(members);

src/org/openstreetmap/josm/gui/history/SelectionSynchronizer.java
@@ -1,7 +1,6 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.history;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -60,13 +59,12 @@ public class SelectionSynchronizer implements ListSelectionListener {
         }
         preventRecursion = true;
         ListSelectionModel referenceModel = (ListSelectionModel) e.getSource();
-        int[] selectedIndices = TableHelper.getSelectedIndices(referenceModel);
         for (ListSelectionModel model : participants) {
             if (model == referenceModel) {
                 continue;
             }
             TableHelper.setSelectedIndices(model,
-                    Arrays.stream(selectedIndices).flatMap(i -> selectionIndexMapper.apply(i, referenceModel)));
+                    TableHelper.selectedIndices(referenceModel).flatMap(i -> selectionIndexMapper.apply(i, referenceModel)));
         }
         preventRecursion = false;
     }

src/org/openstreetmap/josm/gui/util/ReorderableTableModel.java
@@ -2,6 +2,7 @@
 package org.openstreetmap.josm.gui.util;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import javax.swing.JList;
 import javax.swing.JTable;
@@ -37,9 +38,19 @@ public interface ReorderableTableModel<T> extends ReorderableModel<T> {
     /**
      * Returns an array of all of the selected indices in the selection model, in increasing order.
      * @return an array of all of the selected indices in the selection model, in increasing order
+     * @see #selectedIndices()
      */
     default int[] getSelectedIndices() {
         return TableHelper.getSelectedIndices(getSelectionModel());
+    }
+
+    /**
+     * Returns a stream of all of the selected indices in the selection model, in increasing order.
+     * @return a stream of all of the selected indices in the selection model, in increasing order
+     * @since 17773
+     */
+    default IntStream selectedIndices() {
+        return TableHelper.selectedIndices(getSelectionModel());
     }
 
     /**

src/org/openstreetmap/josm/gui/util/TableHelper.java
@@ -122,32 +122,35 @@ public final class TableHelper {
      * Returns an array of all of the selected indices in the selection model, in increasing order.
      * Unfortunately this method is not available in OpenJDK before version 11, see
      * https://bugs.openjdk.java.net/browse/JDK-8199395
-     * Code taken from OpenJDK 11. To be removed when we switch to Java 11 or later.
+     *
+     * To be removed when we switch to Java 11 or later.
      *
      * @param selectionModel list selection model.
      *
      * @return all of the selected indices, in increasing order,
      *         or an empty array if nothing is selected
      * @since 15226
+     * @see #selectedIndices(ListSelectionModel)
      */
     public static int[] getSelectedIndices(ListSelectionModel selectionModel) {
-        int iMin = selectionModel.getMinSelectionIndex();
-        int iMax = selectionModel.getMaxSelectionIndex();
+        return selectedIndices(selectionModel).toArray();
+    }
 
-        if (iMin < 0 || iMax < 0) {
-            return new int[0];
+    /**
+     * Returns a stream of all of the selected indices in the selection model, in increasing order.
+     *
+     * @param selectionModel list selection model.
+     *
+     * @return all of the selected indices, in increasing order,
+     *         or an empty stream if nothing is selected
+     * @since 17773
+     */
+    public static IntStream selectedIndices(ListSelectionModel selectionModel) {
+        if (selectionModel.isSelectionEmpty()) {
+            return IntStream.empty();
         }
-
-        int[] rvTmp = new int[1 + iMax - iMin];
-        int n = 0;
-        for (int i = iMin; i <= iMax; i++) {
-            if (selectionModel.isSelectedIndex(i)) {
-                rvTmp[n++] = i;
-            }
-        }
-        int[] rv = new int[n];
-        System.arraycopy(rvTmp, 0, rv, 0, n);
-        return rv;
+        return IntStream.rangeClosed(selectionModel.getMinSelectionIndex(), selectionModel.getMaxSelectionIndex())
+                .filter(selectionModel::isSelectedIndex);
     }
 
     /**

src/org/openstreetmap/josm/tools/ArrayUtils.java
@@ -1,47 +1,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.tools;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-/**
- * Utility methods for arrays.
- * @since 15226
- */
-public final class ArrayUtils {
-
-    /**
-     * Utility class
-     */
-    private ArrayUtils() {
-        // Hide default constructor for utility classes
-    }
-
-    /**
-     * Converts an array of int to a list of Integer.
-     * @param array array of int
-     * @return list of Integer
-     */
-    public static List<Integer> toList(int[] array) {
-        return Arrays.stream(array).boxed().collect(Collectors.toList());
-    }
-
-    /**
-     * Converts an array of long to a list of Long.
-     * @param array array of long
-     * @return list of Long
-     */
-    public static List<Long> toList(long[] array) {
-        return Arrays.stream(array).boxed().collect(Collectors.toList());
-    }
-
-    /**
-     * Converts an array of double to a list of Double.
-     * @param array array of double
-     * @return list of Double
-     */
-    public static List<Double> toList(double[] array) {
-        return Arrays.stream(array).boxed().collect(Collectors.toList());
-    }
-}

commit 2c6b239d83c986a4d0dea7445e776686b0d7f32d

fix #20965 - History browser: fix NPE when restoring deleted tag value (regression from r17773, patch by Bjoeni) + add unit test

src/org/openstreetmap/josm/actions/RestorePropertyAction.java
@@ -5,10 +5,10 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import javax.swing.AbstractAction;
 import javax.swing.ListSelectionModel;
@@ -55,7 +55,7 @@ public class RestorePropertyAction extends AbstractAction {
         if (primitive == null) return;
 
         Map<String, String> changes = TableHelper.selectedIndices(selectionModel).boxed()
-                .collect(Collectors.toMap(keyFn::apply, valueFn::apply, (a, b) -> b));
+                .collect(HashMap::new, (m,i)->m.put(keyFn.apply(i), valueFn.apply(i)), HashMap::putAll);
         ChangePropertyCommand command = new ChangePropertyCommand(Collections.singleton(primitive), changes);
         UndoRedoHandler.getInstance().add(command);
     }

test/unit/org/openstreetmap/josm/actions/RestorePropertyActionTest.java
@@ -1,0 +1,49 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.actions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.ListSelectionModel;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openstreetmap.josm.command.ChangePropertyCommand;
+import org.openstreetmap.josm.data.UndoRedoHandler;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+/**
+ * Unit tests of {@link RestorePropertyAction}
+ */
+class RestorePropertyActionTest {
+
+    /**
+     * Setup test.
+     */
+    @RegisterExtension
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules().preferences();
+
+    @Test
+    void testTicket20965() {
+        doTest20965(null, null);
+        doTest20965("key", null);
+        doTest20965(null, "val");
+        doTest20965("key", "val");
+    }
+
+    private static void doTest20965(String key, String val) {
+        ListSelectionModel selModel = new DefaultListSelectionModel();
+        selModel.setSelectionInterval(1, 2);
+        Node n = new Node(LatLon.NORTH_POLE);
+        new DataSet(n);
+        UndoRedoHandler.getInstance().clean();
+        new RestorePropertyAction(k -> key, v -> val, () -> n, selModel).actionPerformed(null);
+        assertTrue(UndoRedoHandler.getInstance().getLastCommand() instanceof ChangePropertyCommand);
+    }
+}