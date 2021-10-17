tsmock/josm

commit 26234160ff0b7fbd4056c5f90101e5d1364f4d05

fix #21438 - NPE (patch by marcello)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@18277 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPresets.java
@@ -204,13 +204,13 @@ public final class TaggingPresets {
     /**
      * Return set of values for a key in the tagging presets
      * @param key the key
-     * @return set of values for a key in the tagging presets or null if none is found
+     * @return set of values for a key in the tagging presets
      */
     public static Set<String> getPresetValues(String key) {
         Set<String> values = PRESET_TAG_CACHE.get(key);
         if (values != null)
             return Collections.unmodifiableSet(values);
-        return null;
+        return Collections.emptySet();
     }
 
     /**

src/org/openstreetmap/josm/data/validation/tests/TagChecker.java
@@ -545,7 +545,7 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
      */
     private static Set<String> getPresetValues(String key) {
         Set<String> res = TaggingPresets.getPresetValues(key);
-        if (res != null)
+        if (!Utils.isEmpty(res))
             return res;
         if (additionalPresetsValueData.contains(key))
             return Collections.emptySet();
@@ -560,7 +560,7 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
      * @since 9023
      */
     public static boolean isKeyInPresets(String key) {
-        return TaggingPresets.getPresetValues(key) != null;
+        return !Utils.isEmpty(TaggingPresets.getPresetValues(key));
     }
 
     /**

commit 7bcad35e9952c812420e478973b0ff279c312c14

fix #21438: NPE in AutoCompletionManager.getAllForKeys() - fix regression introduced with r18277

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPresets.java
@@ -214,6 +214,16 @@ public final class TaggingPresets {
     }
 
     /**
+     * Determines if the given key is in the loaded presets.
+     * @param key key
+     * @return {@code true} if the given key in the loaded presets
+     * @since xxx
+     */
+    public static boolean isKeyInPresets(String key) {
+        return PRESET_TAG_CACHE.get(key) != null;
+    }
+
+    /**
      * Replies a new collection of all presets matching the parameters.
      *
      * @param t the preset types to include

src/org/openstreetmap/josm/data/validation/tests/TagChecker.java
@@ -544,9 +544,9 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
      *  else a set which might be empty.
      */
     private static Set<String> getPresetValues(String key) {
-        Set<String> res = TaggingPresets.getPresetValues(key);
-        if (!Utils.isEmpty(res))
-            return res;
+        if (TaggingPresets.isKeyInPresets(key)) {
+            return TaggingPresets.getPresetValues(key);
+        }
         if (additionalPresetsValueData.contains(key))
             return Collections.emptySet();
         // null means key is not known
@@ -558,9 +558,11 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
      * @param key key
      * @return {@code true} if the given key is in internal presets
      * @since 9023
+     * @deprecated Use {@link TaggingPresets#isKeyInPresets(String)} instead
      */
+    @Deprecated
     public static boolean isKeyInPresets(String key) {
-        return !Utils.isEmpty(TaggingPresets.getPresetValues(key));
+        return TaggingPresets.isKeyInPresets(key);
     }
 
     /**
@@ -882,7 +884,7 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
         if (!checkValues || key == null || Utils.isEmpty(value))
             return;
         if (additionalPresetsValueData != null && !isTagIgnored(key, value)) {
-            if (!isKeyInPresets(key)) {
+            if (!TaggingPresets.isKeyInPresets(key)) {
                 spellCheckKey(withErrors, p, key);
             } else if (!isTagInPresets(key, value)) {
                 if (oftenUsedTags.contains(key, value)) {
@@ -906,7 +908,7 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
         if (ignoreDataEquals.contains(prettifiedKey)) {
             fixedKey = prettifiedKey;
         } else {
-            fixedKey = isKeyInPresets(prettifiedKey) ? prettifiedKey : harmonizedKeys.get(prettifiedKey);
+            fixedKey = TaggingPresets.isKeyInPresets(prettifiedKey) ? prettifiedKey : harmonizedKeys.get(prettifiedKey);
         }
         if (fixedKey == null && ignoreDataTag.stream().anyMatch(a -> a.getKey().equals(prettifiedKey))) {
             fixedKey = prettifiedKey;