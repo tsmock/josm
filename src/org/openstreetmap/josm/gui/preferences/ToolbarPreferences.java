tsmock/josm

commit 0d456c911ebd297e6c156a6562ac705e99331591

fix #21446: Memory leaks with TaggingPreset - don't always add ActiveLayerChangeListener in `TaggingPreset` constructor, only needed for some instances - let ToolbarPreferences implement TaggingPresetListener to update toolbar and free (most) refs to presets when a preset was uninstalled
git-svn-id: https://josm.openstreetmap.de/svn/trunk@18321 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/preferences/ToolbarPreferences.java
@@ -76,6 +76,8 @@ import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.gui.help.HelpUtil;
 import org.openstreetmap.josm.gui.tagging.presets.TaggingPreset;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetListener;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresets;
 import org.openstreetmap.josm.gui.util.GuiHelper;
 import org.openstreetmap.josm.gui.util.ReorderableTableModel;
 import org.openstreetmap.josm.spi.preferences.Config;
@@ -90,7 +92,7 @@ import org.openstreetmap.josm.tools.Utils;
  * Toolbar preferences.
  * @since 172
  */
-public class ToolbarPreferences implements PreferenceSettingFactory {
+public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPresetListener {
 
     private static final String EMPTY_TOOLBAR_MARKER = "<!-empty-!>";
 
@@ -1022,6 +1024,7 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
             control.setComponentPopupMenu(popupMenu);
         });
         MapFrame.TOOLBAR_VISIBLE.addListener(e -> refreshToolbarControl());
+        TaggingPresets.addListener(this);
     }
 
     private void loadAction(DefaultMutableTreeNode node, MenuElement menu) {
@@ -1138,6 +1141,7 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
             }
         }
         if (toolbar != null) {
+            actions.put(toolbar, action);
             regactions.put(toolbar, action);
         }
         return action;
@@ -1152,6 +1156,7 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
     public Action unregister(Action action) {
         Object toolbar = action.getValue("toolbar");
         if (toolbar instanceof String) {
+            actions.remove(toolbar);
             return regactions.remove(toolbar);
         }
         return null;
@@ -1284,4 +1289,9 @@ public class ToolbarPreferences implements PreferenceSettingFactory {
     }
 
     private static final DataFlavor ACTION_FLAVOR = new DataFlavor(ActionDefinition.class, "ActionItem");
+
+    @Override
+    public void taggingPresetsModified() {
+        refreshToolbarControl();
+    }
 }

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPreset.java
@@ -177,7 +177,6 @@ public class TaggingPreset extends AbstractAction implements ActiveLayerChangeLi
      * Use this as default item for "do not select anything".
      */
     public TaggingPreset() {
-        MainApplication.getLayerManager().addActiveLayerChangeListener(this);
         updateEnabledState();
     }
 

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPresets.java
@@ -90,6 +90,7 @@ public final class TaggingPresets {
         for (TaggingPreset tp: taggingPresets) {
             if (!(tp instanceof TaggingPresetSeparator)) {
                 MainApplication.getToolbar().register(tp);
+                MainApplication.getLayerManager().addActiveLayerChangeListener(tp);
             }
         }
         if (taggingPresets.isEmpty()) {
@@ -137,12 +138,16 @@ public final class TaggingPresets {
      */
     public static void destroy() {
         ToolbarPreferences toolBar = MainApplication.getToolbar();
-        taggingPresets.forEach(toolBar::unregister);
+        for (TaggingPreset tp: taggingPresets) {
+            toolBar.unregister(tp);
+            if (!(tp instanceof TaggingPresetSeparator)) {
+                MainApplication.getLayerManager().removeActiveLayerChangeListener(tp);
+            }
+        }
         taggingPresets.clear();
         PRESET_TAG_CACHE.clear();
         PRESET_ROLE_CACHE.clear();
         MainApplication.getMenu().presetsMenu.removeAll();
-        listeners.forEach(TaggingPresetListener::taggingPresetsModified);
     }
 
     /**

commit 23d3461458f80f8265564494064d6f795faa5ed9

fix #21730: Not all JosmActions registering in toolbar - separate again between all registered actions and those which appear in menus (regression from r18321) - replace global map actions by local one to avoid memory leak

src/org/openstreetmap/josm/gui/preferences/ToolbarPreferences.java
@@ -575,7 +575,6 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
      * Key: Registered name (property "toolbar" of action).
      * Value: The action to execute.
      */
-    private final Map<String, Action> actions = new ConcurrentHashMap<>();
     private final Map<String, Action> regactions = new ConcurrentHashMap<>();
 
     private final DefaultMutableTreeNode rootActionsNode = new DefaultMutableTreeNode(tr("Actions"));
@@ -1026,7 +1025,7 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
         TaggingPresets.addListener(this);
     }
 
-    private void loadAction(DefaultMutableTreeNode node, MenuElement menu) {
+    private void loadAction(DefaultMutableTreeNode node, MenuElement menu, Map<String, Action> actionsInMenu) {
         Object userObject = null;
         MenuElement menuElement = menu;
         if (menu.getSubElements().length > 0 &&
@@ -1052,12 +1051,12 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
                         continue;
                     } else {
                         String toolbar = (String) tb;
-                        Action r = actions.get(toolbar);
+                        Action r = actionsInMenu.get(toolbar);
                         if (r != null && r != action && !toolbar.startsWith(IMAGERY_PREFIX)) {
                             Logging.info(tr("Toolbar action {0} overwritten: {1} gets {2}",
                             toolbar, r.getClass().getName(), action.getClass().getName()));
                         }
-                        actions.put(toolbar, action);
+                        actionsInMenu.put(toolbar, action);
                     }
                 } else {
                     userObject = menuItem.getText();
@@ -1065,15 +1064,15 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
             }
             DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(userObject);
             node.add(newNode);
-            loadAction(newNode, item);
+            loadAction(newNode, item, actionsInMenu);
         }
     }
 
-    private void loadActions() {
+    private void loadActions(Map<String, Action> actionsInMenu) {
         rootActionsNode.removeAllChildren();
-        loadAction(rootActionsNode, MainApplication.getMenu());
+        loadAction(rootActionsNode, MainApplication.getMenu(), actionsInMenu);
         for (Map.Entry<String, Action> a : regactions.entrySet()) {
-            if (actions.get(a.getKey()) == null) {
+            if (actionsInMenu.get(a.getKey()) == null) {
                 rootActionsNode.add(new DefaultMutableTreeNode(a.getValue()));
             }
         }
@@ -1098,10 +1097,12 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
     }
 
     private Collection<ActionDefinition> getDefinedActions() {
-        loadActions();
+        Map<String, Action> actionsInMenu = new ConcurrentHashMap<>();
+
+        loadActions(actionsInMenu);
 
         Map<String, Action> allActions = new ConcurrentHashMap<>(regactions);
-        allActions.putAll(actions);
+        allActions.putAll(actionsInMenu);
         ActionParser actionParser = new ActionParser(allActions);
 
         Collection<ActionDefinition> result = new ArrayList<>();
@@ -1140,7 +1141,6 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
             }
         }
         if (toolbar != null) {
-            actions.put(toolbar, action);
             regactions.put(toolbar, action);
         }
         return action;
@@ -1155,7 +1155,6 @@ public class ToolbarPreferences implements PreferenceSettingFactory, TaggingPres
     public Action unregister(Action action) {
         Object toolbar = action.getValue("toolbar");
         if (toolbar instanceof String) {
-            actions.remove(toolbar);
             return regactions.remove(toolbar);
         }
         return null;