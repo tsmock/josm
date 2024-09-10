tsmock/josm

commit 8e373fc77e01b5fe47950b5b70a412bd9e9bcfbd

Fix #23828: Very slow wireframe view with large data set
This occurred since we were drawing every object in every relation (including
nodes of ways) even if those objects were not in view. We fix that by only
drawing objects that are inside the current viewing area.

git-svn-id: https://josm.openstreetmap.de/svn/trunk@19166 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/visitor/paint/WireframeMapRenderer.java
@@ -177,6 +177,7 @@ public class WireframeMapRenderer extends AbstractMapRenderer implements Primiti
                     way.accept(this);
                 }
             }
+            displaySegments();
         }
         displaySegments();
 
@@ -351,18 +352,19 @@ public class WireframeMapRenderer extends AbstractMapRenderer implements Primiti
         }
         g.setColor(col);
 
+        Bounds viewArea = mapState.getViewArea().getLatLonBoundsBox();
         for (IRelationMember<?> m : r.getMembers()) {
             if (m.getMember().isIncomplete() || !m.getMember().isDrawable()) {
                 continue;
             }
 
-            if (m.isNode()) {
+            if (m.isNode() && viewArea.contains((INode) m.getMember())) {
                 MapViewPoint p = mapState.getPointFor((INode) m.getMember());
                 if (p.isInView()) {
                     g.draw(new Ellipse2D.Double(p.getInViewX()-4, p.getInViewY()-4, 9, 9));
                 }
 
-            } else if (m.isWay()) {
+            } else if (m.isWay() && viewArea.contains(m.getMember().getBBox())) {
                 GeneralPath path = new GeneralPath();
 
                 boolean first = true;

commit 323b341ff5ab0411934ce46f363833796426e38c

Fix #23911: wireframe was not properly highlighting relations
This was a regression from r19166.

src/org/openstreetmap/josm/data/osm/visitor/paint/WireframeMapRenderer.java
@@ -364,7 +364,7 @@ public class WireframeMapRenderer extends AbstractMapRenderer implements Primiti
                     g.draw(new Ellipse2D.Double(p.getInViewX()-4, p.getInViewY()-4, 9, 9));
                 }
 
-            } else if (m.isWay() && viewArea.contains(m.getMember().getBBox())) {
+            } else if (m.isWay() && viewArea.intersects(m.getMember().getBBox())) {
                 GeneralPath path = new GeneralPath();
 
                 boolean first = true;